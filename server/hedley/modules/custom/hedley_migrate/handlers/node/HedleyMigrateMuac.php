98250faf1a471d639eb5c5658c8d8cc9eee42ba0
David Hernandez <davidhernandezruiz@gmail.com>
2017-01-09 15:36:32 +0100

    Adds migration for the Muac content type.

---
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateMuac.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @file
+ * Contains \HedleyMigrateMuac.
+ */
+
+/**
+ * Class HedleyMigrateMuac.
+ */
+class HedleyMigrateMuac extends HedleyMigrateBase {
+
+  public $entityType = 'node';
+  public $bundle = 'muac';
+
+  /**
+   *    * {@inheritdoc}
+   *       */
+  public function __construct($arguments) {
+    parent::__construct($arguments);
+    $this->description = t('Import Muacs from the CSV.');
+    $this->dependencies = [
+      'HedleyMigrateChild',
+    ];
+
+    $column_names = [
+      'title',
+      'field_child',
+      'field_date',
+      'field_activity_status',
+      'field_middle_circumference',
+      'field_upper_circumference',
+      'field_arm_circumference',
+    ];
+
+    $columns = [];
+    foreach ($column_names as $column_name) {
+      $columns[] = [$column_name, $column_name];
+    }
+
+    $source_file = $this->getMigrateDirectory() . '/csv/muac.csv';
+    $options = array('header_rows' => 1);
+    $this->source = new MigrateSourceCSV($source_file, $columns, $options);
+
+    $key = array(
+      'title' => array(
+        'type' => 'varchar',
+        'length' => 255,
+        'not null' => TRUE,
+      ),
+    );
+
+    $this->destination = new MigrateDestinationNode($this->bundle);
+
+    $this->map = new MigrateSQLMap($this->machineName, $key, MigrateDestinationNode::getKeySchema());
+
+    $simple_fields = drupal_map_assoc([
+      'title',
+    ]);
+
+    $this->addSimpleMappings($simple_fields);
+
+    $this->addFieldMapping('field_child', 'field_child')
+      ->sourceMigration('HedleyMigrateChild');
+
+    $this->addFieldMapping('field_activity_status', 'field_activity_status');
+
+    $this->addFieldMapping('field_middle_circumference', 'field_middle_circumference');
+    $this->addFieldMapping('field_upper_circumference', 'field_upper_circumference');
+    $this->addFieldMapping('field_arm_circumference', 'field_arm_circumference');
+
+    $this->addFieldMapping('field_date', 'field_date')
+      ->callbacks([$this, 'dateProcess']);
+  }
+
+}
