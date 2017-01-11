a557dd873d98837ca0a9702fab7ab4ea9ca2a7fc
Amitai Burstein <amitai@gizra.com>
2017-01-11 19:48:19 +0100

    More cases

--- client/src/elm/Activity/Utils.elm
+++ client/src/elm/Activity/Utils.elm
@@ -4,15 +4,93 @@ module Activity.Utils
         , getPendingNumberPerActivity
         )
 
-import Activity.Model exposing (ActivityIdentity, ActivityType(..), ChildActivityType(..))
+import Activity.Model exposing (ActivityIdentity, ActivityType(..), ChildActivityType(..), MotherActivityType(..))
 import Patient.Model exposing (PatientsDict)
 
 
 getActivityNameAndIcon : ActivityType -> ActivityIdentity
 getActivityNameAndIcon activityType =
-    ActivityIdentity "" ""
+    case activityType of
+        Child childActivityType ->
+            case childActivityType of
+                ChildPicture ->
+                    ActivityIdentity "" ""
+
+                Height ->
+                    ActivityIdentity "" ""
+
+                Weight ->
+                    ActivityIdentity "" ""
+
+                Muac ->
+                    ActivityIdentity "" ""
+
+                ProgressReport ->
+                    ActivityIdentity "" ""
+
+        Mother motherActivityType ->
+            case motherActivityType of
+                Aheza ->
+                    ActivityIdentity "" ""
+
+                Attendance ->
+                    ActivityIdentity "" ""
+
+                Education ->
+                    ActivityIdentity "" ""
+
+                FamilyPlanning ->
+                    ActivityIdentity "" ""
+
+                Hiv ->
+                    ActivityIdentity "" ""
+
+                MotherPicture ->
+                    ActivityIdentity "" ""
+
+                NutritionSigns ->
+                    ActivityIdentity "" ""
 
 
-getPendingNumberPerActivity : ChildActivityType -> PatientsDict -> Int
-getPendingNumberPerActivity childActivityType patients =
-    0
+getPendingNumberPerActivity : ActivityType -> PatientsDict -> Int
+getPendingNumberPerActivity activityType patients =
+    case activityType of
+        Child childActivityType ->
+            case childActivityType of
+                ChildPicture ->
+                    0
+
+                Height ->
+                    0
+
+                Weight ->
+                    0
+
+                Muac ->
+                    0
+
+                ProgressReport ->
+                    0
+
+        Mother motherActivityType ->
+            case motherActivityType of
+                Aheza ->
+                    0
+
+                Attendance ->
+                    0
+
+                Education ->
+                    0
+
+                FamilyPlanning ->
+                    0
+
+                Hiv ->
+                    0
+
+                MotherPicture ->
+                    0
+
+                NutritionSigns ->
+                    0
