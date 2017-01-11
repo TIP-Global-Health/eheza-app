f45c0f05a7fbe38546529398401539d5552775dd
Amitai Burstein <amitai@gizra.com>
2017-01-11 11:00:33 +0200

    Split viewChild and viewMother

--- client/src/elm/Pages/Patient/View.elm
+++ client/src/elm/Pages/Patient/View.elm
@@ -1,8 +1,10 @@
 module Pages.Patient.View exposing (view)
 
+import Child.Model exposing (Child, ChildId)
 import Date exposing (Date)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Mother.Model exposing (Mother, MotherId)
 import Pages.Patient.Model exposing (Msg(..))
 import Patient.Model exposing (PatientId, Patient, PatientType(..))
 import User.Model exposing (User)
@@ -10,31 +12,45 @@ import User.Model exposing (User)
 
 view : Date -> User -> PatientId -> Patient -> Html Msg
 view currentDate currentUser patientId patient =
-    let
-        -- @todo: move to Patient.View
-        ( name, image ) =
-            case patient.info of
-                PatientChild child ->
-                    ( child.name
-                    , child.image
-                    )
+    case patient.info of
+        PatientChild child ->
+            viewChild currentDate currentUser patientId child
 
-                PatientMother mother ->
-                    ( mother.name
-                    , mother.image
-                    )
-    in
-        div []
-            [ div
-                [ class "ui secondary pointing fluid menu" ]
-                [ h1
-                    [ class "ui header" ]
-                    [ text name ]
-                ]
-            , div []
-                [ img [ src image ] []
-                ]
-            , div
-                [ class "ui divider" ]
-                []
+        PatientMother mother ->
+            viewMother currentDate currentUser patientId mother
+
+
+viewChild : Date -> User -> ChildId -> Child -> Html Msg
+viewChild currentDate currentUser childId child =
+    div []
+        [ div
+            [ class "ui secondary pointing fluid menu" ]
+            [ h1
+                [ class "ui header" ]
+                [ text child.name ]
             ]
+        , div []
+            [ img [ src child.image ] []
+            ]
+        , div
+            [ class "ui divider" ]
+            [ text <| "Mother ID: " ++ child.motherId ]
+        ]
+
+
+viewMother : Date -> User -> MotherId -> Mother -> Html Msg
+viewMother currentDate currentUser motherId mother =
+    div []
+        [ div
+            [ class "ui secondary pointing fluid menu" ]
+            [ h1
+                [ class "ui header" ]
+                [ text mother.name ]
+            ]
+        , div []
+            [ img [ src mother.image ] []
+            ]
+        , div
+            [ class "ui divider" ]
+            []
+        ]
