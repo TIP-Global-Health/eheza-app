efd6775c6a262d2f417df4bfeccd26ca76514ae6
Amitai Burstein <amitai@gizra.com>
2017-01-11 08:50:03 +0200

    Add patient resource

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPatients.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulPatients.
+ */
+
+/**
+ * Class HedleyRestfulPatients.
+ */
+class HedleyRestfulPatients extends RestfulEntityBaseMultipleBundles {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    return $public_fields;
+  }
+
+}
