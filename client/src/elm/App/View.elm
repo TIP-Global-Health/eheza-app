a526e8867959d6d4ed1f1e479ce1d3e4546b19e7
IshaDakota <adam@marchinglizard.net>
2017-01-14 14:31:03 -0600

    Elm draft

--- client/src/elm/App/View.elm
+++ client/src/elm/App/View.elm
@@ -21,47 +21,91 @@ view model =
             Config.View.view
 
         _ ->
+            div []
+                [ viewSidebar model
+                , div [ class "pusher" ]
+                    [ viewHeader model 
+
+                    , div [ class "ui main container" ]
+                        [ viewMainContent model
+                        ]
+                    ]
+                ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    case model.user of
+        Success user ->
             div [ class "ui inverted masthead segment" ]
                 [ div [ class "ui container" ]
                     [ div [ class "ui large inverted secondary pointing menu" ]
-                        [ a [ class "toc item" ]
-                            [ href "#" ]
-                        , a [ class "item" ]
-                            [ text "Admin" ]
-                            [ href "#" ]
-                        , a [ class "item" ]
-                            [ text "Ativities" ]
-                            [ href "#" ]
-                        , a [ class "item" ]
+                        [ a
+                            [ class "toc item"
+                            , href "#"
+                            ]
+                            [ i [ class "sidebar icon" ]
+                                []
+                            ]
+                        , a
+                            [ class "item"
+                            , onClick <| SetActivePage MyAccount
+                            ]
+                            [ text user.name ]
+                        , a
+                            [ class "item"
+                            , onClick <| SetActivePage Activities
+                            ]
+                            [ text "Activities" ]
+                        , a
+                            [ class "item"
+                            , onClick <| SetActivePage Dashboard
+                            ]
                             [ text "Dashboard" ]
-                            [ href "#" ]
                         , div [ class "right item" ]
                             [ a [ class "ui inverted button " ]
                                 [ text "Sign Out" ]
                             ]
+                        , span
+                            [ class "item"
+                            ]
+                            [ text <|
+                                if model.offline then
+                                    "Not Connected"
+                                else
+                                    "Connected"
+                            , i
+                                [ classList
+                                    [ ( "icon wifi", True )
+                                    , ( "disabled", model.offline )
+                                    ]
+                                ]
+                                []
+                            ]
                         ]
                     ]
                 ]
+
+        _ ->
+            div [] []
+
 
 viewSidebar : Model -> Html Msg
 viewSidebar model =
     case model.user of
         Success user ->
             div
-                [ class "ui visible sidebar inverted vertical menu" ]
+                [ class "ui vertical inverted sidebar menu" ]
                 [ a
                     [ class "item"
                     , onClick <| SetActivePage MyAccount
                     ]
-                    [ h4
-                        [ class "ui grey header" ]
-                        [ text user.name ]
-                    ]
+                    [ text user.name ]
                 , a
                     [ class "item"
-                    , onClick Logout
+                    , onClick <| SetActivePage Activities
                     ]
-                    [ text "Sign Out" ]
+                    [ text "Activities" ]
                 , a
                     [ class "item"
                     , onClick <| SetActivePage Dashboard
@@ -69,9 +113,9 @@ viewSidebar model =
                     [ text "Dashboard" ]
                 , a
                     [ class "item"
-                    , onClick <| SetActivePage Activities
+                    , onClick Logout
                     ]
-                    [ text "Activities" ]
+                    [ text "Sign Out" ]
                 , span
                     [ class "item"
                     ]
--- client/src/js/app.js
+++ client/src/js/app.js
@@ -43,3 +43,27 @@ Offline.on('down', function() {
 Offline.on('up', function() {
     elmApp.ports.offline.send (false);
 });
+
+$(document)
+  .ready(function() {
+
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      })
+    ;
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item')
+    ;
+
+  })
+;
