57fa7fc00ac17ecc55f7c8b427a07de4bc3d244f
Amitai Burstein <amitai@gizra.com>
2017-01-15 00:56:17 +0900

    Remove todo

--- client/src/elm/Pages/Patient/View.elm
+++ client/src/elm/Pages/Patient/View.elm
@@ -170,6 +170,5 @@ viewActivity report =
         [ a [ href "#" ] [ i [ class (report.activity.icon ++ " icon") ] [] ]
         , div [ class "content" ]
             [ a [ class "header activities__item__title" ] [ text report.activity.name ]
-            , div [ class "meta" ] [ text "@todo: Date" ]
             ]
         ]
