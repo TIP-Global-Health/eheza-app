d9db6e65b7e34f49d10f142ca5df4a868c801a23
IshaDakota <adam@marchinglizard.net>
2017-01-14 14:43:56 -0600

    remove double declaration

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -43,27 +43,3 @@ Offline.on('down', function() {
 Offline.on('up', function() {
     elmApp.ports.offline.send (false);
 });
-
-$(document)
-  .ready(function() {
-
-    // fix menu when passed
-    $('.masthead')
-      .visibility({
-        once: false,
-        onBottomPassed: function() {
-          $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function() {
-          $('.fixed.menu').transition('fade out');
-        }
-      })
-    ;
-
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
-
-  })
-;
