5eccd79ccabf8f1e8c354620fe4e600962ad3ea5
Aron Novak <aron@gizra.com>
2017-06-29 16:05:04 +0200

    initial port of the Travis scripts

---
+++ ci-scripts/docker_files/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+cd ci-scripts/docker_files
+
+if [ -z "$DOCKER_DEBUG" ]; then
+  # Regular Travis execution, failing on the first error is what we want.
+  bash pre_cache.sh
+
+  bash preparing_server.sh
+  bash preparing_client.sh
+
+  bash run_simpletest.sh
+  bash run_webdriverio.sh
+
+  bash post_cache.sh
+
+else
+  # We keep going and open a Bash shell to interactively inspect what failed or
+  # what's the status of the application.
+  bash preparing_server.sh || :
+  bash preparing_client.sh || :
+
+  bash run_simpletest.sh || :
+  bash run_webdriverio.sh || :
+
+  bash
+fi
