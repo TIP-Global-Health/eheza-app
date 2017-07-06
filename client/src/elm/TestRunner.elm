4904787f445b00ff2a8d0a01597a4f5be4d43124
Perry Birch <perrybirch@vizidrix.com>
2017-07-06 12:27:36 -0700

    test runner import order

--- client/src/elm/TestRunner.elm
+++ client/src/elm/TestRunner.elm
@@ -8,16 +8,16 @@ import Test.Runner.Node exposing (TestProgram, run)
 -- Register Test Stubs Here
 
 import App.Test exposing (all)
-import Pusher.Test exposing (all)
 import Pages.Patient.Test exposing (all)
+import Pusher.Test exposing (all)
 
 
 allTests : Test
 allTests =
     describe "All tests"
         [ App.Test.all
-        , Pusher.Test.all
         , Pages.Patient.Test.all
+        , Pusher.Test.all
         ]
 
 
