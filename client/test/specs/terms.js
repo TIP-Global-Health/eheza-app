3480a858bf5a3d11cf06feb597a678b58d11c0fc
RoySegall <roy.segall@gizra.com>
2017-07-06 13:59:15 +0300

    Commit the files.

--- client/test/specs/terms.js
+++ client/test/specs/terms.js
@@ -1,30 +1,18 @@
 var assert = require('assert');
 
-describe('login page', function() {
-    it('should allow a user to login', function() {
-        const loginForm = '.login-container .form';
-        browser.url('/#login');
+describe('taxonomies', function() {
+    it('Testing the vocabulary created', function() {
+        browser.url('http://google.com');
 
-        browser.waitForVisible(loginForm);
-        browser.setValueSafe('[name="username"]', 'admin');
-        browser.setValueSafe('[name="password"]', 'admin');
-        browser.submitForm(loginForm);
-        browser.waitForVisible('h1=Patients');
-
-        // Logout session.
-        browser.click('.masthead .right .ui.button');
-        browser.waitForVisible('[name="username"]');
+        // browser.waitForVisible(loginForm);
+        // browser.setValueSafe('[name="username"]', 'admin');
+        // browser.setValueSafe('[name="password"]', 'admin');
+        // browser.submitForm(loginForm);
+        // browser.waitForVisible('h1=Patients');
+        //
+        // // Logout session.
+        // browser.click('.masthead .right .ui.button');
+        // browser.waitForVisible('[name="username"]');
     });
 
-    it('should not allow an anonymous user with wrong credentials to login', function() {
-        const loginForm = '.login-container .form';
-        browser.url('/#login');
-
-        browser.waitForVisible(loginForm);
-        browser.setValueSafe('[name="username"]', 'wrong-name');
-        browser.setValueSafe('[name="password"]', 'wrong-pass');
-        browser.submitForm(loginForm);
-
-        browser.waitForVisible('.login-container .ui.error.message');
-    });
 });
