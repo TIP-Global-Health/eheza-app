13ed30161c26bd1c1cb2afaec2bdaaefdb1ffb26
RoySegall <roy.segall@gizra.com>
2017-07-06 08:58:22 +0300

    Merge master[skip ci]

---
+++ client/src/elm/Translate.elm
@@ -0,0 +1,196 @@
+module Translate exposing (..)
+
+
+type Language
+    = English
+
+
+allLanguages : List Language
+allLanguages =
+    [ English
+    ]
+
+
+type alias TranslationSet =
+    { english : String
+    }
+
+
+type TranslationId
+    = AccessDenied
+    | Activities
+    | ActivitiesCompleted
+    | ActivitiesToComplete
+    | Children
+    | Connected
+    | Dashboard
+    | ErrorBadUrl
+    | ErrorBadPayload
+    | ErrorBadStatus
+    | ErrorCheckLocalConfig
+    | ErrorConfigurationError
+    | ErrorNetworkError
+    | ErrorTimeout
+    | LinkToMother
+    | Login
+    | Logout
+    | Mother
+    | Mothers
+    | MyAccount
+    | NoActiveIncidents
+    | NoChildrenRegisteredInTheSystem
+    | NoPatientsFound
+    | NotConnected
+    | Page404
+    | PageNotFoundMsg
+    | Password
+    | Patients
+    | ReportRemaining Int
+    | ReloadPatient
+    | Retry
+    | SearchByName
+    | SignOut
+    | Username
+    | WelcomeUser String
+
+
+translate : Language -> TranslationId -> String
+translate lang trans =
+    let
+        translationSet =
+            case trans of
+                AccessDenied ->
+                    { english = "Access denied" }
+
+                Activities ->
+                    { english = "Activities" }
+
+                ActivitiesCompleted ->
+                    { english = "ActivitiesCompleted" }
+
+                ActivitiesToComplete ->
+                    { english = "ActivitiesToComplete" }
+
+                Children ->
+                    { english = "Children" }
+
+                Connected ->
+                    { english = "Connected" }
+
+                Dashboard ->
+                    { english = "Dashboard" }
+
+                ErrorBadUrl ->
+                    { english = "URL is not valid." }
+
+                ErrorBadPayload ->
+                    { english = "The server responded with data of an unexpected type." }
+
+                ErrorBadStatus ->
+                    { english = "The server indicated the following error:" }
+
+                ErrorCheckLocalConfig ->
+                    { english = "Check your LocalConfig.elm file and make sure you have defined the enviorement properly" }
+
+                ErrorConfigurationError ->
+                    { english = "Configuration error" }
+
+                ErrorNetworkError ->
+                    { english = "There was a network error." }
+
+                ErrorTimeout ->
+                    { english = "The network request timed out." }
+
+                LinkToMother ->
+                    { english = "Link to mother" }
+
+                Login ->
+                    { english = "Login" }
+
+                Logout ->
+                    { english = "Logout" }
+
+                Mother ->
+                    { english = "Mother" }
+
+                Mothers ->
+                    { english = "Mothers" }
+
+                MyAccount ->
+                    { english = "My Account" }
+
+                NoActiveIncidents ->
+                    { english = "No active incidents!" }
+
+                NoChildrenRegisteredInTheSystem ->
+                    { english = "No children registered in the system" }
+
+                NoPatientsFound ->
+                    { english = "No patients found" }
+
+                NotConnected ->
+                    { english = "Not Connected" }
+
+                Page404 ->
+                    { english = "404 page" }
+
+                PageNotFoundMsg ->
+                    { english = "Sorry, nothing found in this URL." }
+
+                Password ->
+                    { english = "Password" }
+
+                Patients ->
+                    { english = "Patients" }
+
+                ReportRemaining remaining ->
+                    { english = toString remaining ++ " remaning" }
+
+                ReloadPatient ->
+                    { english = "Re-load Patient" }
+
+                Retry ->
+                    { english = "Retry" }
+
+                SearchByName ->
+                    { english = "Search by Name" }
+
+                SignOut ->
+                    { english = "Sign Out" }
+
+                Username ->
+                    { english = "Username" }
+
+                WelcomeUser name ->
+                    { english = "Welcome " ++ name }
+    in
+        case lang of
+            English ->
+                .english translationSet
+
+
+languageFromString : String -> Result String Language
+languageFromString str =
+    case str of
+        "English" ->
+            Ok English
+
+        _ ->
+            Err "Not a language"
+
+
+languageFromCode : String -> Result String Language
+languageFromCode str =
+    case str of
+        "en" ->
+            Ok English
+
+        _ ->
+            Err "Not a language"
+
+
+languageToCode : Language -> String
+languageToCode lang =
+    case lang of
+        English ->
+            "en"
--- client/src/js/app.js
+++ client/src/js/app.js
@@ -7,32 +7,32 @@ elmApp.ports.accessTokenPort.subscribe(function(accessToken) {
     localStorage.setItem('accessToken', accessToken);
 });
 
-
-elmApp.ports.pusherKey.subscribe(function(appKey) {
-  var pusher = new Pusher(appKey, {
-    cluster: 'mt1'
+/**
+ * Port the 'Pusher' events names into our Elm's app.
+ */
+elmApp.ports.pusherKey.subscribe(function (appKey) {
+  var pusher = new Pusher(appKey[0], {
+    cluster: appKey[1]
   });
 
   var channelName = 'general';
 
   if (!pusher.channel(channelName)) {
-      var channel = pusher.subscribe(channelName);
+    var channel = pusher.subscribe(channelName);
 
-      var eventNames = [
-        'patient__update',
-      ];
+    var eventNames = appKey[2];
 
-      eventNames.forEach(function(eventName) {
-        channel.bind(eventName, function (data) {
-            // We wrap the data with some information which will
-            // help us dispatch it on the Elm side
-            var event = {
-                eventType: eventName,
-                data: data
-            };
-            elmApp.ports.pusherPatientMessages.send(event);
-        });
+    eventNames.forEach(function (eventName) {
+      channel.bind(eventName, function (data) {
+        // We wrap the data with some information which will
+        // help us dispatch it on the Elm side
+        var event = {
+          eventType: eventName,
+          data: data
+        };
+        elmApp.ports.pusherItemMessages.send(event);
       });
+    });
   }
 });
 
