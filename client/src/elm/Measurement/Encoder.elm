798f20749f8dbbdf055582a17ae14a066cdec860
Or David <ordavidil@gmail.com>
2017-07-16 21:34:55 +0300

    order by alphabet

--- client/src/elm/Measurement/Encoder.elm
+++ client/src/elm/Measurement/Encoder.elm
@@ -4,17 +4,17 @@ import Child.Model exposing (ChildId)
 import Json.Encode as Encoder exposing (Value, float, int)
 
 
-encodeWeight : ChildId -> Float -> Value
-encodeWeight childId weight =
-    Encoder.object <|
-        [ ( "child", Encoder.int childId )
-        , ( "weight", Encoder.float weight )
-        ]
-
-
 encodePhoto : ChildId -> Float -> Value
 encodePhoto childId weight =
     Encoder.object <|
         [ ( "child", Encoder.int childId )
         , ( "photo", Encoder.float weight )
+        ]
+
+
+encodeWeight : ChildId -> Float -> Value
+encodeWeight childId weight =
+    Encoder.object <|
+        [ ( "child", Encoder.int childId )
+        , ( "weight", Encoder.float weight )
         ]
