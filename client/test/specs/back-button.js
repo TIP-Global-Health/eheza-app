fac0b6b8958bec5e2cbbc201c9dd4e33b8a320c9
Or David <ordavidil@gmail.com>
2017-07-20 19:04:45 +0300

    Removing dots.

--- client/test/specs/back-button.js
+++ client/test/specs/back-button.js
@@ -6,19 +6,19 @@ describe('Clicking the Back Button', () => {
         browser.loginAndViewPatientsPage('aya');
     });
 
-    it('should lead to the activities page from the patients page.', () => {
+    it('should lead to the activities page from the patients page', () => {
         browser.validateCurrentPageIs('patients');
         browser.goBack();
         browser.validateCurrentPageIs('activities');
     });
 
-    it('should lead to the patients page from the activities page.', () => {
+    it('should lead to the patients page from the activities page', () => {
         browser.validateCurrentPageIs('activities');
         browser.goBack();
         browser.validateCurrentPageIs('patients');
     });
 
-    it('should lead to the patients page from a child page.', () => {
+    it('should lead to the patients page from a child page', () => {
         // Following the first patient (child) page.
         browser.element('#patients-table tbody tr td a.child').click();
         browser.validateCurrentPageIs('child');
@@ -26,7 +26,7 @@ describe('Clicking the Back Button', () => {
         browser.validateCurrentPageIs('patients');
     });
 
-    it('should lead to the patients page from a mother page.', () => {
+    it('should lead to the patients page from a mother page', () => {
         // Following the first patient (mother) page.
         browser.element('#patients-table tbody tr td a.mother').click();
         browser.validateCurrentPageIs('mother');
