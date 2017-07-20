69ac6adee9f3bc9e2303f37b7541398e4876c981
Aron Novak <aron@gizra.com>
2017-07-20 16:44:53 +0200

    test/specs/auto-measurement-form-switch.js: more explicit selectors [skip ci]

--- client/test/specs/auto-measurement-form-switch.js
+++ client/test/specs/auto-measurement-form-switch.js
@@ -26,21 +26,21 @@ describe('Auto transform between measurement forms.', () => {
     });
 
     it('Saving the Weight form should lead to the Height form.', () => {
-        browser.setValue('.form input', 4 + Math.random());
+        browser.setValue('.weight .form input', 4 + Math.random());
         browser.element('#save-form').click();
         // The help text of the Height form.
         browser.waitForVisible("p=Ask the mother to hold the baby’s head at the end of the measuring board. Move the slider to the baby’s heel and pull their leg straight.");
     });
 
     it('Saving the Height form should lead to the MUAC form.', () => {
-        browser.setValue('.form input', 56 + Math.random());
+        browser.setValue('.height .form input', 56 + Math.random());
         browser.element('#save-form').click();
         // The help text of the MUAC form.
         browser.waitForVisible("p=Make sure to measure at the center of the baby’s upper arm.");
     });
 
     it('Saving the MUAC form should lead to the Nutrition Signs form.', () => {
-        browser.setValue('.form input', 4 + Math.random());
+        browser.setValue('.muac .form input', 4 + Math.random());
         browser.element('#save-form').click();
         // The help text of the Nutrition Signs form.
         browser.waitForVisible("p=Explain to the mother how to check the malnutrition signs for their own child.");
