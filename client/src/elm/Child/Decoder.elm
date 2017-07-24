79b0ce64e630acc05b2c67e539bedb0d07b7b267
Amitai Burstein <amitai@gizra.com>
2017-07-24 14:55:31 +0300

    Assume new examination [skip ci]

--- client/src/elm/Child/Decoder.elm
+++ client/src/elm/Child/Decoder.elm
@@ -6,8 +6,10 @@ module Child.Decoder
 import Activity.Decoder exposing (decodeChildActivityDates)
 import Child.Model exposing (..)
 import EveryDictList
+import Examination.Model exposing (ExaminationStorage(New))
 import Json.Decode exposing (Decoder, andThen, dict, fail, field, int, list, map, map2, nullable, string, succeed)
 import Json.Decode.Pipeline exposing (custom, decode, hardcoded, optional, optionalAt, required)
+import RemoteData exposing (RemoteData(NotAsked))
 import Utils.Json exposing (decodeInt)
 
 
@@ -17,6 +19,9 @@ decodeChild =
         |> required "label" string
         |> optionalAt [ "avatar", "styles", "patient-photo" ] string "https://placehold.it/200x200"
         |> required "mother" (nullable decodeInt)
+        -- @todo We would issue a call to /api/examinations to populate this
         |> hardcoded EveryDictList.empty
-        |> hardcoded Nothing
+        -- We assume measurementes are for a new examination. In the future they
+        -- could be updates of existing ones.
+        |> hardcoded (Just <| New NotAsked)
         |> custom decodeChildActivityDates
