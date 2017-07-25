f06e36fb1c5f7f390054ca71605059e4aea4dd4b
amitaibu <amitai@gizra.com>
2017-07-25 11:34:01 +0300

    Add comments

--- client/src/elm/Utils/EditableWebData.elm
+++ client/src/elm/Utils/EditableWebData.elm
@@ -14,50 +14,58 @@ import Editable exposing (Editable(..))
 import RemoteData exposing (RemoteData(..), WebData)
 
 
+{-| A wrapper for `Editable`, that allows provides the means to track saving
+back to the backend via `WebData`.
+-}
 type EditableWebData a
     = EditableWebData (Editable a) (WebData ())
 
 
+{-| Creates a new `EditableWebData`.
+-}
 create : a -> EditableWebData a
 create record =
     EditableWebData (Editable.ReadOnly record) NotAsked
 
 
+{-| Pipes map to `Editable.map`
+-}
 map : (a -> a) -> EditableWebData a -> EditableWebData a
 map f (EditableWebData editable webData) =
     EditableWebData (Editable.map f editable) webData
 
 
-{-| Map Editable functions.
+{-| Maps `Editable` functions. That is, unlike `map` it keeps the `Editable`
+value wrapped.
 -}
 mapEditable : (Editable a -> Editable a) -> EditableWebData a -> EditableWebData a
 mapEditable f (EditableWebData editable webData) =
     EditableWebData (f editable) webData
 
 
-{-| Updates an `Editable` and doesn't change a `ReadOnly`.
-
-    Editable.ReadOnly "old"
-        |> Editable.update "new"  --> ReadOnly "old"
-    Editable.Editable "old" "old"
-        |> Editable.update "new"  --> Editable "old" "new"
-
+{-| Pipes update to `Editable.update`.
 -}
 update : a -> EditableWebData a -> EditableWebData a
 update value =
     map (always value)
 
 
+{-| Updates the `WebData` value.
+-}
 webDataUpdate : WebData () -> EditableWebData a -> EditableWebData a
 webDataUpdate newWebData (EditableWebData editable webData) =
     EditableWebData editable newWebData
 
 
+{-| Extracts the `Editable` value.
+-}
 value : EditableWebData a -> Editable a
 value (EditableWebData x _) =
     x
 
 
+{-| Extracts the `WebData` value.
+-}
 webDataValue : EditableWebData a -> WebData ()
 webDataValue (EditableWebData _ x) =
     x
