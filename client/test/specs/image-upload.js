121d39d2a761a27c9c86b44ae9c37db2e9b4f2ff
Aron Novak <aron@gizra.com>
2017-07-26 16:36:25 +0200

    test/specs/image-upload.js: wdio test for retake

--- client/test/specs/image-upload.js
+++ client/test/specs/image-upload.js
@@ -33,29 +33,12 @@ describe('The Photo form', () => {
     browser.checkImageBasename('.dropzone .dz-complete > div.dz-image:nth-child(1) img', 'Testfile1');
   });
 
-  it('should reset the dropzone control.', () => {
-
-    browser.login('aya');
-    browser.waitForVisible('#patients-table');
-
-    // Proceeding to the patient where activities are pending.
-    browser.url('/#patient/41');
-    browser.waitUntil(() => browser.isVisible('.ui.header.mother'));
-
-    // Follow the photo form.
-    browser.element('a=Photo').click();
-
-    // Add and then check the image.
-    browser.addTestImage('Testfile1');
-    browser.checkImageBasename('.dropzone .dz-complete > div.dz-image:nth-child(1) img', 'Testfile1');
-
-    browser.click('button*=Retake');
-    var imageCount = browser.elements('.dropzone .dz-complete > div.dz-image').length();
-
-    Console.log('imageCount', imageCount);
-
-    // assert.equal(0, imageCount);
-
+  it('should reset the dropzone control', () => {
+    const retakeButton = '.photo button.retake';
+    browser.waitForVisible(retakeButton);
+    browser.click(retakeButton);
+    const imageCount = browser.elements('.dropzone .dz-complete > div.dz-image').length();
+    assert.equal(0, imageCount, 'No images are available after Retake');
   });
 
 });
