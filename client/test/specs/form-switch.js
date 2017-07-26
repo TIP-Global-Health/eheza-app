0bd6ae890ef4a39d50060bdcf6ec337e8b3efb7e
Aron Novak <aron@gizra.com>
2017-07-26 10:25:38 +0200

    test/specs/form-switch.js: added test to preserve nutrition data inside the session

--- client/test/specs/form-switch.js
+++ client/test/specs/form-switch.js
@@ -1,12 +1,19 @@
 var assert = require('assert');
 
 describe('assesment pages', () => {
+
+   before(() => {
+        browser.loginAndViewPatientsPage('aya');
+    });
+
+   beforeEach(() => {
+       browser.visitChildWithTodoTasks();
+    });
+
+
     it('should allow a user to switch between forms', () => {
         const firstUsedTab = 'Weight';
         const secondUsedTab = 'Height';
-
-        browser.login('aya');
-        browser.visitChildWithTodoTasks();
 
         // Switching to Weight.
         browser.click('a=' + firstUsedTab);
@@ -17,5 +24,31 @@ describe('assesment pages', () => {
         browser.click('a=' + secondUsedTab);
         browser.waitForVisible('div=' + firstUsedTab + ':', 500, true);
         browser.waitForVisible('div=' + secondUsedTab + ':');
+    });
+
+    it('should preserve Nutrition Signs  data while the user is switching between forms', () => {
+        const firstUsedTab = 'Nutrition signs';
+        const secondUsedTab = 'Photo';
+
+        // Switching to Nutrition Signs, fill data.
+        browser.click('a=' + firstUsedTab);
+        browser.waitForVisible('div=' + secondUsedTab + ':', 500, true);
+        assert(!browser.isSelected('#apathy'), 'Apathy is not selected');
+        assert(!browser.isSelected('#brittle-hair'), 'Brittle Hair is not selected');
+        assert(!browser.isSelected('#edema'), 'Edema is not selected');
+
+        browser.click('#apathy');
+        browser.click('#brittle-hair');
+
+        // Switching to Photo.
+        browser.click('a=' + secondUsedTab);
+        browser.waitForVisible('div=' + firstUsedTab + ':', 500, true);
+        browser.waitForVisible('h3=' + secondUsedTab + ':');
+
+        // Switching back to Nutrition Signs, check existence of the previous selection.
+        browser.click('a=' + firstUsedTab);
+        assert(browser.isSelected('#apathy'), 'Apathy is still selected');
+        assert(browser.isSelected('#brittle-hair'), 'Brittle Hair is still selected');
+        assert(!browser.isSelected('#edema'), 'Edema is not selected');
     });
 });
