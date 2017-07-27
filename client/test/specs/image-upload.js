c53cc03564d34fdeea8215b2a96d9a04b3d110ba
Aron Novak <aron@gizra.com>
2017-07-27 09:46:49 +0200

    test/specs/image-upload.js: simplify the test of the Retake button

--- client/test/specs/image-upload.js
+++ client/test/specs/image-upload.js
@@ -21,24 +21,21 @@ describe('The Photo form', () => {
   });
 
   it('should not allow to Save without an image', () => {
-    // The Save button is enabled.
+    // The Save button is disabled.
     const classesBefore = browser.getAttribute('#save-form', 'class');
     assert.notEqual(classesBefore.indexOf('disabled'), -1, 'The Save button is disabled');
+  });
 
+  it('should not allow to Retake without an image', () => {
+    // The Retake button is disabled.
+    const classesBefore = browser.getAttribute('button.retake', 'class');
+    assert.notEqual(classesBefore.indexOf('disabled'), -1, 'The Retake button is disabled');
   });
 
   it('should allow to upload an image to the dropzone', () => {
     // Add and then check the image.
     browser.addTestImage('Testfile1');
     browser.checkImageBasename('.dropzone .dz-complete > div.dz-image:nth-child(1) img', 'Testfile1');
-  });
-
-  it('should reset the dropzone control', () => {
-    const retakeButton = '.photo button.retake';
-    browser.waitForVisible(retakeButton);
-    browser.click(retakeButton);
-    const imageCount = browser.elements('.dropzone .dz-complete > div.dz-image').length();
-    assert.equal(0, imageCount, 'No images are available after Retake');
   });
 
 });
