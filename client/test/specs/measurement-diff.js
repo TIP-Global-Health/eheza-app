c6334962408ef72b13fe97e290da45bfb97df309
anatoly vaitsman <anatoly@gizra.com>
2017-08-28 19:22:23 +0300

    Reformat WDIO test to match new markup

--- client/test/specs/measurement-diff.js
+++ client/test/specs/measurement-diff.js
@@ -9,41 +9,31 @@ describe('when updating a measurement form', function() {
    *   The new value to set.
    */
   var adjustFormValue = function (value) {
-    browser.setValue('input[type="number"]', value);
+    browser.setValue('input[type="text"]', value);
   };
 
   /**
    * Waiting for the up arrow to be shown.
    */
   var waitForGainedIndication = function () {
-    browser.waitForVisible('.label-up .icon-up');
+    browser.waitForVisible('.label-with-icon .icon-up');
   };
 
   /**
    * Waiting for the down arrow to be shown.
    */
   var waitForLostIndication = function () {
-    browser.waitForVisible('.label-down .icon-down');
+    browser.waitForVisible('.label-with-icon .icon-down');
   };
 
   /**
-   * Get the indication message of the 'gained' value.
+   * Get difference indication message.
    *
    * @returns {String|String[]}
    *   The indication message.
    */
-  var getDiffFromGainedIndication = function () {
-    return browser.getText('.label-up');
-  };
-
-  /**
-   * Get the indication message of the 'lost' value.
-   *
-   * @returns {String|String[]}
-   *   The indication message.
-   */
-  var getDiffFromLostIndication = function () {
-    return browser.getText('.label-down');
+  var getDiffFromIndication = function () {
+    return browser.getText('.label-with-icon');
   };
 
   before(() => {
@@ -60,14 +50,14 @@ describe('when updating a measurement form', function() {
 
     adjustFormValue(50);
     waitForGainedIndication();
-    const result = getDiffFromGainedIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '46 kg', 'Indication for the gained weight is incorrect.');
   })
 
   it('should display an indication when weight is lost', () => {
     adjustFormValue(1);
     waitForLostIndication();
-    const result = getDiffFromLostIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '3 kg', 'Indication for the lost weight is incorrect.');
   })
 
@@ -80,14 +70,14 @@ describe('when updating a measurement form', function() {
 
     adjustFormValue(100);
     waitForGainedIndication();
-    const result = getDiffFromGainedIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '50 cm', 'Indication for the gained height is incorrect.');
   })
 
   it('should display an indication when height is lost', () => {
     adjustFormValue(10);
     waitForLostIndication();
-    const result = getDiffFromLostIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '40 cm', 'Indication for the lost height is incorrect.');
   })
 
@@ -100,14 +90,14 @@ describe('when updating a measurement form', function() {
 
     adjustFormValue(50);
     waitForGainedIndication();
-    const result = getDiffFromGainedIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '37 cm', 'Indication for the gained MUAC is incorrect.');
   })
 
   it('should display an indication when MUAC is lost', () => {
     adjustFormValue(5);
     waitForLostIndication();
-    const result = getDiffFromLostIndication();
+    const result = getDiffFromIndication();
     assert.equal(result, '8 cm', 'Indication for the lost MUAC is incorrect.');
   })
 
