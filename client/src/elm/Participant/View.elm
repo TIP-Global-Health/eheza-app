25613667b22a3a8283c3d3cdc07c4cab3580142d
Aron Novak <aron@gizra.com>
2017-08-28 11:07:24 +0200

    getParticipantAge: initial implementation [skip ci]

--- client/src/elm/Participant/View.elm
+++ client/src/elm/Participant/View.elm
@@ -5,16 +5,30 @@ module Participant.View
         , viewParticipantTypeFilter
         )
 
+import Date exposing (Date)
+import Date.Extra.Period
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, onClick, onInput, onWithOptions)
-import Participant.Model exposing (AgeDay, Participant, ParticipantTypeFilter(..))
+import Participant.Model exposing (AgeDay, Participant, ParticipantType(..), ParticipantTypeFilter(..))
 import Translate as Trans exposing (translate, Language)
 
 
-getParticipantAge : Participant -> AgeDay
-getParticipantAge participant =
-    Participant.Model.AgeDay 10
+getParticipantAge : Participant -> Date -> AgeDay
+getParticipantAge participant now =
+    let
+        birthDate =
+            case participant.info of
+                ParticipantChild child ->
+                    child.birthDate
+
+                ParticipantMother mother ->
+                    mother.birthDate
+
+        diff =
+            Date.Extra.Period.diff birthDate now
+    in
+        Participant.Model.AgeDay diff.day
 
 
 renderParticipantAge : Participant -> String
