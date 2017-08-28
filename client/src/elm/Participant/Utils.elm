f5b7033a72fdb7e9136b9368fd2baccb92293df7
Aron Novak <aron@gizra.com>
2017-08-28 11:27:57 +0200

    Participant/Utils.elm: moved the utils func there [skip ci]

--- client/src/elm/Participant/Utils.elm
+++ client/src/elm/Participant/Utils.elm
@@ -1,11 +1,17 @@
 module Participant.Utils
     exposing
-        ( getParticipantAvatarThumb
+        ( getParticipantAge
+        , getParticipantAvatarThumb
         , getParticipantName
         , getParticipantTypeAsString
+        , renderParticipantAge
         )
 
-import Participant.Model exposing (Participant, ParticipantType(..))
+import Date exposing (Date)
+import Date.Extra.Duration
+import Date.Extra.Period
+import Participant.Model exposing (AgeDay, Participant, ParticipantType(..))
+import Translate as Trans exposing (translate, Language)
 
 
 getParticipantAvatarThumb : Participant -> String
@@ -36,3 +42,53 @@ getParticipantTypeAsString participant =
 
         ParticipantMother mother ->
             "mother"
+
+
+{-| Calculates the age of a participant.
+    To get current time, see App/Model.currentDate
+-}
+getParticipantAge : Participant -> Date -> AgeDay
+getParticipantAge participant now =
+    let
+        birthDate =
+            case participant.info of
+                ParticipantChild child ->
+                    child.birthDate
+
+                ParticipantMother mother ->
+                    mother.birthDate
+
+        diff =
+            Date.Extra.Period.diff birthDate now
+    in
+        Participant.Model.AgeDay diff.day
+
+
+renderParticipantAge : Language -> Participant -> Date -> String
+renderParticipantAge language participant now =
+    let
+        birthDate =
+            case participant.info of
+                ParticipantChild child ->
+                    child.birthDate
+
+                ParticipantMother mother ->
+                    mother.birthDate
+
+        diff =
+            Date.Extra.Duration.diff birthDate now
+
+        days =
+            diff.day
+
+        months =
+            diff.month
+    in
+        if (months == 0) then
+            translate language <| Trans.AgeDays days
+        else if (months == 1) then
+            translate language <| Trans.AgeSingleMonth months days
+        else if (days == 1) then
+            translate language <| Trans.AgeSingleDay months days
+        else
+            translate language <| Trans.Age months days
--- client/src/elm/Participant/View.elm
+++ client/src/elm/Participant/View.elm
@@ -1,70 +1,13 @@
 module Participant.View
     exposing
-        ( getParticipantAge
-        , renderParticipantAge
-        , viewParticipantTypeFilter
+        ( viewParticipantTypeFilter
         )
 
-import Date exposing (Date)
-import Date.Extra.Duration
-import Date.Extra.Period
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, onClick, onInput, onWithOptions)
-import Participant.Model exposing (AgeDay, Participant, ParticipantType(..), ParticipantTypeFilter(..))
+import Participant.Model exposing (AgeDay, Participant, ParticipantTypeFilter(..))
 import Translate as Trans exposing (translate, Language)
-
-
-{- Calculates the age of a participant.
-   To get current time, see App/Model.currentDate
--}
-
-
-getParticipantAge : Participant -> Date -> AgeDay
-getParticipantAge participant now =
-    let
-        birthDate =
-            case participant.info of
-                ParticipantChild child ->
-                    child.birthDate
-
-                ParticipantMother mother ->
-                    mother.birthDate
-
-        diff =
-            Date.Extra.Period.diff birthDate now
-    in
-        Participant.Model.AgeDay diff.day
-
-
-renderParticipantAge : Participant -> Date -> String
-renderParticipantAge participant now =
-    let
-        birthDate =
-            case participant.info of
-                ParticipantChild child ->
-                    child.birthDate
-
-                ParticipantMother mother ->
-                    mother.birthDate
-
-        diff =
-            Date.Extra.Duration.diff birthDate now
-
-        days =
-            diff.day
-
-        months =
-            diff.month
-    in
-        if (months == 0) then
-            translate language Trans.AgeDays days
-        else if (months == 1) then
-            translate language Trans.AgeSingleMonth months days
-        else if (days == 1) then
-            translate language Trans.AgeSingleDay months days
-        else
-            translate language Trans.Age months days
 
 
 viewParticipantTypeFilter : Language -> (String -> msg) -> ParticipantTypeFilter -> Html msg
