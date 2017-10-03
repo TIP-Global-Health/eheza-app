e19c6532ab7efcc3a15c405164dd1b1e99670d36
Ryan Rempel <rgrempel@gmail.com>
2017-10-03 15:58:01 -0500

    Add mothers and children to offline_session.

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulOfflineSessions.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulOfflineSessions.class.php
@@ -42,6 +42,13 @@ class HedleyRestfulOfflineSessions extends HedleyRestfulEntityBaseNode {
       ],
     ];
 
+    $public_fields['participants'] = [
+      'property' => 'nid',
+      'process_callbacks' => [
+        [$this, 'getParticipantData'],
+      ],
+    ];
+
     return $public_fields;
   }
 
@@ -62,6 +69,71 @@ class HedleyRestfulOfflineSessions extends HedleyRestfulEntityBaseNode {
     return [
       'value' => $date['value'] ? hedley_restful_timestamp_only_date($date['value']) : NULL,
       'value2' => $date['value2'] ? hedley_restful_timestamp_only_date($date['value2']) : NULL,
+    ];
+  }
+
+  /**
+   * Return participant data.
+   *
+   * @param int $nid
+   *   The session node ID.
+   *
+   * @return array
+   *   Array with the RESTful output.
+   */
+  public function getParticipantData($nid) {
+    $wrapper = entity_metadata_wrapper('node', $nid);
+
+    // First, let us get all the mothers assigned to this clinic.
+    $mothers = (new EntityFieldQuery())
+      ->entityCondition('entity_type', 'node')
+      ->entityCondition('bundle', 'mother')
+      ->fieldCondition('field_clinic', 'target_id', $wrapper->field_clinic->getIdentifier())
+      ->propertyCondition('status', NODE_PUBLISHED)
+      ->range(0, 1000)
+      ->execute();
+
+    $mother_ids = empty($mothers['node']) ? [] : array_keys($mothers['node']);
+
+    // Then, get all the children of all the mothers. It's more
+    // efficient to do this in one query than many.
+    $children = (new EntityFieldQuery())
+      ->entityCondition('entity_type', 'node')
+      ->entityCondition('bundle', 'child')
+      ->fieldCondition('field_mother', 'target_id', $mother_ids, "IN")
+      ->propertyCondition('status', NODE_PUBLISHED)
+      ->range(0, 2000)
+      ->execute();
+
+    $child_ids = empty($children['node']) ? [] : array_keys($children['node']);
+
+    // Now, provide the usual output, since that's easiest. We'll
+    // stitch together the structures we want on the client.
+    $mother_output = [];
+    node_load_multiple($mother_ids);
+
+    foreach ($mother_ids as $nid) {
+      $handler = restful_get_restful_handler('mothers');
+      $handler->setAccount($this->getAccount());
+      $response = $handler->get($nid);
+
+      $mother_output[] = $response[0];
+    }
+
+    $child_output = [];
+    node_load_multiple($child_ids);
+
+    foreach ($child_ids as $nid) {
+      $handler = restful_get_restful_handler('children');
+      $handler->setAccount($this->getAccount());
+      $response = $handler->get($nid);
+
+      $child_output[] = $response[0];
+    }
+
+    return [
+      "mothers" => $mother_output,
+      "children" => $child_output,
     ];
   }
 
