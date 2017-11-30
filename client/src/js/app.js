65efd85e425b49fb4b73b89b89e232d92754f373
Ryan Rempel <rgrempel@gmail.com>
2017-11-30 16:32:01 -0600

    Remove some Pusher code for now, until we're using it.

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -81,35 +81,6 @@ elmApp.ports.deleteEditableSession.subscribe(function () {
     localStorage.setItem('edits', "");
 });
 
-/**
- * Port the 'Pusher' events names into our Elm's app.
- */
-elmApp.ports.pusherKey.subscribe(function(appKey) {
-    var pusher = new Pusher(appKey[0], {
-        cluster: appKey[1]
-    });
-
-    var channelName = 'general';
-
-    if (!pusher.channel(channelName)) {
-        var channel = pusher.subscribe(channelName);
-
-        var eventNames = appKey[2];
-
-        eventNames.forEach(function(eventName) {
-            channel.bind(eventName, function(data) {
-                // We wrap the data with some information which will
-                // help us dispatch it on the Elm side
-                var event = {
-                    eventType: eventName,
-                    data: data
-                };
-                elmApp.ports.pusherItemMessages.send(event);
-            });
-        });
-    }
-});
-
 Offline.on('down', function() {
     elmApp.ports.offline.send(true);
 });
