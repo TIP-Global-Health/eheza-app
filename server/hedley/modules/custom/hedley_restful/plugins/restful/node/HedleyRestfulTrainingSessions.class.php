785337448f38713eca6a6c1af7abaa41994401c7
nedSaf <nader@gizra.com>
2018-07-02 18:01:50 +0300

    Small fixes for the training sessions creation and add docs. [skip ci]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulTrainingSessions.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulTrainingSessions.class.php
@@ -25,7 +25,12 @@ class HedleyRestfulTrainingSessions extends HedleyRestfulOfflineSessions {
   }
 
   /**
-   * @todo: Add docs.
+   * Create training sessions for all existing clinics.
+   *
+   * This would be used by administrators on the sandbox website for creating
+   * training sessions for all the existing clinics which are scheduled for the
+   * day of creating them, meaning they will be open for the same day the admin
+   * clicks on the "Create all training sessions" button.
    *
    * @throws \RestfulForbiddenException
    */
@@ -37,9 +42,10 @@ class HedleyRestfulTrainingSessions extends HedleyRestfulOfflineSessions {
 
     // Get all clinics.
     $all_clinics = $this->getClinicData();
+    $scheduled_date = date('Y-m-d', REQUEST_TIME);
 
     foreach ($all_clinics as $clinic) {
-      if (hedley_schedule_clinic_has_sessions($clinic['id'], REQUEST_TIME)) {
+      if (hedley_schedule_clinic_has_sessions($clinic['id'], $scheduled_date)) {
         // Clinic has an active session for today, no need to create a session
         // for it.
         continue;
@@ -49,7 +55,7 @@ class HedleyRestfulTrainingSessions extends HedleyRestfulOfflineSessions {
         'clinic' => $clinic['id'],
         'training' => TRUE,
         'scheduled_date' => [
-          'value' => REQUEST_TIME,
+          'value' => $scheduled_date,
         ],
       ];
 
@@ -64,7 +70,13 @@ class HedleyRestfulTrainingSessions extends HedleyRestfulOfflineSessions {
     }
   }
 
+  /**
+   * Check if the current user has permissions to administer nodes.
+   *
+   * @return bool
+   *   Boolean TRUE if the user has the requested permission.
+   */
   protected function checkTrainingSessionsAccess() {
-    return user_access('administer nodes');
+    return user_access('administer nodes', $this->getAccount());
   }
 }
