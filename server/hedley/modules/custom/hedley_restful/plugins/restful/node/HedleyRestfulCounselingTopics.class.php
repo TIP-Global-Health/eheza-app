599dcde15ec307ee5f3b212a1bfbedc953194e4c
Ryan Rempel <rgrempel@gmail.com>
2018-08-09 17:20:12 -0500

    Use the "label" as the English text for the "Counseling Topic"

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulCounselingTopics.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulCounselingTopics.class.php
@@ -16,17 +16,11 @@ class HedleyRestfulCounselingTopics extends HedleyRestfulEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
-    $public_fields['english_title'] = [
-      'property' => 'field_english_title',
-    ];
-
+    // The english is in the "label" field
     $public_fields['kinyarwanda_title'] = [
       'property' => 'field_kinyarwanda_title',
     ];
 
-    // No need for the label field because we are using translations fields for
-    // the title.
-    unset($public_fields['label']);
     unset($public_fields['self']);
     unset($public_fields['created']);
 
