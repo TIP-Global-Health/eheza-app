002e805c92e26b8ec0ae78b89ae9eea0fe151837
Ryan Rempel <rgrempel@gmail.com>
2018-09-24 10:07:42 -0500

    Basic XML migration for participant forms.

---
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateParticipantForms.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @file
+ * Contains \HedleyMigrateParticipantForms.
+ */
+
+/**
+ * Class HedleyMigrateParticipantForms.
+ */
+class HedleyMigrateParticipantForms extends XMLMigration {
+
+  /**
+   * HedleyMigrateParticipantForms constructor.
+   */
+  public function __construct($arguments) {
+    parent::__construct($arguments);
+
+    $this->description = t('Migrate participant form entities.');
+
+    $fields = array(
+      'title' => t('Title'),
+      'kinyarwanda_title' => t('Kinyarwanda title'),
+      'body' => t('Body'),
+      'kinyarwanda_body' => t('Kinyarwanda body'),
+    );
+
+    $source_file = $this->getMigrateDirectory() . '/xml/' . 'participant_form.xml';
+
+    $item_xpath = '/forms/form';
+    $item_ID_xpath = '@id';
+
+    $items_class = new MigrateItemsXML($source_file, $item_xpath, $item_ID_xpath);
+    $this->source = new MigrateSourceMultiItems($items_class, $fields);
+
+    $this->destination = new MigrateDestinationNode('participant_form');
+
+    $key = [
+      'id' => [
+        'type' => 'varchar',
+        'length' => 255,
+        'not null' => TRUE,
+      ],
+    ];
+
+    $this->map = new MigrateSQLMap(
+      $this->machineName,
+      $key,
+      MigrateDestinationNode::getKeySchema()
+    );
+
+    $this
+      ->addFieldMapping('uid', 'author')
+      ->defaultValue(1);
+
+    $this
+      ->addFieldMapping('title', 'title')
+      ->xpath('title');
+
+    $this
+      ->addFieldMapping('field_kinyarwanda_title', 'kinyarwanda_title')
+      ->xpath('kinyarwanda_title');
+
+    $this
+      ->addFieldMapping('body', 'body')
+      ->xpath('body');
+
+    $this
+      ->addFieldMapping('field_kinyarwanda_body', 'kinyarwanda_body')
+      ->xpath('kinyarwanda_body');
+
+  }
+
+  /**
+   * Returns the migrate directory.
+   *
+   * @return string
+   *   The migrate directory.
+   */
+  protected function getMigrateDirectory() {
+    return variable_get('hedley_migrate_directory', FALSE) ? variable_get('hedley_migrate_directory') : drupal_get_path('module', 'hedley_migrate');
+  }
+
+}
