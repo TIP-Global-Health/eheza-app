ac0c5c5f8c5da02f06fb6ccfcdd5b6db17556de4
Ryan Rempel <rgrempel@gmail.com>
2018-10-23 16:57:44 -0500

    Migrate some missing data from Excel spreadsheets.

---
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateMissingHeights.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \HedleyMigrateMissingHeights.
+ */
+
+/**
+ * Class HedleyMigrateMissingHeights.
+ */
+class HedleyMigrateMissingHeights extends HedleyMigrateMissing {
+
+  protected $bundle = 'height';
+
+  protected $simpleMappings = [
+    'field_height',
+  ];
+
+}
---
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateMissingMuacs.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \HedleyMigrateMissingMuacs.
+ */
+
+/**
+ * Class HedleyMigrateMissingMuacs.
+ */
+class HedleyMigrateMissingMuacs extends HedleyMigrateMissing {
+
+  protected $bundle = 'muac';
+
+  protected $simpleMappings = [
+    'field_muac',
+  ];
+
+}
