81c445b71b2483fe11e7b0fd08784cac1791f027
Ryan Rempel <rgrempel@gmail.com>
2018-12-18 15:34:49 -0600

    Revert "Try telling webdriver to access the app on localhost."
    This reverts commit 8f2746c8341b9defbca822a8505018fc9956538d.

--- client/wdio.conf.travis.js
+++ client/wdio.conf.travis.js
@@ -73,7 +73,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://server.local:3000',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 20000,
