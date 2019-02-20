b65f8133f47b08ae2c6b182227abb01ff9ab9196
Ryan Rempel <rgrempel@gmail.com>
2019-02-20 11:05:24 -0600

    Remove various unused phrases.

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -138,7 +138,6 @@ type TranslationId
     | Baby
     | BabyName String
     | BackendError
-    | BeginHealthAssessment
     | Born
     | Cancel
     | CentimeterShorthand
@@ -165,7 +164,6 @@ type TranslationId
     | CreateTrainingSessions
     | DeleteTrainingSessions
     | Dashboard
-    | DataIsNowSaved
     | DateOfLastAssessment
     | Day
     | Days
@@ -173,25 +171,16 @@ type TranslationId
     | Device
     | DeviceNotAuthorized
     | DeviceStatus
-    | DownloadHealthAssessment
-    | DownloadSession1
-    | DownloadSession2
-    | DownloadSuccessful
-    | DownloadingSession1
-    | DownloadingSession2
     | DropzoneDefaultMessage
     | EndSession
     | EnterPairingCode
     | ErrorCheckLocalConfig
     | ErrorConfigurationError
-    | ErrorFetchingCachedSessionTitle
-    | ErrorFetchingCachedSessionMessage
     | FamilyPlanningSignLabel FamilyPlanningSign
     | Fetch
     | FilterByName
     | FormError (ErrorValue ValidationError)
     | FormField String
-    | FutureSessions
     | Gender Gender
     | GoHome
     | HttpError Http.Error
@@ -213,7 +202,6 @@ type TranslationId
     | Mothers
     | MuacIndication MuacIndication
     | MyAccount
-    | NoActiveIncidents
     | NoActivitiesCompleted
     | NoActivitiesCompletedForThisParticipant
     | NoActivitiesPending
@@ -223,7 +211,6 @@ type TranslationId
     | NoParticipantsPendingForThisActivity
     | NoParticipantsCompleted
     | NoParticipantsCompletedForThisActivity
-    | NoCachedSession
     | NoChildrenRegisteredInTheSystem
     | NoParticipantsFound
     | NotAvailable
@@ -242,16 +229,12 @@ type TranslationId
     | PlaceholderEnterHeight
     | PlaceholderEnterMUAC
     | PlaceholderEnterWeight
-    | PlaceholderTextGroupDate
-    | PlaceholderTextJoined
     | PleaseSelectClinic
     | PreviousFloatMeasurement Float
     | ProgressReport
-    | ReadyToBeginSession
     | ReportAge String
     | ReportDOB String
     | ReportRemaining Int
-    | ReloadParticipant
     | RecentAndUpcomingGroupSessions
     | ReportCompleted { pending : Int, completed : Int }
     | ResolveMonth Month
@@ -280,7 +263,6 @@ type TranslationId
     | ServiceWorkerStatus
     | SessionClosed
     | SessionClosed2 SessionId
-    | SessionInProgress
     | SessionLoading SessionId
     | SessionUnauthorized
     | SessionUnauthorized2
@@ -297,27 +279,19 @@ type TranslationId
     | TakenCareOfBy
     | ThisActionCannotBeUndone
     | ThisClinicHasNoMothers
-    | TitleHealthAssessment
     | Training
     | TrainingSessionCreateSuccessMessage
     | TrainingSessionDeleteSuccessMessage
     | TrySyncing
     | UbudeheLabel
-    | UnableToDownload
-    | UnableToUpload
     | Unknown
     | Update
     | UpdateError
-    | UploadHealthAssessment
-    | UploadingSession1
-    | UploadingSession2
-    | UploadSuccessful
     | ValidationErrors
     | Version
     | ViewProgressReport
     | WelcomeUser String
     | YouAreNotAnAdmin
-    | YouHaveACompletedSession
     | YourSessionHasBeenSaved
     | ZScoreHeightForAge
     | ZScoreMuacForAge
@@ -612,11 +586,6 @@ translationSet trans =
             , kinyarwanda = Just "Kuvuka/ itariki y'amavuko"
             }
 
-        BeginHealthAssessment ->
-            { english = "Begin Health Assessment"
-            , kinyarwanda = Just "Gutangira igikorwa cy'ipima"
-            }
-
         Cancel ->
             { english = "Cancel"
             , kinyarwanda = Just "Guhagarika"
@@ -802,11 +771,6 @@ translationSet trans =
             , kinyarwanda = Just "Tabeau de bord"
             }
 
-        DataIsNowSaved ->
-            { english = "Data is now saved on the server."
-            , kinyarwanda = Just "Amakuru ubu abitswe kri seriveri."
-            }
-
         DateOfLastAssessment ->
             { english = "Date of last Assessment"
             , kinyarwanda = Just "Amakuru y'ipimwa ry'ubushize"
@@ -847,39 +811,9 @@ translationSet trans =
             , kinyarwanda = Nothing
             }
 
-        DownloadHealthAssessment ->
-            { english = "Download Health Assessment"
-            , kinyarwanda = Just "Gukurura Igikorwa cy’ipima"
-            }
-
-        DownloadSuccessful ->
-            { english = "Download Successful"
-            , kinyarwanda = Just "Gukurura Igikorwa cy’ipima byagenze neza"
-            }
-
-        DownloadingSession1 ->
-            { english = "Downloading…"
-            , kinyarwanda = Just "Uri gukurura Igikorwa cy’ipima (gukurura amakuru y'ipima)"
-            }
-
-        DownloadingSession2 ->
-            { english = "Downloading may take a few minutes, or a few hours. Do not leave this page while data is downloading."
-            , kinyarwanda = Just "Gukurura Igikorwa cy’ipima bishobora gutwara iminota mike cg amasaha make. Ub uretse gufunga iyi paji mu gihe ugikurura amakuru."
-            }
-
         DropzoneDefaultMessage ->
             { english = "Touch here to take a photo, or drop a photo file here."
             , kinyarwanda = Just "Kanda hano niba ushaka gufotora cg ukure ifoto mu bubiko hano."
-            }
-
-        DownloadSession1 ->
-            { english = "You have no sessions loaded to this device. Your next session will be available for download the day before it is scheduled to begin."
-            , kinyarwanda = Just "Nta bikirwa ry'ipimwa byinjijwe kuri tablet, ibikorwa by'ipimwa bikurikira bazaboneka kuba byakurwa kuri internet umunsi ubanziriza ipima. "
-            }
-
-        DownloadSession2 ->
-            { english = "You must be connected to the internet to download a session."
-            , kinyarwanda = Just "Ugomba gukoresha internet (murandasi) kugirango ubone amakuru y'ipima."
             }
 
         EndSession ->
@@ -910,21 +844,6 @@ translationSet trans =
         ErrorConfigurationError ->
             { english = "Configuration error"
             , kinyarwanda = Just "Ikosa mu igena miterere"
-            }
-
-        ErrorFetchingCachedSessionTitle ->
-            { english = "Error Loading Cached Session"
-            , kinyarwanda = Nothing
-            }
-
-        ErrorFetchingCachedSessionMessage ->
-            { english = """
-                There was an error loading the session data cached on this
-                device. An error report has been sent (or will be sent when the
-                device is online). Contact the Ihangane project for further
-                instructions.
-                """
-            , kinyarwanda = Nothing
             }
 
         FamilyPlanningSignLabel sign ->
@@ -979,11 +898,6 @@ translationSet trans =
 
         FormField field ->
             translateFormField field
-
-        FutureSessions ->
-            { english = "Future Sessions"
-            , kinyarwanda = Nothing
-            }
 
         Gender gender ->
             case gender of
@@ -1145,11 +1059,6 @@ translationSet trans =
             , kinyarwanda = Just "Konti yanjye"
             }
 
-        NoActiveIncidents ->
-            { english = "No active incidents!"
-            , kinyarwanda = Nothing
-            }
-
         NoActivitiesCompleted ->
             { english = "No activities are entirely completed for the attending participants."
             , kinyarwanda = Just "Nta gikorwa cyarangiye cyose kubitabiriye."
@@ -1193,11 +1102,6 @@ translationSet trans =
         NoParticipantsPendingForThisActivity ->
             { english = "All attending participants have completed this activitity."
             , kinyarwanda = Just "Ababje bose barangirijwe."
-            }
-
-        NoCachedSession ->
-            { english = "No session was found on this device."
-            , kinyarwanda = Nothing
             }
 
         NoChildrenRegisteredInTheSystem ->
@@ -1296,16 +1200,6 @@ translationSet trans =
             , kinyarwanda = Just "Andika ibiro hano…"
             }
 
-        PlaceholderTextGroupDate ->
-            { english = "Group Date"
-            , kinyarwanda = Just "Itariki y'itsinda"
-            }
-
-        PlaceholderTextJoined ->
-            { english = "Joined in June 2017"
-            , kinyarwanda = Just "Yinjiye muri kamena 2017"
-            }
-
         PleaseSelectClinic ->
             { english = "Please select the relevant clinic for the new session"
             , kinyarwanda = Nothing
@@ -1321,11 +1215,6 @@ translationSet trans =
             , kinyarwanda = Just "Raporo igaragaza imikurire y'umwana"
             }
 
-        ReadyToBeginSession ->
-            { english = "You are now ready to begin your session."
-            , kinyarwanda = Just "Ubu ushobora gutangira ibikorwa byawe."
-            }
-
         ReportAge age ->
             { english = "Age: " ++ age
             , kinyarwanda = Just <| "Imyaka: " ++ age
@@ -1339,11 +1228,6 @@ translationSet trans =
         ReportRemaining remaining ->
             { english = toString remaining ++ " remaning"
             , kinyarwanda = Just <| toString remaining ++ " iyibutswa rya raporo"
-            }
-
-        ReloadParticipant ->
-            { english = "Re-load Participant"
-            , kinyarwanda = Just "Ishakisha ryabaritabira"
             }
 
         RecentAndUpcomingGroupSessions ->
@@ -1496,11 +1380,6 @@ translationSet trans =
             , kinyarwanda = Nothing
             }
 
-        SessionInProgress ->
-            { english = "A health assessment is already in progress for another clinic."
-            , kinyarwanda = Just "Hari igikorwa cy’ipima kiri gukorwa mu kindi kigo nderabuzima."
-            }
-
         SessionUnauthorized ->
             { english = "Session unauthorized"
             , kinyarwanda = Nothing
@@ -1508,8 +1387,9 @@ translationSet trans =
 
         SessionUnauthorized2 ->
             { english =
-                """A health assessment is in progress on this device, but you are not authorized to view it.
-        Please contact the Ihangane project for further instructions."""
+                """You are not authorized to view this health assessment.
+                Please contact the Ihangane project for further
+                instructions."""
             , kinyarwanda = Nothing
             }
 
@@ -1568,11 +1448,6 @@ translationSet trans =
             , kinyarwanda = Nothing
             }
 
-        TitleHealthAssessment ->
-            { english = "2017 July Health Assessment"
-            , kinyarwanda = Just "Igikorwa kipima ,kamena2017"
-            }
-
         Training ->
             { english = "Training"
             , kinyarwanda = Nothing
@@ -1598,16 +1473,6 @@ translationSet trans =
             , kinyarwanda = Nothing
             }
 
-        UnableToDownload ->
-            { english = "Unable to Download"
-            , kinyarwanda = Just "ntibishoboka gukurura"
-            }
-
-        UnableToUpload ->
-            { english = "Unable to Upload"
-            , kinyarwanda = Just "Kwohereza ntibikunda"
-            }
-
         Unknown ->
             { english = "Unknown"
             , kinyarwanda = Nothing
@@ -1621,26 +1486,6 @@ translationSet trans =
         UpdateError ->
             { english = "Update Error"
             , kinyarwanda = Just "ikosa mwivugurura"
-            }
-
-        UploadHealthAssessment ->
-            { english = "Upload Health Assessment"
-            , kinyarwanda = Just "Kwohereza Igikorwa cy’ipima"
-            }
-
-        UploadingSession1 ->
-            { english = "Uploading…"
-            , kinyarwanda = Just "Kohereza"
-            }
-
-        UploadingSession2 ->
-            { english = "Uploading may take a few minutes, or a few hours. Do not leave this page while data is uploading."
-            , kinyarwanda = Just "Kohereza igikorwa bishobora gufata iminota mike cyangwa amasaha make. Wifunga iyi paji mugihe iki gikorwa kitararangira."
-            }
-
-        UploadSuccessful ->
-            { english = "Upload Successful"
-            , kinyarwanda = Just "Kwohereza byagenze neza"
             }
 
         ValidationErrors ->
@@ -1667,11 +1512,6 @@ translationSet trans =
         YouAreNotAnAdmin ->
             { english = "You are not logged in as an Administrator."
             , kinyarwanda = Nothing
-            }
-
-        YouHaveACompletedSession ->
-            { english = "You have a completed session that needs to be uploaded. Please connect to the internet and upload this session within 48 hours."
-            , kinyarwanda = Just "Ufite igikorwa cyarangiye ukeneye kohereza. Jya kuri intereneti ucyohereze bitarenze  amasaha 48."
             }
 
         YourSessionHasBeenSaved ->
