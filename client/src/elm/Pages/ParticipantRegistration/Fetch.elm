0834a73d784194934ebe09d046fe7ac61508c7d0
Ryan Rempel <rgrempel@gmail.com>
2019-03-09 15:47:08 -0600

    Fetch some additional data the view needs.

--- client/src/elm/Pages/ParticipantRegistration/Fetch.elm
+++ client/src/elm/Pages/ParticipantRegistration/Fetch.elm
@@ -2,47 +2,68 @@ module Pages.ParticipantRegistration.Fetch exposing (fetch)
 
 import Backend.Model exposing (ModelIndexedDb, MsgIndexedDb(..))
 import EveryDict
+import Maybe.Extra
 import Pages.ParticipantRegistration.Model exposing (..)
-import Participant.Model exposing (ParticipantId(..))
+import Participant.Model exposing (ParticipantId(..), ParticipantType(..))
 import RemoteData exposing (RemoteData(..))
 
 
 fetch : ModelIndexedDb -> Model -> List MsgIndexedDb
 fetch db model =
-    case model.registrationPhase of
-        ParticipantSearch _ ->
-            let
-                trimmed =
-                    model.submittedSearch
-                        |> Maybe.withDefault ""
-                        |> String.trim
-            in
-            if trimmed == "" then
-                []
-
-            else
-                [ FetchParticipantsByName trimmed ]
-
-        ParticipantView participantId ->
-            case participantId of
-                ParticipantChild childId ->
-                    let
-                        fetchMother =
-                            EveryDict.get childId db.children
-                                |> Maybe.withDefault NotAsked
+    let
+        forDialog =
+            case model.dialogState of
+                Just (Registering participant) ->
+                    case participant of
+                        ChildParticipant _ ->
+                            db.postChild
                                 |> RemoteData.toMaybe
-                                |> Maybe.andThen .motherId
-                                |> Maybe.map FetchMother
+                                |> Maybe.map FetchChild
+                                |> Maybe.Extra.toList
+
+                        MotherParticipant _ ->
+                            []
+
+                _ ->
+                    []
+
+        forPhase =
+            case model.registrationPhase of
+                ParticipantSearch _ ->
+                    let
+                        trimmed =
+                            model.submittedSearch
+                                |> Maybe.withDefault ""
+                                |> String.trim
                     in
-                    List.filterMap identity
-                        [ Just <| FetchChild childId
-                        , fetchMother
-                        ]
+                    if trimmed == "" then
+                        []
 
-                ParticipantMother motherId ->
-                    [ FetchMother motherId
-                    , FetchChildrenOfMother motherId
-                    ]
+                    else
+                        [ FetchParticipantsByName trimmed ]
 
-        ParticipantRegistration step ->
-            []
+                ParticipantView participantId ->
+                    case participantId of
+                        ParticipantChild childId ->
+                            let
+                                fetchMother =
+                                    EveryDict.get childId db.children
+                                        |> Maybe.withDefault NotAsked
+                                        |> RemoteData.toMaybe
+                                        |> Maybe.andThen .motherId
+                                        |> Maybe.map FetchMother
+                            in
+                            List.filterMap identity
+                                [ Just <| FetchChild childId
+                                , fetchMother
+                                ]
+
+                        ParticipantMother motherId ->
+                            [ FetchMother motherId
+                            , FetchChildrenOfMother motherId
+                            ]
+
+                ParticipantRegistration step ->
+                    []
+    in
+    forDialog ++ forPhase
