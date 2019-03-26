8b181a460544a1497b2d2cff980752da3cae7112
Ryan Rempel <rgrempel@gmail.com>
2019-03-25 23:24:38 -0500

    Migrate field_mother to field_person.

---
+++ server/hedley/modules/custom/hedley_person/scripts/migrate-field-mother.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @file
+ * Migrate existing field_mother to populate field_person.
+ *
+ * Run migrate-mothers and migrate-children first.
+ *
+ * Drush scr
+ * profiles/hedley/modules/custom/hedley_person/scripts/migrate-field-mother.php.
+ */
+
+if (!drupal_is_cli()) {
+  // Prevent execution from browser.
+  return;
+}
+
+// Get the last node id.
+$nid = drush_get_option('nid', 0);
+
+// Get the number of nodes to be processed.
+$batch = drush_get_option('batch', 50);
+
+// Get allowed memory limit.
+$memory_limit = drush_get_option('memory_limit', 500);
+
+// We're just testing that field_child has a value ...
+$base_query = new EntityFieldQuery();
+$base_query
+  ->entityCondition('entity_type', 'node')
+  ->propertyCondition('status', NODE_PUBLISHED)
+  ->propertyCondition('type', 'family_planning')
+  ->propertyOrderBy('nid', 'ASC');
+
+if ($nid) {
+  $base_query->propertyCondition('nid', $nid, '>');
+}
+
+while (TRUE) {
+  // Free up memory.
+  drupal_static_reset();
+
+  $query = clone $base_query;
+  if ($nid) {
+    $query->propertyCondition('nid', $nid, '>');
+  }
+
+  $result = $query
+    ->range(0, $batch)
+    ->execute();
+
+  if (empty($result['node'])) {
+    // No more items left.
+    break;
+  }
+
+  $ids = array_keys($result['node']);
+  $nodes = node_load_multiple($ids);
+
+  foreach ($nodes as $node) {
+    $measurement = entity_metadata_wrapper('node', $node);
+
+    $params = [
+      '@id' => $node->nid,
+    ];
+
+    $measurement->field_person->set($measurement->field_mother->field_person->value());
+    $measurement->save();
+
+    drush_print(format_string('Migrated Id @id.', $params));
+  }
+
+  $nid = end($ids);
+
+  if (round(memory_get_usage() / 1048576) >= $memory_limit) {
+    drush_print(dt('Stopped before out of memory. Start process from the node ID @nid', ['@nid' => $nid]));
+    return;
+  }
+}
+
+drush_print('Done!');
