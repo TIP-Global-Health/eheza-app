03069b0ec1638433d60a08c230a192986aacc870
Ryan Rempel <rgrempel@gmail.com>
2019-06-24 17:19:52 -0500

    Track when a prenatal participant is expected.

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPrenatalParticipants.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPrenatalParticipants.class.php
@@ -21,10 +21,27 @@ class HedleyRestfulPrenatalParticipants extends HedleyRestfulSyncBase {
       'sub_property' => 'field_uuid',
     ];
 
+    $public_fields['expected'] = [
+      'property' => 'field_expected',
+      'process_callbacks' => [
+        [$this, 'renderDate'],
+      ],
+    ];
+
     // The label is decorative only.
     unset($public_fields['label']);
 
     return $public_fields;
+  }
+
+  /**
+   * Show the date only.
+   */
+  public function renderDate($date) {
+    return [
+      'value' => $date['value'] ? hedley_restful_timestamp_only_date($date['value']) : NULL,
+      'value2' => $date['value2'] ? hedley_restful_timestamp_only_date($date['value2']) : NULL,
+    ];
   }
 
 }
