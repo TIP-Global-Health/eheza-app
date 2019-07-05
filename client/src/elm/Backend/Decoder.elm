9f232cacdc9f1badecc718fb771a7995d13dfcd4
Ryan Rempel <rgrempel@gmail.com>
2019-07-05 09:28:57 -0500

    Decode revisions for the prenatal measurements.

--- client/src/elm/Backend/Decoder.elm
+++ client/src/elm/Backend/Decoder.elm
@@ -3,12 +3,14 @@ module Backend.Decoder exposing (decodeRevision)
 import Backend.Clinic.Decoder exposing (decodeClinic)
 import Backend.Counseling.Decoder exposing (decodeCounselingSchedule, decodeCounselingTopic)
 import Backend.HealthCenter.Decoder exposing (decodeCatchmentArea, decodeHealthCenter)
-import Backend.Measurement.Decoder exposing (decodeAttendance, decodeCounselingSession, decodeFamilyPlanning, decodeHeight, decodeMuac, decodeNutrition, decodeParticipantConsent, decodePhoto, decodeWeight)
+import Backend.Measurement.Decoder exposing (..)
 import Backend.Model exposing (..)
 import Backend.Nurse.Decoder exposing (decodeNurse)
 import Backend.ParticipantConsent.Decoder exposing (decodeParticipantForm)
 import Backend.Person.Decoder exposing (decodePerson)
 import Backend.PmtctParticipant.Decoder exposing (decodePmtctParticipant)
+import Backend.PrenatalEncounter.Decoder exposing (decodePrenatalEncounter)
+import Backend.PrenatalParticipant.Decoder exposing (decodePrenatalParticipant)
 import Backend.Relationship.Decoder exposing (decodeRelationship)
 import Backend.Session.Decoder exposing (decodeSession)
 import Json.Decode exposing (..)
@@ -26,11 +28,17 @@ decodeRevision =
                     "attendance" ->
                         decodeWithUuid AttendanceRevision decodeAttendance
 
+                    "breast_exam" ->
+                        decodeWithUuid BreastExamRevision decodeBreastExam
+
                     "catchment_area" ->
                         decodeWithUuid CatchmentAreaRevision decodeCatchmentArea
 
                     "clinic" ->
                         decodeWithUuid ClinicRevision decodeClinic
+
+                    "core_physical_exam" ->
+                        decodeWithUuid CorePhysicalExamRevision decodeCorePhysicalExam
 
                     "counseling_schedule" ->
                         decodeWithUuid CounselingScheduleRevision decodeCounselingSchedule
@@ -41,35 +49,41 @@ decodeRevision =
                     "counseling_topic" ->
                         decodeWithUuid CounselingTopicRevision decodeCounselingTopic
 
-                    "health_center" ->
-                        decodeWithUuid HealthCenterRevision decodeHealthCenter
-
-                    "person" ->
-                        decodeWithUuid PersonRevision decodePerson
-
-                    "relationship" ->
-                        decodeWithUuid RelationshipRevision decodeRelationship
-
-                    "pmtct_participant" ->
-                        decodeWithUuid PmtctParticipantRevision decodePmtctParticipant
-
-                    "session" ->
-                        decodeWithUuid SessionRevision decodeSession
-
-                    "nurse" ->
-                        decodeWithUuid NurseRevision decodeNurse
+                    "danger_signs" ->
+                        decodeWithUuid DangerSignsRevision decodeDangerSigns
 
                     "family_planning" ->
                         decodeWithUuid FamilyPlanningRevision decodeFamilyPlanning
 
+                    "health_center" ->
+                        decodeWithUuid HealthCenterRevision decodeHealthCenter
+
                     "height" ->
                         decodeWithUuid HeightRevision decodeHeight
+
+                    "last_menstrual_period" ->
+                        decodeWithUuid LastMenstrualPeriodRevision decodeLastMenstrualPeriod
+
+                    "medical_history" ->
+                        decodeWithUuid MedicalHistoryRevision decodeMedicalHistory
+
+                    "medication" ->
+                        decodeWithUuid MedicationRevision decodeMedication
 
                     "muac" ->
                         decodeWithUuid MuacRevision decodeMuac
 
+                    "nurse" ->
+                        decodeWithUuid NurseRevision decodeNurse
+
                     "nutrition" ->
                         decodeWithUuid ChildNutritionRevision decodeNutrition
+
+                    "obstetric_history" ->
+                        decodeWithUuid ObstetricHistoryRevision decodeObstetricHistory
+
+                    "obstetrical_exam" ->
+                        decodeWithUuid ObstetricalExamRevision decodeObstetricalExam
 
                     "participant_consent" ->
                         decodeWithUuid ParticipantConsentRevision decodeParticipantConsent
@@ -77,8 +91,41 @@ decodeRevision =
                     "participant_form" ->
                         decodeWithUuid ParticipantFormRevision decodeParticipantForm
 
+                    "person" ->
+                        decodeWithUuid PersonRevision decodePerson
+
                     "photo" ->
                         decodeWithUuid PhotoRevision decodePhoto
+
+                    "pmtct_participant" ->
+                        decodeWithUuid PmtctParticipantRevision decodePmtctParticipant
+
+                    "prenatal_encounter" ->
+                        decodeWithUuid PrenatalEncounterRevision decodePrenatalEncounter
+
+                    "prenatal_family_planning" ->
+                        decodeWithUuid PrenatalFamilyPlanningRevision decodePrenatalFamilyPlanning
+
+                    "prenatal_nutrition" ->
+                        decodeWithUuid PrenatalNutritionRevision decodePrenatalNutrition
+
+                    "prenatal_participant" ->
+                        decodeWithUuid PrenatalParticipantRevision decodePrenatalParticipant
+
+                    "relationship" ->
+                        decodeWithUuid RelationshipRevision decodeRelationship
+
+                    "resource" ->
+                        decodeWithUuid ResourceRevision decodeResource
+
+                    "session" ->
+                        decodeWithUuid SessionRevision decodeSession
+
+                    "social_history" ->
+                        decodeWithUuid SocialHistoryRevision decodeSocialHistory
+
+                    "vitals" ->
+                        decodeWithUuid VitalsRevision decodeVitals
 
                     "weight" ->
                         decodeWithUuid WeightRevision decodeWeight
