7127eb44553f88e7c8540968b65360ee70fb8347
Anatoly <anatoly@gizra.com>
2019-07-10 19:52:05 +0300

    Expose with Restful

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulObstetricHistoriesStep2.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulObstetricHistoriesStep2.
+ */
+
+/**
+ * Class HedleyRestfulObstetricHistoriesStep2.
+ */
+class HedleyRestfulObstetricHistoriesStep2 extends HedleyRestfulPrenatalActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $standard_fields_names = [
+      'field_c_sections',
+      'field_c_section_reason',
+      'field_previous_delivery_period',
+      'field_obstetric_history',
+    ];
+
+    foreach ($standard_fields_names as $field_name) {
+      $public_name = str_replace('field_', '', $field_name);
+      $public_fields[$public_name] = [
+        'property' => $field_name,
+      ];
+    }
+
+    return $public_fields;
+  }
+
+}
