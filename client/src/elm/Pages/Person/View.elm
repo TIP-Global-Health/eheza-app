9de1ded1af382abc7ec7e1623f99636ce2c2f8f7
Anatoly <anatoly@gizra.com>
2019-07-24 12:45:09 +0300

    View natinal id as numeric input

--- client/src/elm/Pages/Person/View.elm
+++ client/src/elm/Pages/Person/View.elm
@@ -736,6 +736,7 @@ viewCreateForm language currentDate relationId model db =
                 :: (List.map (Html.map (MsgForm relationId)) <|
                         [ viewTextInput language Translate.FirstName Backend.Person.Form.firstName False personForm
                         , viewTextInput language Translate.SecondName Backend.Person.Form.secondName True personForm
+                        , viewNumberInput language Translate.NationalIdNumber Backend.Person.Form.nationalIdNumber False personForm
                         ]
                    )
                 ++ [ birthDateInput ]
@@ -1065,6 +1066,16 @@ viewCreateForm language currentDate relationId model db =
 
 viewTextInput : Language -> TranslationId -> String -> Bool -> Form e a -> Html Form.Msg
 viewTextInput language labelId fieldName isRequired form =
+    viewFormInput language labelId Form.Input.textInput fieldName isRequired form
+
+
+viewNumberInput : Language -> TranslationId -> String -> Bool -> Form e a -> Html Form.Msg
+viewNumberInput language labelId fieldName isRequired form =
+    viewFormInput language labelId (Form.Input.baseInput "number" Form.Field.String Form.Text) fieldName isRequired form
+
+
+viewFormInput : Language -> TranslationId -> Form.Input.Input e String -> String -> Bool -> Form e a -> Html Form.Msg
+viewFormInput language labelId formInput fieldName isRequired form =
     let
         field =
             Form.getFieldAsString fieldName form
