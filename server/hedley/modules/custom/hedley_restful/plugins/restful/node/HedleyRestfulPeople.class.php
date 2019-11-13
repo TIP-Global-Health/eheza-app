a059f497ff92181ed55b120aa25726ec8024af32
Amitai Burstein <amitai@gizra.com>
2019-11-13 10:06:20 +0200

    Start wire

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPeople.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPeople.class.php
@@ -67,4 +67,84 @@ class HedleyRestfulPeople extends HedleyRestfulSyncBase {
     return $public_fields;
   }
 
+  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
+    $field_names = [
+      'field_birth_date_estimated',
+      'field_cell',
+      'field_district',
+      'field_education_level',
+      'field_first_name',
+      'field_gender',
+      'field_marital_status',
+      'field_national_id_number',
+      'field_phone_number',
+      'field_province',
+      'field_second_name',
+      'field_sector',
+      'field_ubudehe',
+      'field_village',
+      'field_hiv_status',
+      'field_number_of_children',
+      'field_mode_of_delivery',
+      'field_hmis_number',
+
+      // Other fields.
+      'field_photo',
+      'field_birth_date',
+      'field_health_center',
+    ];
+
+    foreach ($field_names as $field_name) {
+      hedley_restful_join_field_to_query($query, 'node', $field_name);
+    }
+
+    // For the Photo, get to the `file`. We'll convert the `uri` to `field_photo`
+    // in
+    $query->innerJoin('file_managed', 'f', 'f.fid = field_photo.field_photo_fid');
+    $query->addField('f', 'uri');
+
+    // Get the UUID of the health center.
+    hedley_restful_join_field_to_query($query, 'node', 'field_uuid', TRUE, "field_health_center.field_health_center_target_id");
+
+  }
+
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $fields_info = $this->getPublicFields();
+
+    foreach ($items as &$row) {
+      foreach ($fields_info as $public_name => $field_info) {
+        if (strpos($field_info['property'], 'field_') !== 0) {
+          continue;
+        }
+
+        if (!isset($row->{$field_info['property']})) {
+          continue;
+        }
+
+        $row->{$public_name} = $row->{$field_info['property']};
+        unset($row->{$field_info['property']});
+      }
+
+      $row->birth_date = $this->convertTimestampToYmd($row->birth_date);
+
+      $fid = $row->photo;
+      $uri = $row->uri;
+
+      $image_style  = 'patient-photo';
+
+      $value = [
+        'id' => $fid,
+        'self' => file_create_url($uri),
+        'styles' => [
+          $image_style => image_style_url($image_style  , $uri),
+        ],
+      ];
+
+      $row->photo = $value;
+    }
+
+    return $items;
+  }
+
+
 }
