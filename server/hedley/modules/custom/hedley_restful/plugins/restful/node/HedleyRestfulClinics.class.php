5c08cc1e4f04eaeee2215835668167551e6dab28
Anatoly <anatoly@gizra.com>
2020-02-05 15:02:14 +0200

    Handle entityrefference fileds with multiple values [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulClinics.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulClinics.class.php
@@ -28,4 +28,34 @@ class HedleyRestfulClinics extends HedleyRestfulSyncBase {
     return $public_fields;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
+    $field_names = [
+      'field_group_type',
+      'field_health_center',
+    ];
+
+    foreach ($field_names as $field_name) {
+      hedley_restful_join_field_to_query($query, 'node', $field_name, FALSE);
+    }
+
+    // Get the UUID of the health center.
+    hedley_restful_join_field_to_query($query, 'node', 'field_uuid', TRUE, "field_health_center.field_health_center_target_id");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      $item->health_center = hedley_restful_nid_to_uuid($item->health_center);
+    }
+
+    return $items;
+  }
+
 }
--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulNurses.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulNurses.class.php
@@ -32,4 +32,46 @@ class HedleyRestfulNurses extends HedleyRestfulSyncBase {
     return $public_fields;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
+    $field_names = [
+      'field_role',
+      'field_pin_code',
+      'field_health_centers',
+    ];
+
+    foreach ($field_names as $field_name) {
+      hedley_restful_join_field_to_query($query, 'node', $field_name, FALSE);
+    }
+
+    // Get the UUID of the health center.
+    hedley_restful_join_field_to_query($query, 'node', 'field_uuid', TRUE, "field_health_centers.field_health_centers_target_id");
+
+    $query->addExpression("GROUP_CONCAT(DISTINCT field_role.field_role_value)", 'field_role');
+    $query->addExpression("GROUP_CONCAT(DISTINCT field_health_centers.field_health_centers_target_id)", 'field_health_centers');
+    $query->groupBy('node.nid');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      $item->role = explode(',', $item->role);
+
+      $health_centers = explode(',', $item->health_centers);
+      foreach ($health_centers as &$health_center) {
+        $health_center = hedley_restful_nid_to_uuid($health_center);
+      }
+
+      $item->health_centers = $health_centers;
+    }
+
+    return $items;
+  }
+
 }
