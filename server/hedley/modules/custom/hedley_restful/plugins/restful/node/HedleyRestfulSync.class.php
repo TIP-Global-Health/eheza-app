d2e4f46ffe13efea81399a5920ad7403a20478a9
nedSaf <nader@gizra.com>
2020-02-05 16:05:00 +0200

    analyze the data and put it in an array [skip ci]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulSync.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulSync.class.php
@@ -243,6 +243,7 @@ class HedleyRestfulSync extends \RestfulBase implements \RestfulDataProviderInte
       'type' => 'statistics',
       // UUID of the health center.
       'uuid' => $wrapper->field_uuid->value(),
+      'case_management' => hedley_stats_get_case_management($health_center_id),
       'good_nutrition' => hedley_stats_get_good_nutrition($health_center_id),
       'children_beneficiaries' => hedley_stats_get_children_beneficiaries_stats_by_period($health_center_id),
       'malnourished_beneficiaries' => hedley_stats_get_malnourished_beneficiaries_stats_by_period($health_center_id),
