5c9d62646235c54a41c9d76d1d0a4a92faa96cc4
Anatoly <anatoly@gizra.com>
2020-02-10 11:03:49 +0200

    Fix daysDiff Calculations

--- client/src/elm/Measurement/View.elm
+++ client/src/elm/Measurement/View.elm
@@ -216,7 +216,7 @@ viewFloatForm config language currentDate child measurements zscores model =
         -- And, we'll need the child's age.
         maybeAgeInDays =
             Maybe.map
-                (\birthDate -> diffDays dateMeasured birthDate)
+                (\birthDate -> diffDays birthDate dateMeasured)
                 child.birthDate
 
         renderedZScoreForAge =
