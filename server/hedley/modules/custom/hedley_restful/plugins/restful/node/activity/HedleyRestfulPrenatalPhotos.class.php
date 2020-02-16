ff78bd561766dbffbbcaf0ce5e79f7e01359fdaf
Anatoly <anatoly@gizra.com>
2020-02-16 22:00:19 +0200

    A fix for photo [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalPhotos.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalPhotos.class.php
@@ -36,4 +36,33 @@ class HedleyRestfulPrenatalPhotos extends HedleyRestfulPrenatalActivityBase {
     return $public_fields;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
+    $query = parent::alterQueryForViewWithDbSelect($query);
+
+    // For the Photo, get to the `file`. We'll convert the `uri`
+    // to `field_photo`.
+    $query->innerJoin('file_managed', 'f', 'f.fid = field_photo.field_photo_fid');
+    $query->addField('f', 'uri');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      if (!empty($item->photo) && !empty($item->uri)) {
+        $item->photo = image_style_url('patient-photo', $item->uri);
+      }
+
+      unset($item->uri);
+    }
+
+    return $items;
+  }
+
 }
