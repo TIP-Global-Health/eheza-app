3df0e73fdeb6b8aead648cdb6a06ff92eae9656c
Anatoly <anatoly@gizra.com>
2020-04-13 11:46:44 +0300

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulHCContacts.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulHCContacts.
+ */
+
+/**
+ * Class HedleyRestfulHCContacts.
+ */
+class HedleyRestfulHCContacts extends HedleyRestfulAcuteIllnessActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_hc_contact',
+    'field_hc_recommendation',
+    'field_hc_response_time',
+    'field_ambulance_arrival_time',
+  ];
+
+}
