dc1d95a934281c639f42b21b3fb281f0e831bf95
Anatoly <anatoly@gizra.com>
2020-05-20 13:10:35 +0300

    Do not resize by DropZone [ci skip]

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -156,9 +156,6 @@ function attachDropzone() {
   dropZone = new Dropzone(selector, {
     url: "cache-upload/images",
     dictDefaultMessage: "Touch here to take a photo, or drop a photo file here.",
-    resizeWidth: 800,
-    resizeHeight: 800,
-    resizeMethod: "contain",
     acceptedFiles: "jpg,jpeg,png,gif,image/*"
   });
 
