531dd5894aeb668e299a7808edea1aa9c378ebde
Amitai Burstein <amitai@gizra.com>
2020-05-30 18:39:32 +0300

    Delete photo from cache storage.

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -623,6 +623,11 @@ elmApp.ports.sendLocalIdsForDelete.subscribe(async function(info) {
 
   // Delete also from the photoUploadChanges table.
   await photoUploadTable.bulkDelete(localIds);
+
+  // Delete photo from cache storage.
+  const row = result[0];
+  const cache = await caches.open('photos-upload');
+  await cache.delete(row.data.photo);
 });
 
 
