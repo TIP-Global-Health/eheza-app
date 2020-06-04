2b0d7ae66bce3350d38fb4a105ef42e272a24831
Anatoly <anatoly@gizra.com>
2020-06-04 11:28:30 +0300

    Expose with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulSymptomsGeneral.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulSymptomsGeneral.class.php
@@ -19,6 +19,17 @@ class HedleyRestfulSymptomsGeneral extends HedleyRestfulAcuteIllnessActivityBase
     'field_night_sweats_period',
     'field_body_aches_period',
     'field_headache_period',
+    'field_coke_colored_urine_period',
+    'field_convulsions_period',
+    'field_dry_mouth_period',
+    'field_increased_thirst_period',
+    'field_lethargy_period',
+    'field_poor_suck_period',
+    'field_severe_weakness_period',
+    'field_spontaneos_bleeding_period',
+    'field_unable_to_drink_period',
+    'field_unable_to_eat_period',
+    'field_yellow_eyes_period',
   ];
 
 }
