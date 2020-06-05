2f6329ccfc20bebfa48f93efa3a5c70de31b4495
Aron Novak <aron@gizra.com>
2020-06-05 12:44:12 +0200

    fix client installation

--- ci-scripts/install_client.sh
+++ ci-scripts/install_client.sh
@@ -14,11 +14,11 @@ npm install -g elm@latest-0.19.1
 npm install -g elm-test@0.18.12
 npm install --global gulp-cli
 npm install -g bower
-bower install
-elm-package install -y
 
 cd "$TRAVIS_BUILD_DIR"/client
+bower install
 npm install
+elm-package install -y
 
 # Gulp is responsible for creating the `src/generated` files.
 cp ./src/elm/LocalConfig.Example.elm ./src/elm/LocalConfig.elm
