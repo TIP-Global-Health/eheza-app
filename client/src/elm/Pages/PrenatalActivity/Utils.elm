6f9af84ba5d809a66bcf92df5b75dfd1aa78cf13
Anatoly <anatoly@gizra.com>
2020-07-19 21:51:49 +0300

    Make isDirty logic more generic [ci skip]

--- client/src/elm/Pages/PrenatalActivity/Utils.elm
+++ client/src/elm/Pages/PrenatalActivity/Utils.elm
@@ -8,7 +8,7 @@ import Maybe.Extra exposing (andMap, isJust, isNothing, or, unwrap)
 import Pages.PrenatalActivity.Model exposing (..)
 import Pages.PrenatalEncounter.Model exposing (AssembledData)
 import Pages.PrenatalEncounter.Utils exposing (getMotherHeightMeasurement)
-import Pages.Utils exposing (ifEverySetEmpty, taskCompleted, taskListCompleted)
+import Pages.Utils exposing (ifEverySetEmpty, taskCompleted, taskListCompleted, valueConsideringIsDirtyField)
 
 
 {-| This is a convenience for cases where the form values ought to be redefined
@@ -360,61 +360,18 @@ obstetricHistoryFormWithDefault form saved =
         |> unwrap
             form
             (\value ->
-                let
-                    termPregnancyValue =
-                        if form.termPregnancyDirty then
-                            form.termPregnancy
-
-                        else
-                            or form.termPregnancy (Just value.termPregnancy)
-
-                    preTermPregnancyValue =
-                        if form.preTermPregnancyDirty then
-                            form.preTermPregnancy
-
-                        else
-                            or form.preTermPregnancy (Just value.preTermPregnancy)
-
-                    stillbirthsAtTermValue =
-                        if form.stillbirthsAtTermDirty then
-                            form.stillbirthsAtTerm
-
-                        else
-                            or form.stillbirthsAtTerm (Just value.stillbirthsAtTerm)
-
-                    stillbirthsPreTermValue =
-                        if form.stillbirthsPreTermDirty then
-                            form.stillbirthsPreTerm
-
-                        else
-                            or form.stillbirthsPreTerm (Just value.stillbirthsPreTerm)
-
-                    abortionsValue =
-                        if form.abortionsDirty then
-                            form.abortions
-
-                        else
-                            or form.abortions (Just value.abortions)
-
-                    liveChildrenValue =
-                        if form.liveChildrenDirty then
-                            form.liveChildren
-
-                        else
-                            or form.liveChildren (Just value.liveChildren)
-                in
                 { currentlyPregnant = or form.currentlyPregnant (Just value.currentlyPregnant)
-                , termPregnancy = termPregnancyValue
+                , termPregnancy = valueConsideringIsDirtyField form.termPregnancyDirty form.termPregnancy value.termPregnancy
                 , termPregnancyDirty = form.termPregnancyDirty
-                , preTermPregnancy = preTermPregnancyValue
+                , preTermPregnancy = valueConsideringIsDirtyField form.preTermPregnancyDirty form.preTermPregnancy value.preTermPregnancy
                 , preTermPregnancyDirty = form.preTermPregnancyDirty
-                , stillbirthsAtTerm = stillbirthsAtTermValue
+                , stillbirthsAtTerm = valueConsideringIsDirtyField form.stillbirthsAtTermDirty form.stillbirthsAtTerm value.stillbirthsAtTerm
                 , stillbirthsAtTermDirty = form.stillbirthsAtTermDirty
-                , stillbirthsPreTerm = stillbirthsPreTermValue
+                , stillbirthsPreTerm = valueConsideringIsDirtyField form.stillbirthsPreTermDirty form.stillbirthsPreTerm value.stillbirthsPreTerm
                 , stillbirthsPreTermDirty = form.stillbirthsPreTermDirty
-                , abortions = abortionsValue
+                , abortions = valueConsideringIsDirtyField form.abortionsDirty form.abortions value.abortions
                 , abortionsDirty = form.abortionsDirty
-                , liveChildren = liveChildrenValue
+                , liveChildren = valueConsideringIsDirtyField form.liveChildrenDirty form.liveChildren value.liveChildren
                 , liveChildrenDirty = form.liveChildrenDirty
                 }
             )
@@ -443,15 +400,7 @@ obstetricHistoryStep2FormWithDefault form saved =
         |> unwrap
             form
             (\value ->
-                let
-                    cSectionsValue =
-                        if form.cSectionsDirty then
-                            form.cSections
-
-                        else
-                            or form.cSections (Just value.cSections)
-                in
-                { cSections = cSectionsValue
+                { cSections = valueConsideringIsDirtyField form.cSectionsDirty form.cSections value.cSections
                 , cSectionsDirty = form.cSectionsDirty
                 , cSectionInPreviousDelivery = or form.cSectionInPreviousDelivery (EverySet.member CSectionInPreviousDelivery value.previousDelivery |> Just)
                 , cSectionReason = or form.cSectionReason (value.cSectionReason |> EverySet.toList |> List.head)
