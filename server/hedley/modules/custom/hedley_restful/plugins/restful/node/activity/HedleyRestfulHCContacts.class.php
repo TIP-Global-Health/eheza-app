dbfe55cd9a1e87c039712ffea01ea0f1902e3521
Anatoly <anatoly@gizra.com>
2020-09-19 22:04:51 +0300

    Update CT on backend + expose with restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulHCContacts.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulHCContacts.class.php
@@ -14,10 +14,9 @@ class HedleyRestfulHCContacts extends HedleyRestfulAcuteIllnessActivityBase {
    * {@inheritdoc}
    */
   protected $multiFields = [
-    'field_hc_response_time',
-    'field_ambulance_arrival_time',
     'field_hc_contact',
     'field_hc_recommendation',
+    'field_site_recommendation',
   ];
 
 }
