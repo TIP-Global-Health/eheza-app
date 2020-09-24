7f4f9226a7aeeea128aff9cf469d2a4e62ddadc3
Anatoly <anatoly@gizra.com>
2020-09-24 12:54:20 +0300

    Call 114 Content type + expose with restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulCall114s.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulCall114s.
+ */
+
+/**
+ * Class HedleyRestfulCall114s.
+ */
+class HedleyRestfulCall114s extends HedleyRestfulAcuteIllnessActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_hc_contact',
+    'field_hc_recommendation',
+    'field_site_recommendation',
+  ];
+
+}
