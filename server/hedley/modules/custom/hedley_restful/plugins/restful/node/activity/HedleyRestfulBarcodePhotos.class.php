cbbb74b60504c1dc94d3da07b3c103b66f69b8d0
Anatoly <anatoly@gizra.com>
2020-10-20 00:12:59 +0300

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulBarcodePhotos.class.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulBarcodePhotos.
+ */
+
+/**
+ * Class HedleyRestfulBarcodePhotos.
+ */
+class HedleyRestfulBarcodePhotos extends HedleyRestfulAcuteIllnessActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_photo',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    unset($public_fields['photo']);
+
+    $public_fields['photo'] = [
+      'property' => 'field_photo',
+      'process_callbacks' => [
+        [$this, 'imageProcess'],
+      ],
+      'image_styles' => ['patient-photo'],
+    ];
+
+    return $public_fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
+    $query = parent::alterQueryForViewWithDbSelect($query);
+
+    // For the Photo, get to the `file`. We'll convert the `uri`
+    // to `field_photo`.
+    $query->innerJoin('file_managed', 'f', 'f.fid = field_photo.field_photo_fid');
+    $query->addField('f', 'uri');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      if (!empty($item->photo) && !empty($item->uri)) {
+        $item->photo = image_style_url('patient-photo', $item->uri);
+      }
+
+      unset($item->uri);
+    }
+
+    return $items;
+  }
+
+}
