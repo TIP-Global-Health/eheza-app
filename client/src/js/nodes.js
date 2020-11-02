2684ff42d39b6dae38027037e8e2862337f066a7
Anatoly <anatoly@gizra.com>
2020-11-02 10:08:25 +0200

    Revert 4befc9b

--- client/src/js/nodes.js
+++ client/src/js/nodes.js
@@ -212,8 +212,6 @@
                                         return Promise.resolve(response);
                                     });
                                 } else {
-                                    json.deleted = false;
-
                                     var change = {
                                         type: type,
                                         uuid: uuid,
@@ -263,7 +261,6 @@
                         json.uuid = uuid;
                         json.type = type;
                         json.status = Status.published;
-                        json.deleted = false;
 
                         // Not entirely clear whose job it should be to figure
                         // out the shard, but we'll do it here for now.
