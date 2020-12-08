4cbe0c60fb13ac182cf17a4f4bb180d28ff415dc
diame1 <danimahoro75@gmail.com>
2020-12-08 17:37:56 +0200

    add translation for Aheza Child

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -1330,7 +1330,7 @@ translationSet trans =
 
         ActivitityTitleAchi ->
             { english = "Aheza Child"
-            , kinyarwanda = Nothing
+            , kinyarwanda = Just "Aheza igenewe umwana"
             }
 
         ActivityProgressReport activity ->
@@ -1397,7 +1397,7 @@ translationSet trans =
 
         ActivitityLabelAchi ->
             { english = "Enter the amount of Aheza distributed below."
-            , kinyarwanda = Nothing
+            , kinyarwanda = Just "Uzuza hano ingano ya Aheza utanze"
             }
 
         ActivePage page ->
@@ -2533,7 +2533,7 @@ translationSet trans =
             case clinicType of
                 Achi ->
                     { english = "Aheza Distribution"
-                    , kinyarwanda = Nothing
+                    , kinyarwanda = Just "Gutanga Aheza"
                     }
 
                 _ ->
