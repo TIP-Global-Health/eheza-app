7312e7e007c50aa112e8b180fe6e1dcdf911ddd2
Anatoly <anatoly@gizra.com>
2020-12-13 12:33:53 +0200

    AcuteIllnessVitals [ci skip]

---
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/activity/acute_illness/HedleyMigrateAcuteIllnessVitals.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \HedleyMigrateAcuteIllnessVitals.
+ */
+
+/**
+ * Class HedleyMigrateAcuteIllnessVitals.
+ */
+class HedleyMigrateAcuteIllnessVitals extends HedleyMigrateAcuteIllnessMeasurementBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $bundle = 'acute_illness_vitals';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function csvColumns() {
+    $columns = parent::csvColumns();
+
+    return array_merge(
+      $columns, [
+        'field_respiratory_rate',
+        'field_body_temperature',
+      ]
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function simpleMappings() {
+    $mappings = parent::simpleMappings();
+
+    return array_merge(
+      $mappings, [
+        'field_respiratory_rate',
+        'field_body_temperature',      ]
+    );
+  }
+
+}
