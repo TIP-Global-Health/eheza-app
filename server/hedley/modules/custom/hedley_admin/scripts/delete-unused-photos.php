cea2d376adfe476b7c5a3f5710bd175e7403ba34
Anatoly <anatoly@gizra.com>
2020-12-24 20:47:41 +0200

    Delete unused files script

---
+++ server/hedley/modules/custom/hedley_admin/scripts/delete-unused-photos.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @file
+ * Resizes large dimension images to standard size (600x800).
+ *
+ * Drush scr
+ * profiles/hedley/modules/custom/hedley_admin/scripts/delete-unused-photos.php.
+ */
+
+if (!drupal_is_cli()) {
+  // Prevent execution from browser.
+  return;
+}
+
+// Get the last node id.
+$fid = drush_get_option('fid', 0);
+
+// Get the number of nodes to be processed.
+$batch = drush_get_option('batch', 50);
+
+// Get allowed memory limit.
+$memory_limit = drush_get_option('memory_limit', 500);
+
+$base_query = db_select('file_managed ', 'files')
+  ->fields('files', ['fid']);
+
+$count_query = clone $base_query;
+$count_query->condition('fid', $fid, '>');
+$executed = $count_query->execute();
+$total = $executed->rowCount();
+
+if ($total == 0) {
+  drush_print("There are no images in DB.");
+  exit;
+}
+
+drush_print("$total images located.");
+
+if ($total == 0) {
+  drush_print("There are no images in DB.");
+  exit;
+}
+
+$unused = 0;
+$unused_size = 0;
+
+$processed = 0;
+while ($processed < $total) {
+  // Free up memory.
+  drupal_static_reset();
+
+  $query = clone $base_query;
+  if ($fid) {
+    $query->condition('fid', $fid, '>');
+  }
+
+  $rows = $query
+    ->range(0, $batch)
+    ->execute()
+    ->fetchAllAssoc('fid');
+
+  if (empty($rows)) {
+    // No more items left.
+    break;
+  }
+
+  $ids = array_keys($rows);
+  foreach ($ids as $id) {
+    $file = file_load($id);
+
+    $file_usage = file_usage_list($file);
+
+    if (!empty($file_usage['file']['node'])) {
+      continue;
+    }
+
+    drush_print("File with ID $id is not in use. Size: $file->filesize");
+    $unused++;
+    $unused_size += (int) $file->filesize;
+
+    $success = file_delete($file, TRUE);
+    if (!$success) {
+      drush_print("File with ID $id can't be deleted.");
+    }
+  }
+
+  $fid = end($ids);
+
+  if (round(memory_get_usage() / 1048576) >= $memory_limit) {
+    drush_print(dt('Stopped before out of memory. Start process from the node ID @nid', ['@nid' => $fid]));
+    return;
+  }
+
+  $count = count($ids);
+  $processed += $count;
+  drush_print("$processed images processed.");
+}
+
+drush_print("Total unused: $unused, total size: $unused_size");
+drush_print('Done!');
