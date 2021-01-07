acfe610947cf7d675f749f7af57262dfe09e282e
Anatoly <anatoly@gizra.com>
2021-01-07 14:45:45 +0200

    A fix

--- server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateIndividualEncounterBase.php
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateIndividualEncounterBase.php
@@ -40,40 +40,11 @@ abstract class HedleyMigrateIndividualEncounterBase extends HedleyMigrateBase {
 
     $this
       ->addFieldMapping('field_scheduled_date', 'field_scheduled_date')
-      ->callbacks([$this, 'dateProcess']);
+      ->callbacks([$this, 'date2Process']);
 
     $this
       ->addFieldMapping('field_individual_participant', 'field_individual_participant')
       ->sourceMigration('HedleyMigrateIndividualParticipants');
-  }
-
-  /**
-   * Convert a date string to a timestamp.
-   *
-   * @param string $date
-   *   A string containing a date.
-   *
-   * @return int
-   *   A timestamp.
-   */
-  public function dateProcess($date) {
-    // The source material uses several date formats.
-    $trimmed = trim($date);
-
-    if (empty($trimmed)) {
-      return $trimmed;
-    }
-
-    if (preg_match('/^\\d\\d\\d\\d-\\d\\d-\\d\\d$/', $trimmed)) {
-      $stamp = DateTime::createFromFormat('!Y-m-d', $trimmed)->getTimestamp();
-
-      return [
-        'value' => $stamp,
-        'value2' => NULL,
-      ];
-    }
-
-    throw new Exception("$date was not a recognized date format.");
   }
 
 }
