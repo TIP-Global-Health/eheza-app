be663274320c9627659359e606671af901f057e9
Anatoly <anatoly@gizra.com>
2021-01-13 15:58:26 +0200

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNutritionSendToHC.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulNutritionSendToHC.
+ */
+
+/**
+ * Class HedleyRestfulNutritionSendToHC.
+ */
+class HedleyRestfulNutritionSendToHC extends HedleyRestfulAcuteIllnessActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_send_to_hc',
+  ];
+
+}
