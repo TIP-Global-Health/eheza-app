29bfff273e643ecc4d31b631c595a5fbc45a72bf
Anatoly <anatoly@gizra.com>
2021-01-14 16:12:39 +0200

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNutritionHealthEducation.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulNutritionHealthEducation.
+ */
+
+/**
+ * Class HedleyRestfulNutritionHealthEducation.
+ */
+class HedleyRestfulNutritionHealthEducation extends HedleyRestfulNutritionActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_health_education_signs',
+  ];
+
+}
