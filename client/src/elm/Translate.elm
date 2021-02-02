e8a5ceb74616abc0014290d490bd5ed5c03b612f
diame1 <danimahoro75@gmail.com>
2021-02-02 13:35:12 +0200

    add colon before medication name on Progress report

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -1304,7 +1304,7 @@ translationSet trans =
 
         Administered ->
             { english = "Administered"
-            , kinyarwanda = Just "Umuti watanzwe"
+            , kinyarwanda = Just "Umuti watanzwe:"
             }
 
         AdministeredMedicationQuestion ->
