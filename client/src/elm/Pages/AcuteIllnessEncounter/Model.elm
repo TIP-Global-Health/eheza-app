91acc81bc07733f3d43943a26fb86b98759744c9
Anatoly <anatoly@gizra.com>
2021-02-08 23:04:04 +0200

    Comments

--- client/src/elm/Pages/AcuteIllnessEncounter/Model.elm
+++ client/src/elm/Pages/AcuteIllnessEncounter/Model.elm
@@ -48,6 +48,14 @@ type alias AssembledData =
     , person : Person
     , measurements : AcuteIllnessMeasurements
     , previousEncountersData : List ( ( NominalDate, AcuteIllnessDiagnosis ), AcuteIllnessMeasurements )
+
+    -- The diagnosis that we have right now.
+    -- This can be the diagnosis of first encounter, or one of
+    -- subsequent encounters, where diagnoses was updated to
+    -- Malaria (this is the only case when we allow to update a diagnosis).
     , diagnosis : Maybe ( NominalDate, AcuteIllnessDiagnosis )
+
+    -- In case diagnosis was updated to Malaria, stores the original
+    -- diagnosis that was determined at first encounter.
     , previousDiagnosis : Maybe ( NominalDate, AcuteIllnessDiagnosis )
     }
