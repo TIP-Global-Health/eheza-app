76c9a0294605e80a4d5bd98b7a676a154f4c1c92
diame1 <danimahoro75@gmail.com>
2021-02-11 16:00:45 +0200

    fix drupal

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulSendToHCs.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulSendToHCs.class.php
@@ -21,7 +21,7 @@ class HedleyRestfulSendToHCs extends HedleyRestfulAcuteIllnessActivityBase {
    * {@inheritdoc}
    */
   protected $fields = [
-      'field_not_sent_to_hc_reason'
+      'field_not_sent_to_hc_reason',
   ];
 
 }
