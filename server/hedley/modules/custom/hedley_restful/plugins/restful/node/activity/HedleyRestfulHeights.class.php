dfe3c4a4a4ba43ef9b60f9535fbd13de2dc732a4
Anatoly <anatoly@gizra.com>
2021-03-17 15:35:12 +0200

    Addapt Group activities handler to new implementation of inheritance [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulHeights.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulHeights.class.php
@@ -13,28 +13,9 @@ class HedleyRestfulHeights extends HedleyRestfulGroupActivityBase {
   /**
    * {@inheritdoc}
    */
-  public function publicFieldsInfo() {
-    $public_fields = parent::publicFieldsInfo();
-
-    $public_fields['height'] = [
-      'property' => 'field_height',
-    ];
-
-    $public_fields['zscore_age'] = [
-      'property' => 'field_zscore_age',
-    ];
-
-    return $public_fields;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function alterQueryForViewWithDbSelect(SelectQuery $query) {
-    $query = parent::alterQueryForViewWithDbSelect($query);
-
-    hedley_restful_join_field_to_query($query, 'node', 'field_height', FALSE);
-    hedley_restful_join_field_to_query($query, 'node', 'field_zscore_age', FALSE);
-  }
+  protected $fields = [
+    'field_height',
+    'field_zscore_age',
+  ];
 
 }
