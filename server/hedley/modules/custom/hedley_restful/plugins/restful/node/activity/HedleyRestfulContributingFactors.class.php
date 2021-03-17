bd8452c409223ecd922dba4ac2e54e7c1a7713b8
Anatoly <anatoly@gizra.com>
2021-03-17 16:07:29 +0200

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulContributingFactors.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulContributingFactors.
+ */
+
+/**
+ * Class HedleyRestfulContributingFactors.
+ */
+class HedleyRestfulContributingFactors extends HedleyRestfulNutritionActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_contributing_factors_signs',
+  ];
+
+}
