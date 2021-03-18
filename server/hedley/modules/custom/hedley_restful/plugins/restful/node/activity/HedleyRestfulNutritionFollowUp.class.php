680196dfc54e30038f10990fb0ae344ee72c09da
Anatoly <anatoly@gizra.com>
2021-03-18 12:06:03 +0200

    Export with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNutritionFollowUp.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulNutritionFollowUp.
+ */
+
+/**
+ * Class HedleyRestfulNutritionFollowUp.
+ */
+class HedleyRestfulNutritionFollowUp extends HedleyRestfulNutritionActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_follow_up_options',
+  ];
+
+}
