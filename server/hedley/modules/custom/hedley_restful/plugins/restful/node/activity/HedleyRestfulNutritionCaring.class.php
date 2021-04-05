acbc10c2713c0f6be62819e1262fae5bdb906ff8
diame1 <danimahoro75@gmail.com>
2021-04-05 16:18:15 +0200

    expose restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNutritionCaring.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulNutritionCaring.
+ */
+
+/**
+ * Class HedleyRestfulNutritionCaring.
+ */
+class HedleyRestfulNutritionCaring extends HedleyRestfulHomeVisitActivityBase
+{
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_nutrition_caring_questions',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_child_caring_options',
+  ];
+
+}
