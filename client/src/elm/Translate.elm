ec21160c912527e11766213a94f83365fab7b0f1
diame1 <danimahoro75@gmail.com>
2021-04-19 20:35:33 +0200

    view forms

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -4733,6 +4733,10 @@ translationSet trans =
                     , kinyarwanda = Just "Umuturanyi"
                     }
 
+                CaredByHouseHelper ->
+                    { english = "House helper"
+                    , kinyarwanda = Just "Umukozi wo mu rugo"}
+
                 CaredByDaycare ->
                     { english = "Daycare"
                     , kinyarwanda = Just "Irerero"
