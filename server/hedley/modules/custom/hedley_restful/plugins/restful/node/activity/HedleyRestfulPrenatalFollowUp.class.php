f93e922cc083d13875f84e2e6191acc78efd49fe
diame1 <danimahoro75@gmail.com>
2021-04-27 17:21:24 +0200

    expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalFollowUp.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulPrenatalFollowUp.
+ */
+
+/**
+ * Class HedleyRestfulPrenatalFollowUp.
+ */
+class HedleyRestfulPrenatalFollowUp extends HedleyRestfulPrenatalActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_follow_up_options',
+  ];
+}
