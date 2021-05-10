879fb7c29f45748c4298c20404ddf5b1ff3aecf9
Anatoly <anatoly@gizra.com>
2021-05-10 12:42:22 +0300

    Update question [ci skip]

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -2839,7 +2839,7 @@ translationSet trans =
                     }
 
                 AntenatalEncounter ->
-                    { english = "Do you want to start a Prenatal encounter for"
+                    { english = "Do you want to start an Antenatal encounter for"
                     , kinyarwanda = Nothing
                     }
 
