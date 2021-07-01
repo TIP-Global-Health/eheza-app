faa08985df9f6e7b0320974cd0100b786419d0f1
Anatoly <anatoly@gizra.com>
2021-07-01 11:10:22 +0300

    Update comment [ci skip]

--- client/src/elm/Pages/AcuteIllnessParticipant/Utils.elm
+++ client/src/elm/Pages/AcuteIllnessParticipant/Utils.elm
@@ -7,7 +7,7 @@ import Maybe.Extra exposing (isNothing)
 
 
 {-| Illness is considered 'active' if it does not have it's end
-date and outcome set.
+date or outcome set.
 -}
 isAcuteIllnessActive : NominalDate -> IndividualEncounterParticipant -> Bool
 isAcuteIllnessActive currentDate session =
