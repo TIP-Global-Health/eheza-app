7c9ac78ce62720e9396c996f4120405859aef3a2
Anatoly <anatoly@gizra.com>
2021-07-13 13:58:01 +0300

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulWellChildMebendezole.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulWellChildMebendezole.
+ */
+
+/**
+ * Class HedleyRestfulWellChildMebendezole.
+ */
+class HedleyRestfulWellChildMebendezole extends HedleyRestfulWellChildActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_administration_note',
+  ];
+
+}
