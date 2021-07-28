befd1d3aa43e081a8c505fb9f448a5c5b10941bf
Anatoly <anatoly@gizra.com>
2021-07-28 14:39:33 +0300

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulWellChildVaccinationHistory.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulWellChildVaccinationHistory.
+ */
+
+/**
+ * Class HedleyRestfulWellChildVaccinationHistory.
+ */
+class HedleyRestfulWellChildVaccinationHistory extends HedleyRestfulWellChildActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_suggested_vaccines',
+    'field_bcg_vaccination_date',
+    'field_opv_vaccination_date',
+    'field_dtp_vaccination_date',
+    'field_pcv13_vaccination_date',
+    'field_rotarix_vaccination_date',
+    'field_ipv_vaccination_date',
+    'field_mr_vaccination_date',
+    'field_hpv_vaccination_date',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiDateFields = [
+    'field_bcg_vaccination_date',
+    'field_opv_vaccination_date',
+    'field_dtp_vaccination_date',
+    'field_pcv13_vaccination_date',
+    'field_rotarix_vaccination_date',
+    'field_ipv_vaccination_date',
+    'field_mr_vaccination_date',
+    'field_hpv_vaccination_date',
+  ];
+
+}
