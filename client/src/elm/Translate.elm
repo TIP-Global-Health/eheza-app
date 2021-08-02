4b2c3765549910684030e4a26b51ca7353f64760
Anatoly <anatoly@gizra.com>
2021-08-02 10:28:28 +0300

    Remove unneeded space [ci skip]

--- client/src/elm/Translate.elm
+++ client/src/elm/Translate.elm
@@ -2955,7 +2955,7 @@ translationSet trans =
         ECDSignQuestion sign ->
             case sign of
                 FollowMothersEyes ->
-                    { english = " Does the child follow their mothers eyes"
+                    { english = "Does the child follow their mothers eyes"
                     , kinyarwanda = Nothing
                     }
 
