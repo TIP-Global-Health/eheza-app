1aa65712ff8dc3f5002a086796bd86818758deff
Anatoly <anatoly@gizra.com>
2021-08-15 18:30:02 +0300

    Extend Model, Encoders + Decoders [ci skip]

--- client/src/elm/Backend/Measurement/Model.elm
+++ client/src/elm/Backend/Measurement/Model.elm
@@ -183,7 +183,13 @@ type ChildNutritionSign
 
 
 type alias ChildNutrition =
-    GroupMeasurement (EverySet ChildNutritionSign)
+    GroupMeasurement NutritionValue
+
+
+type alias NutritionValue =
+    { signs : EverySet ChildNutritionSign
+    , assesment : EverySet NutritionAssessment
+    }
 
 
 type alias CounselingSession =
@@ -255,7 +261,7 @@ type alias NutritionHeight =
 
 
 type alias NutritionNutrition =
-    NutritionMeasurement (EverySet ChildNutritionSign)
+    NutritionMeasurement NutritionValue
 
 
 type alias NutritionPhoto =
@@ -1259,7 +1265,7 @@ type alias WellChildMuac =
 
 
 type alias WellChildNutrition =
-    WellChildMeasurement (EverySet ChildNutritionSign)
+    WellChildMeasurement NutritionValue
 
 
 type alias WellChildPhoto =
