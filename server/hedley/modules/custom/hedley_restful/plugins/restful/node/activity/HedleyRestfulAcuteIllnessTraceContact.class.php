5de3a67dd32e4f219bd001336692dbb454f7cbec
Anatoly <anatoly@gizra.com>
2021-10-26 23:52:56 +0300

    A fix [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulAcuteIllnessTraceContact.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulAcuteIllnessTraceContact.class.php
@@ -14,6 +14,7 @@ class HedleyRestfulAcuteIllnessTraceContact extends HedleyRestfulAcuteIllnessAct
    * {@inheritdoc}
    */
   protected $fields = [
+    'field_referred_person',
     'field_first_name',
     'field_second_name',
     'field_phone_number',
