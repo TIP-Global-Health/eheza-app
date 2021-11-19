94b4daf437f6562dcb7debf61db814022fc4dc3c
Aron Novak <AronNovak@users.noreply.github.com>
2021-11-19 10:36:45 +0100

    Merge pull request #2865 from Gizra/2864-cleanup
    Code cleanup

--- client/src/elm/Config.elm
+++ client/src/elm/Config.elm
@@ -3,117 +3,12 @@ module Config exposing (configs)
 import AssocList as Dict exposing (Dict)
 import Config.Model as Config exposing (Model)
 import LocalConfig exposing (localConfigs)
-import Pusher.Model exposing (Cluster(..), PusherAppKey)
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "***REMOVED***" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "***REMOVED***" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-livePantheon : Model
-livePantheon =
-    { backendUrl = "https://***REMOVED***"
-    , name = "livePantheon"
-    , pusherKey = PusherAppKey "***REMOVED***" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-ehezaGlobal : Model
-ehezaGlobal =
-    { backendUrl = "https://***REMOVED***"
-    , name = "eheza-global"
-    , pusherKey = PusherAppKey "***REMOVED***" UsEast1
-    , debug = False
-    , sandbox = False
-    }
 
 
 ***REMOVED*** : Model
 ***REMOVED*** =
     { backendUrl = "https://***REMOVED***"
     , name = "live-aos"
-    , pusherKey = PusherAppKey "***REMOVED***" EuWest1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-
-    -- We're not actually using Pusher at the moment, so just filling in a
-    -- blank key for now.
-    , pusherKey = PusherAppKey "" UsEast1
-    , debug = False
-    , sandbox = True
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "" UsEast1
-    , debug = False
-    , sandbox = False
-    }
-
-
-***REMOVED*** : Model
-***REMOVED*** =
-    { backendUrl = "https://***REMOVED***"
-    , name = "***REMOVED***"
-    , pusherKey = PusherAppKey "" UsEast1
     , debug = False
     , sandbox = False
     }
@@ -122,16 +17,5 @@ ehezaGlobal =
 configs : Dict String Model
 configs =
     Dict.fromList
-        [ ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", livePantheon )
-        , ( "***REMOVED***", ehezaGlobal )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        , ( "***REMOVED***", ***REMOVED*** )
-        ]
+        [ ( "***REMOVED***", ***REMOVED*** ) ]
         |> Dict.union localConfigs
