3945ecfe04fa7f02eceea63efa04d9edf4e51d18
Anatoly <anatoly@gizra.com>
2021-11-29 12:50:53 +0200

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalUrineDipstickTest.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulPrenatalUrineDipstickTest.
+ */
+
+/**
+ * Class HedleyRestfulPrenatalUrineDipstickTest.
+ */
+class HedleyRestfulPrenatalUrineDipstickTest extends HedleyRestfulPrenatalActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_test_execution_note',
+    'field_execution_date',
+    'field_protein',
+    'field_ph',
+    'field_glucose',
+    'field_leukocytes',
+    'field_nitrite',
+    'field_urobilinogen',
+    'field_haemoglobin',
+    'field_specific_gravity',
+    'field_ketone',
+    'field_bilirubin',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dateFields = [
+    'field_execution_date',
+  ];
+
+}
