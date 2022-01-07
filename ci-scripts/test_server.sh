491183711f84b1579fb276dbf4ceab641ab71790
Aron Novak <aron@gizra.com>
2022-01-07 08:18:41 +0100

    test execution

--- ci-scripts/test_server.sh
+++ ci-scripts/test_server.sh
@@ -8,6 +8,5 @@ set -euo pipefail
 # ---------------------------------------------------------------------------- #
 
 # Simple Docker run to execute Behat.
-mkdir -p "$CIRCLE_WORKING_DIRECTORY"/travis-cache
-docker run -v "$CIRCLE_WORKING_DIRECTORY"/travis-cache:/tmp/travis-cache -it -e "BUILD_WEBDRIVERIO=0" -p 8080:80 server
+docker run -e "BUILD_WEBDRIVERIO=0" -p 8080:80 server
 exit $?
