be9c8b79294a7c1d5eeb0bd0cdab861c2a83466c
Anatoly <anatoly@gizra.com>
2022-02-13 15:44:28 +0200

    Expose with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPrenatalEncounter.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/HedleyRestfulPrenatalEncounter.class.php
@@ -20,6 +20,10 @@ class HedleyRestfulPrenatalEncounter extends HedleyRestfulIndividualEncounter {
       'property' => 'field_prenatal_encounter_type',
     ];
 
+    $public_fields['prenatal_diagnosis'] = [
+      'property' => 'field_prenatal_diagnosis',
+    ];
+
     return $public_fields;
   }
 
@@ -31,11 +35,27 @@ class HedleyRestfulPrenatalEncounter extends HedleyRestfulIndividualEncounter {
 
     $field_names = [
       'field_prenatal_encounter_type',
+      'prenatal_diagnosis',
     ];
 
     foreach ($field_names as $field_name) {
       hedley_general_join_field_to_query($query, 'node', $field_name, FALSE);
     }
+
+    $query->addExpression("GROUP_CONCAT(DISTINCT field_prenatal_diagnosis.field_prenatal_diagnosis_value)", 'field_prenatal_diagnosis');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      $item->prenatal_diagnosis = explode(',', $item->prenatal_diagnosis);
+    }
+
+    return $items;
   }
 
 }
