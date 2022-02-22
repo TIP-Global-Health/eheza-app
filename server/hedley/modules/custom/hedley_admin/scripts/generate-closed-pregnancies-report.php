b1223b6f52a2c250e69a405624aa30d956705c82
Adam H. Stewart <adam@gizra.com>
2022-02-22 09:14:28 -0600

    Code formatting.

--- server/hedley/modules/custom/hedley_admin/scripts/generate-closed-pregnancies-report.php
+++ server/hedley/modules/custom/hedley_admin/scripts/generate-closed-pregnancies-report.php
@@ -7,48 +7,48 @@
 
 require_once __DIR__ . '/report_common.inc';
 
-$limit_date = drush_get_option('limit_date', false);
+$limit_date = drush_get_option('limit_date', FALSE);
 if (!$limit_date) {
-    drush_print('Please specify --limit_date option');
-    exit;
+  drush_print('Please specify --limit_date option');
+  exit;
 }
 
 drush_print("# Closed pregnancies report  - " . $limit_date);
 
 $queries = [
   "Outcomes of completed pregnancies (30 days beyond EDD)" => "
-SELECT
-  field_outcome_value AS type, COUNT(*) AS counter
-FROM
-  field_data_field_outcome fo
-LEFT JOIN
-  node ON fo.entity_id = node.nid
-WHERE field_outcome_value NOT IN ('referred-to-hc', 'illness-resolved') AND
-FROM_UNIXTIME(node.changed) < '$limit_date'
-GROUP BY
- field_outcome_value
-",
-  "Outcome Location" => "
-SELECT
-  field_outcome_location_value AS type, COUNT(*) AS counter
-FROM
-  field_data_field_outcome_location fol
-LEFT JOIN
-  node ON fol.entity_id = node.nid
-WHERE FROM_UNIXTIME(node.changed) < '$limit_date'
-GROUP BY
-  field_outcome_location_value;",
+  SELECT
+    field_outcome_value AS type, COUNT(*) AS counter
+  FROM
+    field_data_field_outcome fo
+  LEFT JOIN
+    node ON fo.entity_id = node.nid
+  WHERE field_outcome_value NOT IN ('referred-to-hc', 'illness-resolved') AND
+  FROM_UNIXTIME(node.changed) < '$limit_date'
+  GROUP BY
+   field_outcome_value
+  ",
+    "Outcome Location" => "
+  SELECT
+    field_outcome_location_value AS type, COUNT(*) AS counter
+  FROM
+    field_data_field_outcome_location fol
+  LEFT JOIN
+    node ON fol.entity_id = node.nid
+  WHERE FROM_UNIXTIME(node.changed) < '$limit_date'
+  GROUP BY
+    field_outcome_location_value;",
 ];
 
 foreach ($queries as $label => $query) {
-    $table = new HedleyAdminTextTable([$label, 'Counter']);
-    $results = db_query($query)->fetchAll(PDO::FETCH_ASSOC);
-    $data = [];
-    foreach ($results as $result) {
-        $data[] = [
-        $result['type'],
-        $result['counter'],
-        ];
-    }
-    drush_print($table->render($data));
+  $table = new HedleyAdminTextTable([$label, 'Counter']);
+  $results = db_query($query)->fetchAll(PDO::FETCH_ASSOC);
+  $data = [];
+  foreach ($results as $result) {
+    $data[] = [
+    $result['type'],
+    $result['counter'],
+    ];
+  }
+  drush_print($table->render($data));
 }
