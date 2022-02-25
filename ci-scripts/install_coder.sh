fd54f11da508cdef271113653c73a79c2d66e8af
Aron Novak <aron@gizra.com>
2022-02-25 08:44:55 +0100

    fix local coder

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -7,11 +7,11 @@ set -e
 #
 # ---------------------------------------------------------------------------- #
 
-COMPOSER_MEMORY_LIMIT=-1 composer global require squizlabs/php_codesniffer:3.5.6
+COMPOSER_MEMORY_LIMIT=-1 composer global require squizlabs/php_codesniffer:3.6.2
 COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:8.3.9
 if [[ -f ~/.composer/vendor/bin/phpcs ]]
 then
-  ~/.composer/vendor/bin/phpcs --config-set installed_path "$HOME"/.composer/vendor/drupal/coder/coder_sniffer
+  ~/.composer/vendor/bin/phpcs --config-set installed_paths "$HOME"/.composer/vendor/drupal/coder/coder_sniffer
 else
   phpcs --config-set installed_paths "$HOME"/.config/composer/vendor/drupal/coder/coder_sniffer
 fi
