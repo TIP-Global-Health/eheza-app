c529802a09efd2c90b52fc057075ff0b89becf6d
Anatoly <anatoly@gizra.com>
2022-03-02 15:45:02 +0200

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalRecommendedTreatment.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulPrenatalRecommendedTreatment.
+ */
+
+/**
+ * Class HedleyRestfulPrenatalRecommendedTreatment.
+ */
+class HedleyRestfulPrenatalRecommendedTreatment extends HedleyRestfulPrenatalActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_recommended_treatment',
+  ];
+
+}
