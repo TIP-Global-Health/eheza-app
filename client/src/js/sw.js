e4aaa6babc7ca03a4b0102a4e2233ae44fa39d15
Anatoly <anatoly@gizra.com>
2022-03-02 15:46:01 +0200

    Introduce to JS layer [ci skip]

--- client/src/js/sw.js
+++ client/src/js/sw.js
@@ -120,6 +120,7 @@ var tableForType = {
     prenatal_medication_distribution: 'shards',
     prenatal_nutrition: 'shards',
     prenatal_random_blood_sugar_test: 'shards',
+    prenatal_recommended_treatment: 'shards',
     prenatal_send_to_hc: 'shards',
     prenatal_syphilis_test: 'shards',
     prenatal_urine_dipstick_test: 'shards',
