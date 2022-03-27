626ca4ec5108d7248479a0357e74ac9f3de54aaa
Anatoly <anatoly@gizra.com>
2022-03-27 14:32:07 +0300

    Replicate #166

--- server/hedley/modules/custom/hedley_admin/scripts/generate-closed-pregnancies-report.php
+++ server/hedley/modules/custom/hedley_admin/scripts/generate-closed-pregnancies-report.php
@@ -7,28 +7,22 @@
 
 require_once __DIR__ . '/report_common.inc';
 
+$limit_date = drush_get_option('limit_date', FALSE);
+if (!$limit_date) {
+  drush_print('Please specify --limit_date option');
+  exit;
+}
+
+drush_print("# Closed pregnancies report - " . $limit_date);
+
 $queries = [
-  "Outcomes of completed pregnancies (30 days beyond EDD)" => "
-SELECT
-  field_outcome_value AS type, COUNT(*) AS counter
-FROM
-  field_data_field_outcome
-WHERE field_outcome_value NOT IN ('referred-to-hc', 'illness-resolved')
-GROUP BY
- field_outcome_value
-",
-  "Outcome Location" => "
-SELECT
-  field_outcome_location_value AS type, COUNT(*) AS counter
-FROM
-  field_data_field_outcome_location
-GROUP BY
-  field_outcome_location_value;",
+  "Outcomes of completed pregnancies (30 days beyond EDD)" => file_get_contents(__DIR__ . '/closed-pregnancies-outcome-completed-pregnancies.SQL'),
+  "Outcome Location" => file_get_contents(__DIR__ . '/closed-pregnancies-outcome-location.SQL'),
 ];
 
 foreach ($queries as $label => $query) {
   $table = new HedleyAdminTextTable([$label, 'Counter']);
-  $results = db_query($query)->fetchAll(PDO::FETCH_ASSOC);
+  $results = db_query($query, [':limit' => $limit_date])->fetchAll(PDO::FETCH_ASSOC);
   $data = [];
   foreach ($results as $result) {
     $data[] = [
