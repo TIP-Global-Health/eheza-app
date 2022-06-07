5904f22a4fbd55ab9b726a28d059f277d702ca15
Anatoly <anatoly@gizra.com>
2022-06-07 15:48:41 +0300

    Expose with Restful [ci skip]

---
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalMentalHealth.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HedleyRestfulPrenatalMentalHealth.
+ */
+
+/**
+ * Class HedleyRestfulPrenatalMentalHealth.
+ */
+class HedleyRestfulPrenatalMentalHealth extends HedleyRestfulPrenatalActivityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_mental_health_signs',
+  ];
+
+}
