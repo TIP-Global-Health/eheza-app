22be2050ab87607a4497684d413aadf29a2ecd6e
Anatoly <anatoly@gizra.com>
2022-06-09 09:56:50 +0300

    Expose field with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalMentalHealth.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulPrenatalMentalHealth.class.php
@@ -13,8 +13,28 @@ class HedleyRestfulPrenatalMentalHealth extends HedleyRestfulPrenatalActivityBas
   /**
    * {@inheritdoc}
    */
+  protected $fields = [
+    'field_specialist_at_hc',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
   protected $multiFields = [
     'field_mental_health_signs',
   ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function postExecuteQueryForViewWithDbSelect(array $items = []) {
+    $items = parent::postExecuteQueryForViewWithDbSelect($items);
+
+    foreach ($items as &$item) {
+      $item->specialist_at_hc = (bool) $item->specialist_at_hc;
+    }
+
+    return $items;
+  }
 
 }
