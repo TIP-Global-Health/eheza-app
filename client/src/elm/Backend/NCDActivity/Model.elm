882b358f082bb660551fb4e1f181d4cb8c827cf6
Anatoly <anatoly@gizra.com>
2022-08-22 20:06:59 +0300

    Define NCD activities [ci skip]

---
+++ client/src/elm/Backend/NCDActivity/Model.elm
@@ -0,0 +1,11 @@
+module Backend.NCDActivity.Model exposing (..)
+
+
+type NCDActivity
+    = DangerSigns
+    | Examination
+    | FamilyPlanning
+    | Laboratory
+    | MedicalHistory
+    | NextSteps
+    | SymptomReview
---
+++ client/src/elm/Backend/NCDActivity/Utils.elm
@@ -0,0 +1,75 @@
+module Backend.NCDActivity.Utils exposing (..)
+
+import Backend.NCDActivity.Model exposing (..)
+
+
+activityToString : NCDActivity -> String
+activityToString activity =
+    case activity of
+        DangerSigns ->
+            "danger-signs"
+
+        Examination ->
+            "examination"
+
+        FamilyPlanning ->
+            "planning"
+
+        Laboratory ->
+            "laboratory"
+
+        MedicalHistory ->
+            "history"
+
+        NextSteps ->
+            "next-steps"
+
+        SymptomReview ->
+            "symptoms"
+
+
+{-| The inverse of encodeActivityTypeAsString
+-}
+activityFromString : String -> Maybe NCDActivity
+activityFromString s =
+    case s of
+        "danger-signs" ->
+            Just DangerSigns
+
+        "examination" ->
+            Just Examination
+
+        "planning" ->
+            Just FamilyPlanning
+
+        "laboratory" ->
+            Just Laboratory
+
+        "history" ->
+            Just MedicalHistory
+
+        "next-steps" ->
+            Just NextSteps
+
+        "symptoms" ->
+            Just SymptomReview
+
+        _ ->
+            Nothing
+
+
+getActivityIcon : NCDActivity -> String
+getActivityIcon activity =
+    activityToString activity
+
+
+getAllActivities : List NCDActivity
+getAllActivities =
+    [ DangerSigns
+    , Examination
+    , FamilyPlanning
+    , Laboratory
+    , MedicalHistory
+    , NextSteps
+    , SymptomReview
+    ]
