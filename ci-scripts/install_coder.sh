19accbdd2b8dca31db4434d0efb4d363006e4796
Aron Novak <aron@gizra.com>
2022-08-25 07:02:26 +0200

    bump coder

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -8,7 +8,7 @@ set -e
 # ---------------------------------------------------------------------------- #
 
 COMPOSER_MEMORY_LIMIT=-1 composer global require squizlabs/php_codesniffer:3.6.2
-COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:8.3.9
+COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:8.3.10
 if [[ -f ~/.composer/vendor/bin/phpcs ]]
 then
   ~/.composer/vendor/bin/phpcs --config-set installed_paths "$HOME"/.composer/vendor/drupal/coder/coder_sniffer
