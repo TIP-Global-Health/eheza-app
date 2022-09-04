6abd54c226381d5d6dd381de7483cecec811cf3d
Anatoly <anatoly@gizra.com>
2022-09-04 19:15:40 +0300

    Expose with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDHIVTest.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDHIVTest.class.php
@@ -10,4 +10,27 @@
  */
 class HedleyRestfulNCDHIVTest extends HedleyRestfulNCDActivityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_test_execution_note',
+    'field_execution_date',
+    'field_test_result',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_hiv_signs',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dateFields = [
+    'field_execution_date',
+  ];
+
 }
--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDUrineDipstickTest.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDUrineDipstickTest.class.php
@@ -10,4 +10,29 @@
  */
 class HedleyRestfulNCDUrineDipstickTest extends HedleyRestfulNCDActivityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_test_variant',
+    'field_test_execution_note',
+    'field_execution_date',
+    'field_protein',
+    'field_ph',
+    'field_glucose',
+    'field_leukocytes',
+    'field_nitrite',
+    'field_urobilinogen',
+    'field_haemoglobin',
+    'field_ketone',
+    'field_bilirubin',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dateFields = [
+    'field_execution_date',
+  ];
+
 }
