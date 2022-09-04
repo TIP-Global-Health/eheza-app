3cb6f63fb8ad85346c33467ad7af9ecc7453f57b
Anatoly <anatoly@gizra.com>
2022-09-04 19:32:05 +0300

    Expose with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDLabsResults.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulNCDLabsResults.class.php
@@ -10,4 +10,26 @@
  */
 class HedleyRestfulNCDLabsResults extends HedleyRestfulNCDActivityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_date_concluded',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_performed_tests',
+    'field_completed_tests',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dateFields = [
+    'field_date_concluded',
+  ];
+
 }
