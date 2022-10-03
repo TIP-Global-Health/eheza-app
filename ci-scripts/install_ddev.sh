cb070eec7fe8ab4ee1879db633f27e4bb47842c3
Aron Novak <aron@gizra.com>
2022-10-03 06:03:01 +0200

    install script for mac

--- ci-scripts/install_ddev.sh
+++ ci-scripts/install_ddev.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+MAC=0
+if ! command -v docker &> /dev/null
+then
+    echo "docker could not be found, trying to install it on Mac"
+    brew install colima
+    colima start --cpu 4 --memory 4 --dns=1.1.1.1
+    MAC=1
+fi
+
 echo "Install ddev."
 curl -s -L https://raw.githubusercontent.com/drud/ddev/master/scripts/install_ddev.sh | bash
 
@@ -8,3 +17,10 @@ echo "Configuring ddev."
 mkdir ~/.ddev
 cp "ci-scripts/global_config.yaml" ~/.ddev/
 docker network create ddev_default || ddev logs
+
+if [[ "$MAC" == 1 ]];
+then
+  ddev config global --mutagen-enabled
+fi
+
+export MAC
