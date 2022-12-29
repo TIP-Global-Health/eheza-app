036c9152f8d1e0973055debf0927506c8bc3f66f
Anatoly <anatoly@gizra.com>
2022-12-29 21:23:38 +0200

    Scripts that sends SMS message [ci skip]

---
+++ server/hedley/modules/custom/hedley_whatsapp/scripts/send-messages.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+/**
+ * @file
+ * Locates duplicates by UUID and deletes all, but first one.
+ *
+ * Execution: drush scr
+ *   profiles/hedley/modules/custom/hedley_whatsapp/scripts/send-messages.php.
+ */
+
+use CMText\TextClient;
+
+if (!drupal_is_cli()) {
+  // Prevent execution from browser.
+  return;
+}
+
+$client = new TextClient('cac034a9-0c8b-47dd-b1d2-55cb23fd5349');
+$result = $client->SendMessage('Hello world!', 'TIP Health', [ '00972546925278' ]);
+
+drush_print("Status code: $result->statusCode");
+drush_print("Status message: $result->statusMessage");
