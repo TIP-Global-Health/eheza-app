33daf28d4759e2cdf9993bfdc6117b3bfbaaba0e
Anatoly <anatoly@gizra.com>
2023-04-02 13:58:48 +0300

    Bind and clear using ports [ci skip]

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1205,6 +1205,93 @@ elmApp.ports.bindDropZone.subscribe(function() {
   waitForElement('dropzone', attachDropzone, null);
 });
 
+
+// Signature Pad.
+
+// https://github.com/szimek/signature_pad
+var signaturePad = undefined;
+var canvas = undefined;
+var signaturePad = undefined;
+
+var signaturePadSelector = 'signature-pad';
+
+elmApp.ports.bindSignaturePad.subscribe(function() {
+  waitForElement(signaturePadSelector, attachSignaturePad, null);
+});
+
+elmApp.ports.clearSignaturePad.subscribe(function() {
+  if (signaturePad === undefined) {
+    return;
+  }
+  signaturePad.clear();
+});
+
+function attachSignaturePad() {
+  const wrapper = document.getElementById("signature-pad");
+  // const clearButton = wrapper.querySelector("[data-action=clear]");
+  canvas = wrapper.querySelector("canvas");
+  signaturePad = new SignaturePad(canvas, {
+    // It's Necessary to use an opaque color when saving image as JPEG;
+    // this option can be omitted if only saving as PNG or SVG
+    backgroundColor: 'rgb(255, 255, 255)'
+  });
+
+  // Adjust canvas coordinate space taking into account pixel ratio,
+  // to make it look crisp on mobile devices.
+  // This also causes canvas to be cleared.
+  function resizeCanvas() {
+    // When zoomed out to less than 100%, for some very strange reason,
+    // some browsers report devicePixelRatio as less than 1
+    // and only part of the canvas is cleared then.
+    const ratio =  Math.max(window.devicePixelRatio || 1, 1);
+
+    // This part causes the canvas to be cleared
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear();
+  }
+
+  window.onresize = resizeCanvas;
+  resizeCanvas();
+
+  // signaturePadCanvas = document.querySelector(signaturePadSelector);
+  // console.log(signaturePadCanvas);
+  // if (signaturePadCanvas === null) {
+  //   console.err('Signature widget failure');
+  //   return;
+  // }
+  // signaturePadElement = document.querySelector('#signatureCanvas');
+  // signaturePad = new SignaturePad(signaturePadCanvas, {
+    // 'onEnd': function () {
+    //   var event = makeCustomEvent("signatureupdate", {
+    //     file: signaturePad.toDataURL("image/svg+xml")
+    //   });
+    //
+    //   signaturePadElement.dispatchEvent(event);
+    // }
+  // });
+  // var signature_data = signaturePadElement.getAttribute('data-signature');
+  // if (signature_data.length > 0) {
+  //   signaturePadElement.fromDataURL(signature_data);
+  // }
+  // window.addEventListener("resize", signaturePadResizeCanvas);
+  // signaturePadResizeCanvas();
+}
+
+
+
+// On mobile devices it might make more sense to listen to orientation change,
+// rather than window resize events.
+
+// function signaturePadResizeCanvas() {
+//   var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+//   signaturePadCanvas.width = signaturePadCanvas.offsetWidth * ratio;
+//   signaturePadCanvas.height = signaturePadCanvas.offsetHeight * ratio;
+//   signaturePadCanvas.getContext("2d").scale(ratio, ratio);
+//   signaturePad.clear();
+// }
+
 /**
  * Wait for id to appear before invoking related functions.
  */
