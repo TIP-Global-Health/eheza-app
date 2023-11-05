00212d28d391285d0e2316b71abc2a92f16a7eca
Anatoly <anatoly@gizra.com>
2023-11-06 01:08:26 +0200

    Code addaptation for global context [ci skip]

--- client/src/elm/Measurement/Model.elm
+++ client/src/elm/Measurement/Model.elm
@@ -1277,3 +1277,76 @@ type ImmunisationTask
     | TaskPCV13
     | TaskRotarix
     | TaskOverview
+
+
+type alias NutritionFeedingForm =
+    { receiveSupplement : Maybe Bool
+    , rationPresentAtHome : Maybe Bool
+    , enoughTillNextSession : Maybe Bool
+    , supplementShared : Maybe Bool
+    , encouragedToEat : Maybe Bool
+    , refusingToEat : Maybe Bool
+    , breastfeeding : Maybe Bool
+    , cleanWaterAvailable : Maybe Bool
+    , eatenWithWater : Maybe Bool
+    , supplementType : Maybe NutritionSupplementType
+    , sachetsPerDay : Maybe Float
+    }
+
+
+emptyNutritionFeedingForm : NutritionFeedingForm
+emptyNutritionFeedingForm =
+    { receiveSupplement = Nothing
+    , rationPresentAtHome = Nothing
+    , enoughTillNextSession = Nothing
+    , supplementShared = Nothing
+    , encouragedToEat = Nothing
+    , refusingToEat = Nothing
+    , breastfeeding = Nothing
+    , cleanWaterAvailable = Nothing
+    , eatenWithWater = Nothing
+    , supplementType = Nothing
+    , sachetsPerDay = Nothing
+    }
+
+
+type alias NutritionHygieneForm =
+    { soapInTheHouse : Maybe Bool
+    , washHandsBeforeFeeding : Maybe Bool
+    , foodCovered : Maybe Bool
+    , mainWaterSource : Maybe MainWaterSource
+    , waterPreparationOption : Maybe WaterPreparationOption
+    }
+
+
+emptyNutritionHygieneForm : NutritionHygieneForm
+emptyNutritionHygieneForm =
+    { soapInTheHouse = Nothing
+    , washHandsBeforeFeeding = Nothing
+    , foodCovered = Nothing
+    , mainWaterSource = Nothing
+    , waterPreparationOption = Nothing
+    }
+
+
+type alias NutritionFoodSecurityForm =
+    { householdGotFood : Maybe Bool
+    , mainIncomeSource : Maybe MainIncomeSource
+    }
+
+
+emptyNutritionFoodSecurityForm : NutritionFoodSecurityForm
+emptyNutritionFoodSecurityForm =
+    NutritionFoodSecurityForm Nothing Nothing
+
+
+type alias NutritionCaringForm =
+    { caringOption : Maybe CaringOption
+    , parentHealth : Maybe Bool
+    , childClean : Maybe Bool
+    }
+
+
+emptyNutritionCaringForm : NutritionCaringForm
+emptyNutritionCaringForm =
+    NutritionCaringForm Nothing Nothing Nothing
--- client/src/elm/Measurement/Utils.elm
+++ client/src/elm/Measurement/Utils.elm
@@ -6076,3 +6076,163 @@ muacMeasurementIsOff =
     -- MUAC is not green.
     Maybe.map (muacIndication >> (/=) ColorAlertGreen)
         >> Maybe.withDefault False
+
+
+nutritionFeedingFormWithDefault : NutritionFeedingForm -> Maybe NutritionFeedingValue -> NutritionFeedingForm
+nutritionFeedingFormWithDefault form saved =
+    saved
+        |> unwrap
+            form
+            (\value ->
+                { receiveSupplement = or form.receiveSupplement (EverySet.member ReceiveSupplement value.signs |> Just)
+                , rationPresentAtHome = or form.rationPresentAtHome (EverySet.member RationPresentAtHome value.signs |> Just)
+                , enoughTillNextSession = or form.enoughTillNextSession (EverySet.member EnoughTillNextSession value.signs |> Just)
+                , supplementShared = or form.supplementShared (EverySet.member SupplementShared value.signs |> Just)
+                , encouragedToEat = or form.encouragedToEat (EverySet.member EncouragedToEat value.signs |> Just)
+                , refusingToEat = or form.refusingToEat (EverySet.member RefusingToEat value.signs |> Just)
+                , breastfeeding = or form.breastfeeding (EverySet.member FeedingSignBreastfeeding value.signs |> Just)
+                , cleanWaterAvailable = or form.cleanWaterAvailable (EverySet.member CleanWaterAvailable value.signs |> Just)
+                , eatenWithWater = or form.eatenWithWater (EverySet.member EatenWithWater value.signs |> Just)
+                , supplementType = or form.supplementType (Just value.supplementType)
+                , sachetsPerDay = or form.sachetsPerDay (Just value.sachetsPerDay)
+                }
+            )
+
+
+toNutritionFeedingValueWithDefault : Maybe NutritionFeedingValue -> NutritionFeedingForm -> Maybe NutritionFeedingValue
+toNutritionFeedingValueWithDefault saved form =
+    nutritionFeedingFormWithDefault form saved
+        |> toNutritionFeedingValue
+
+
+toNutritionFeedingValue : NutritionFeedingForm -> Maybe NutritionFeedingValue
+toNutritionFeedingValue form =
+    let
+        signs =
+            [ ifNullableTrue ReceiveSupplement form.receiveSupplement
+            , ifNullableTrue RationPresentAtHome form.rationPresentAtHome
+            , ifNullableTrue EnoughTillNextSession form.enoughTillNextSession
+            , ifNullableTrue SupplementShared form.supplementShared
+            , ifNullableTrue EncouragedToEat form.encouragedToEat
+            , ifNullableTrue RefusingToEat form.refusingToEat
+            , ifNullableTrue FeedingSignBreastfeeding form.breastfeeding
+            , ifNullableTrue CleanWaterAvailable form.cleanWaterAvailable
+            , ifNullableTrue EatenWithWater form.eatenWithWater
+            ]
+                |> Maybe.Extra.combine
+                |> Maybe.map (List.foldl EverySet.union EverySet.empty >> ifEverySetEmpty NoNutritionFeedingSigns)
+
+        supplementType =
+            form.supplementType
+                |> Maybe.withDefault NoNutritionSupplementType
+                |> Just
+
+        sachetsPerDay =
+            form.sachetsPerDay
+                |> Maybe.withDefault 0
+                |> Just
+    in
+    Maybe.map NutritionFeedingValue signs
+        |> andMap supplementType
+        |> andMap sachetsPerDay
+
+
+nutritionHygieneFormWithDefault : NutritionHygieneForm -> Maybe NutritionHygieneValue -> NutritionHygieneForm
+nutritionHygieneFormWithDefault form saved =
+    saved
+        |> unwrap
+            form
+            (\value ->
+                { soapInTheHouse = or form.soapInTheHouse (EverySet.member SoapInTheHouse value.signs |> Just)
+                , washHandsBeforeFeeding = or form.washHandsBeforeFeeding (EverySet.member WashHandsBeforeFeeding value.signs |> Just)
+                , foodCovered = or form.foodCovered (EverySet.member FoodCovered value.signs |> Just)
+                , mainWaterSource = or form.mainWaterSource (Just value.mainWaterSource)
+                , waterPreparationOption = or form.waterPreparationOption (Just value.waterPreparationOption)
+                }
+            )
+
+
+toNutritionHygieneValueWithDefault : Maybe NutritionHygieneValue -> NutritionHygieneForm -> Maybe NutritionHygieneValue
+toNutritionHygieneValueWithDefault saved form =
+    nutritionHygieneFormWithDefault form saved
+        |> toNutritionHygieneValue
+
+
+toNutritionHygieneValue : NutritionHygieneForm -> Maybe NutritionHygieneValue
+toNutritionHygieneValue form =
+    let
+        signs =
+            [ ifNullableTrue SoapInTheHouse form.soapInTheHouse
+            , ifNullableTrue WashHandsBeforeFeeding form.washHandsBeforeFeeding
+            , ifNullableTrue FoodCovered form.foodCovered
+            ]
+                |> Maybe.Extra.combine
+                |> Maybe.map (List.foldl EverySet.union EverySet.empty >> ifEverySetEmpty NoNutritionHygieneSigns)
+    in
+    Maybe.map NutritionHygieneValue signs
+        |> andMap form.mainWaterSource
+        |> andMap form.waterPreparationOption
+
+
+nutritionFoodSecurityFormWithDefault : NutritionFoodSecurityForm -> Maybe NutritionFoodSecurityValue -> NutritionFoodSecurityForm
+nutritionFoodSecurityFormWithDefault form saved =
+    saved
+        |> unwrap
+            form
+            (\value ->
+                { householdGotFood = or form.householdGotFood (EverySet.member HouseholdGotFood value.signs |> Just)
+                , mainIncomeSource = or form.mainIncomeSource (Just value.mainIncomeSource)
+                }
+            )
+
+
+toNutritionFoodSecurityValueWithDefault : Maybe NutritionFoodSecurityValue -> NutritionFoodSecurityForm -> Maybe NutritionFoodSecurityValue
+toNutritionFoodSecurityValueWithDefault saved form =
+    nutritionFoodSecurityFormWithDefault form saved
+        |> toNutritionFoodSecurityValue
+
+
+toNutritionFoodSecurityValue : NutritionFoodSecurityForm -> Maybe NutritionFoodSecurityValue
+toNutritionFoodSecurityValue form =
+    let
+        signs =
+            [ ifNullableTrue HouseholdGotFood form.householdGotFood
+            ]
+                |> Maybe.Extra.combine
+                |> Maybe.map (List.foldl EverySet.union EverySet.empty >> ifEverySetEmpty NoNutritionFoodSecuritySigns)
+    in
+    Maybe.map NutritionFoodSecurityValue signs
+        |> andMap form.mainIncomeSource
+
+
+nutritionCaringFormWithDefault : NutritionCaringForm -> Maybe NutritionCaringValue -> NutritionCaringForm
+nutritionCaringFormWithDefault form saved =
+    saved
+        |> unwrap
+            form
+            (\value ->
+                { parentHealth = or form.parentHealth (EverySet.member ParentsAliveHealthy value.signs |> Just)
+                , childClean = or form.childClean (EverySet.member ChildClean value.signs |> Just)
+                , caringOption = or form.caringOption (value.caringOption |> Just)
+                }
+            )
+
+
+toNutritionCaringValueWithDefault : Maybe NutritionCaringValue -> NutritionCaringForm -> Maybe NutritionCaringValue
+toNutritionCaringValueWithDefault saved form =
+    nutritionCaringFormWithDefault form saved
+        |> toNutritionCaringValue
+
+
+toNutritionCaringValue : NutritionCaringForm -> Maybe NutritionCaringValue
+toNutritionCaringValue form =
+    let
+        signs =
+            [ ifNullableTrue ParentsAliveHealthy form.parentHealth
+            , ifNullableTrue ChildClean form.childClean
+            ]
+                |> Maybe.Extra.combine
+                |> Maybe.map (List.foldl EverySet.union EverySet.empty >> ifEverySetEmpty NoCaringSigns)
+    in
+    Maybe.map NutritionCaringValue signs
+        |> andMap form.caringOption
