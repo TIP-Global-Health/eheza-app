7705df5905c2a5aaf7453d83712fc792b019a223
anvmn <anatoly@gizra.com>
2023-12-13 16:54:39 +0200

    Update recalculation script [ci skip]

--- server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
+++ server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
@@ -64,6 +64,7 @@ while (TRUE) {
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_NUTRITION);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_PRENATAL);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_ACUTE_ILLNESS);
+    hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_NCD);
 
     // Add AQ item to re-calculate all the stats offline.
     hedley_general_add_task_to_advanced_queue_by_id(HEDLEY_STATS_CALCULATE_STATS, $id, [
