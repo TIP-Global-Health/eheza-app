49f2974599e929888340a02f4ec8b6bcc96625c0
anvmn <anatoly@gizra.com>
2023-12-17 16:36:56 +0200

    Backend infra for generation of PMTCT data [ci skip]

--- server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
+++ server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
@@ -65,6 +65,7 @@ while (TRUE) {
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_PRENATAL);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_ACUTE_ILLNESS);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_NCD);
+    hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_PMTCT);
 
     // Add AQ item to re-calculate all the stats offline.
     hedley_general_add_task_to_advanced_queue_by_id(HEDLEY_STATS_CALCULATE_STATS, $id, [
