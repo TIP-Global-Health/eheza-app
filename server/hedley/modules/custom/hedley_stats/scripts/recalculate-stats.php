908153a024b31e8f34b16b1cd94762ccb7b0b6d1
anvmn <anatoly@gizra.com>
2023-12-21 10:21:37 +0200

    Generate initial SPV data on backend [ci skip]

--- server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
+++ server/hedley/modules/custom/hedley_stats/scripts/recalculate-stats.php
@@ -66,6 +66,7 @@ while (TRUE) {
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_ACUTE_ILLNESS);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_NCD);
     hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_PMTCT);
+    hedley_stats_clear_caches_for_health_center($id, HEDLEY_STATS_SPV);
 
     // Add AQ item to re-calculate all the stats offline.
     hedley_general_add_task_to_advanced_queue_by_id(HEDLEY_STATS_CALCULATE_STATS, $id, [
