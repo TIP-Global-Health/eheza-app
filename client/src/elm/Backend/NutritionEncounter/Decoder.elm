d3540216f4c54c1ba62db7e3c8103e13ca34cb3e
anvmn <anatoly@gizra.com>
2024-01-01 14:35:30 +0200

    Decode and store filed on client + assign encounter type when creating encounter [ci skip]

--- client/src/elm/Backend/NutritionEncounter/Decoder.elm
+++ client/src/elm/Backend/NutritionEncounter/Decoder.elm
@@ -2,9 +2,10 @@ module Backend.NutritionEncounter.Decoder exposing (decodeNutritionEncounter)
 
 import Backend.NutritionEncounter.Model exposing (..)
 import Gizra.NominalDate exposing (decodeYYYYMMDD)
-import Json.Decode exposing (Decoder, nullable, succeed)
+import Json.Decode exposing (Decoder, andThen, fail, nullable, string, succeed)
 import Json.Decode.Pipeline exposing (optional, optionalAt, required, requiredAt)
 import Restful.Endpoint exposing (decodeEntityUuid)
+import Utils.Json exposing (decodeWithFallback)
 
 
 decodeNutritionEncounter : Decoder NutritionEncounter
@@ -13,4 +14,27 @@ decodeNutritionEncounter =
         |> required "individual_participant" decodeEntityUuid
         |> requiredAt [ "scheduled_date", "value" ] decodeYYYYMMDD
         |> optionalAt [ "scheduled_date", "value2" ] (nullable decodeYYYYMMDD) Nothing
+        |> optional "nutrition_encounter_type" (decodeWithFallback NutritionEncounterUnknown decodeNutritionEncounterType) NutritionEncounterUnknown
         |> optional "shard" (nullable decodeEntityUuid) Nothing
+
+
+decodeNutritionEncounterType : Decoder NutritionEncounterType
+decodeNutritionEncounterType =
+    string
+        |> andThen
+            (\encounterType ->
+                case encounterType of
+                    "nurse" ->
+                        succeed NutritionEncounterNurse
+
+                    "chw" ->
+                        succeed NutritionEncounterCHW
+
+                    "unknown" ->
+                        succeed NutritionEncounterUnknown
+
+                    _ ->
+                        fail <|
+                            encounterType
+                                ++ " is not a recognized NutritionEncounterType"
+            )
