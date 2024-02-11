122f2e794a94720076dbaa241fd2b37cba60641e
anvmn <anatoly@gizra.com>
2024-02-11 13:34:47 +0200

    Expose with Restful [ci skip]

--- server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulTuberculosisFollowUp.class.php
+++ server/hedley/modules/custom/hedley_restful/plugins/restful/node/activity/HedleyRestfulTuberculosisFollowUp.class.php
@@ -10,4 +10,25 @@
  */
 class HedleyRestfulTuberculosisFollowUp extends HedleyRestfulTuberculosisActivityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  protected $fields = [
+    'field_date_concluded',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $multiFields = [
+    'field_follow_up_options',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dateFields = [
+    'field_date_concluded',
+  ];
+
 }
