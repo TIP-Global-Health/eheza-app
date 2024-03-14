39840167898d40102a7fd250f90f7c9808b85590
Adam Stewart <adam@gizra.com>
2024-03-14 16:39:20 -0500

    Code sniffer.

--- server/hedley/modules/custom/hedley_admin/scripts/generate-duplicates-report.php
+++ server/hedley/modules/custom/hedley_admin/scripts/generate-duplicates-report.php
@@ -75,35 +75,34 @@ function get_health_center_id($health_center_name = NULL) {
  *
  * @param int $health_center_id
  *   The node ID of a health center.
- *
  * @param string $village
  *   The name of a village.
  *
  * @return array
  *   The node ID of the health center.
  */
-function get_duplicates($village = NULL, $health_center_id) {
-return db_query ("SELECT node.nid, fn.field_first_name_value, sn.field_second_name_value, bd.field_birth_date_value, v.field_village_value
-  FROM field_data_field_birth_date bd
-  LEFT JOIN node ON bd.entity_id = node.nid
-  LEFT JOIN field_data_field_first_name fn ON node.nid = fn.entity_id
-  LEFT JOIN field_data_field_second_name sn ON node.nid = sn.entity_id
-  LEFT JOIN field_data_field_village v ON node.nid = v.entity_id
-  LEFT JOIN field_data_field_health_center hc ON node.nid = hc.entity_id
-  WHERE bd.field_birth_date_value
-    IN (SELECT dbd.field_birth_date_value
-        FROM field_data_field_birth_date dbd
-        LEFT JOIN node dnode ON dbd.entity_id = dnode.nid
-        LEFT JOIN field_data_field_village dv ON dnode.nid = dv.entity_id
-        LEFT JOIN field_data_field_health_center dhc ON dnode.nid = dhc.entity_id
-        WHERE UPPER(dv.field_village_value) = UPPER('$village')
-        AND dhc.field_health_center_target_id= $health_center_id
-        GROUP BY dbd.field_birth_date_value
-        HAVING COUNT(dbd.field_birth_date_value) > 1)
-    AND bd.field_birth_date_value NOT LIKE '%01-01%'
-    AND UPPER(v.field_village_value) = UPPER('$village')
-    AND hc.field_health_center_target_id= $health_center_id
-    ORDER BY `bd`.`field_birth_date_value` DESC")->fetchAll();
+function get_duplicates($health_center_id, $village = NULL) {
+  return db_query("SELECT node.nid, fn.field_first_name_value, sn.field_second_name_value, bd.field_birth_date_value, v.field_village_value
+    FROM field_data_field_birth_date bd
+    LEFT JOIN node ON bd.entity_id = node.nid
+    LEFT JOIN field_data_field_first_name fn ON node.nid = fn.entity_id
+    LEFT JOIN field_data_field_second_name sn ON node.nid = sn.entity_id
+    LEFT JOIN field_data_field_village v ON node.nid = v.entity_id
+    LEFT JOIN field_data_field_health_center hc ON node.nid = hc.entity_id
+    WHERE bd.field_birth_date_value
+      IN (SELECT dbd.field_birth_date_value
+          FROM field_data_field_birth_date dbd
+          LEFT JOIN node dnode ON dbd.entity_id = dnode.nid
+          LEFT JOIN field_data_field_village dv ON dnode.nid = dv.entity_id
+          LEFT JOIN field_data_field_health_center dhc ON dnode.nid = dhc.entity_id
+          WHERE UPPER(dv.field_village_value) = UPPER('$village')
+          AND dhc.field_health_center_target_id= $health_center_id
+          GROUP BY dbd.field_birth_date_value
+          HAVING COUNT(dbd.field_birth_date_value) > 1)
+      AND bd.field_birth_date_value NOT LIKE '%01-01%'
+      AND UPPER(v.field_village_value) = UPPER('$village')
+      AND hc.field_health_center_target_id= $health_center_id
+      ORDER BY `bd`.`field_birth_date_value` DESC")->fetchAll();
 }
 
 /**
@@ -116,7 +115,7 @@ return db_query ("SELECT node.nid, fn.field_first_name_value, sn.field_second_na
  *   The list of villages.
  */
 function get_villages($health_center_id) {
-  return db_query( "SELECT DISTINCT field_village_value
+  return db_query("SELECT DISTINCT field_village_value
     FROM field_data_field_village v
     LEFT JOIN field_data_field_health_center hc ON hc.entity_id = v.entity_id
     WHERE hc.field_health_center_target_id = $health_center_id
@@ -130,7 +129,7 @@ $villages = get_villages($health_center_id);
 // Iterate over each village.
 $data = [];
 foreach ($villages as $village) {
-  // Get the duplicates for this village
+  // Get the duplicates for this village.
   $results = get_duplicates($village, $health_center_id);
 
   // Add teh results to teh $data array.
@@ -147,6 +146,12 @@ foreach ($villages as $village) {
 
 drush_print("# Duplicates report - " . $health_center . "(" . $health_center_id . ")");
 
-$table = new HedleyAdminTextTable(['Node ID', 'First Name', 'Second Name', 'Birth Date', 'Village']);
+$table = new HedleyAdminTextTable([
+  'Node ID',
+  'First Name',
+  'Second Name',
+  'Birth Date',
+  'Village',
+]);
 
 drush_print($table->render($data));
