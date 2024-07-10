3e134c30331d0a407a072795208335a47aec64a4
anvmn <anatoly@gizra.com>
2024-07-10 13:26:56 +0300

    JS var => const [ci skip]

--- server/hedley/modules/custom/hedley_general/js/elm.js
+++ server/hedley/modules/custom/hedley_general/js/elm.js
@@ -10,11 +10,11 @@
    */
   Drupal.behaviors.ElmApps = {
     attach: function (context, settings) {
-      var elmApps = settings.elm_apps;
+      const elmApps = settings.elm_apps;
       // Iterate over the apps.
       Object.keys(elmApps).forEach(function (appName) {
         // appName with unique css ID, e.g. `elm-app--2`.
-        var node = document.getElementById(appName);
+        const node = document.getElementById(appName);
         if (!node) {
           // We haven't found the div, so prevent an error.
           return;
@@ -23,16 +23,16 @@
         const appSettings = elmApps[appName];
 
         // Initiate ELM application.
-        var app = Elm.Main.init({node: node, flags: {
+        const app = Elm.Main.init({node: node, flags: {
           appData: appSettings.data,
           page: appSettings.page,
         }});
 
         if (appSettings.page === 'reports-results') {
           app.ports.downloadCsv.subscribe(function (data) {
-            var filename = data[0];
-            var content = data[1];
-            var element = document.createElement('a');
+            const filename = data[0];
+            const content = data[1];
+            const element = document.createElement('a');
             element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(content));
             element.setAttribute('download', filename);
 
