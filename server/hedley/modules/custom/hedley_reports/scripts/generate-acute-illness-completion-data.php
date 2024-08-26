cfccc0a1e7d3c0a8f7c94b5ef07c96e8e02c516f
anvmn <anatoly@gizra.com>
2024-08-26 16:33:40 +0300

    Fix typo [ci skip]

--- server/hedley/modules/custom/hedley_reports/scripts/generate-acute-illness-completion-data.php
+++ server/hedley/modules/custom/hedley_reports/scripts/generate-acute-illness-completion-data.php
@@ -63,7 +63,7 @@ while (TRUE) {
   $ids = array_keys($result['node']);
   $nodes = node_load_multiple($ids);
   foreach ($nodes as $node) {
-    hedley_reports_generate_completion_data_for_aculte_illness($node, $exclude_set);
+    hedley_reports_generate_completion_data_for_acute_illness($node, $exclude_set);
     $total++;
 
     $memory = round(memory_get_usage() / 1048576);
