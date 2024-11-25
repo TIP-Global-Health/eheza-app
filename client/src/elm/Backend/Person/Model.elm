3e791b53e928d00aea670942aa4f478a4f95dd72
anvmn <anatoly@gizra.com>
2024-11-25 11:54:22 +0200

    Add fields on client side [ci skip]

--- client/src/elm/Backend/Person/Model.elm
+++ client/src/elm/Backend/Person/Model.elm
@@ -27,6 +27,8 @@ type alias Person =
     , sector : Maybe String
     , cell : Maybe String
     , village : Maybe String
+    , registrationLatitude : Maybe Float
+    , registrationLongitude : Maybe Float
     , telephoneNumber : Maybe String
     , healthCenterId : Maybe HealthCenterId
     , deleted : Bool
