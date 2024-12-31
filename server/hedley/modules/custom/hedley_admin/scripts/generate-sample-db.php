1c83538c3b45f42ea238938befbe855c87610532
anvmn <anatoly@gizra.com>
2024-12-31 13:52:16 +0200

    WIP [ci skip]

--- server/hedley/modules/custom/hedley_admin/scripts/generate-sample-db.php
+++ server/hedley/modules/custom/hedley_admin/scripts/generate-sample-db.php
@@ -39,6 +39,67 @@ if ($count == 0) {
 
 drush_print("There are $count nodes at DB.");
 
+// Get all image files from module directory
+$module_path = drupal_get_path('module', 'hedley_migrate');
+$images_path = $module_path . '/images';
+$files = file_scan_directory($images_path, '/\.(jpg|jpeg|png|gif)$/i');
+$files_array = array_values($files);
+// Create a map to track managed files
+$managed_files = [
+  'boy' => [],
+  'girl' => [],
+  'father' => [],
+  'mother' => [],
+];
+foreach ($files_array as $item) {
+  $source = $item->uri;
+  $file = (object) [
+    'uid' => 1,
+    'filename' => basename($source),
+    'uri' => $source,
+    'filemime' => file_get_mimetype($source),
+    'status' => 1,
+  ];
+
+  // Copy file to public directory
+  $directory = 'public://photos';
+  file_prepare_directory($directory, FILE_CREATE_DIRECTORY);
+  $file = file_copy($file, $directory);
+
+  if ($file) {
+    foreach (array_keys($managed_files) as $key) {
+      if (strpos($source, $key) !== FALSE) {
+        $managed_files[$key][] = $file;
+        break;
+      }
+    }
+  }
+  else {
+    drush_print("Failed to copy file: $source", 'error');
+  }
+}
+
+$data = [
+  'sample_health_centers_ids' => [4],
+  'male_first_names' => hedley_migrate_male_first_names(),
+  'female_first_names' => hedley_migrate_female_first_names(),
+  'second_names' => hedley_migrate_second_names(),
+  'bundles_to_delete' => [
+    'child',
+    'counseling_schedule',
+    'counseling_topic',
+    'mother',
+    'participant_form',
+    'report_data',
+    'resilience_survey',
+    'sync_incident',
+    'whatsapp_record',
+    'nurse',
+    'device',
+  ],
+  'managed_files' => $managed_files,
+];
+
 $processed = 0;
 while (TRUE) {
   // Free up memory.
@@ -61,7 +122,7 @@ while (TRUE) {
   $ids = array_keys($result['node']);
   $nodes = node_load_multiple($ids);
   foreach ($nodes as $node) {
-    process_node($node);
+    process_node($node, $data);
   }
 
   $nid = end($ids);
@@ -82,38 +143,21 @@ while (TRUE) {
 
 drush_print("Done!");
 
-function process_node($node) {
+function process_node($node, $data) {
   if ($node->type === 'catchment_area') {
     return;
   }
 
-  $sample_health_centers_ids = [7091, 7092, 28589];
-
-  $bundles_to_delete = [
-    'child',
-    'counseling_schedule',
-    'counseling_topic',
-    'mother',
-    'participant_form',
-    'report_data',
-    'resilience_survey',
-    'sync_incident',
-    'whatsapp_record',
-    'photo',
-    'nutrition_photo',
-    'prenatal_photo',
-    'well_child_photo',
-    'nurse',
-    'device',
-  ];
+  $bundles_to_delete = $data['bundles_to_delete'];
   if (in_array($node->type, $bundles_to_delete)) {
-    node_delete($node);
+    node_delete($node->nid);
     return;
   }
 
+  $sample_health_centers_ids = $data['sample_health_centers_ids'];
   if ($node->type === 'health_center') {
     if (!in_array($node->nid, $sample_health_centers_ids)) {
-      node_delete($node);
+      node_delete($node->nid);
     };
     return;
   }
@@ -121,7 +165,7 @@ function process_node($node) {
   if ($node->type === 'village') {
     $health_center_id = $node->field_health_center[LANGUAGE_NONE][0]['target_id'];
     if (!in_array($health_center_id , $sample_health_centers_ids)) {
-      node_delete($node);
+      node_delete($node->nid);
     };
     return;
   }
@@ -129,27 +173,92 @@ function process_node($node) {
   if ($node->type === 'person') {
     $shards = sanitise_shards($node->field_shards[LANGUAGE_NONE], $sample_health_centers_ids);
     if (empty($shards)) {
-      node_delete($node);
+      node_delete($node->nid);
     }
     else {
       $node->field_shards[LANGUAGE_NONE] = $shards;
+      // Anonymise national ID.
       $node->field_national_id_number[LANGUAGE_NONE][0]['value'] = '';
+      // Anonymise phone number.
       $node->field_phone_number[LANGUAGE_NONE][0]['value'] = '';
-      // @todo: change name
-      node_save($node);
+      // Anonymise name.
+      $gender = $node->field_gender[LANGUAGE_NONE][0]['value'];
+      if (!$gender) {
+        $gender = 'female';
+      }
+      $first_name_key = $gender == 'male' ? array_rand($data['male_first_names']) : array_rand($data['female_first_names']);
+      $first_name = $gender == 'male' ? $data['male_first_names'][$first_name_key] : $data['female_first_names'][$first_name_key];
+      $second_name_key = array_rand($data['second_names']);
+      $second_name = $data['second_names'][$second_name_key];
+      $node->title = $second_name . ' ' . $first_name;
+      $node->field_first_name[LANGUAGE_NONE][0]['value'] = $first_name;
+      $node->field_second_name[LANGUAGE_NONE][0]['value'] = $second_name;
+      // Replace photo.
+      $current_file = field_get_items('node', $node, 'field_photo');
+      if ($current_file) {
+        $file = file_load($current_file[0]['fid']);
+        if ($file) {
+          file_delete($file);
+        }
+      }
+      $birth_date = field_get_items('node', $node, 'field_birth_date');
+      if ($birth_date) {
+        $birth_timestamp = strtotime($birth_date[0]['value']);
+        $age = (time() - $birth_timestamp) / (365 * 24 * 60 * 60);
+        $is_adult = $age >= 13;
+        if ($gender == 'male') {
+          $folder = $is_adult ? 'father' : 'boy';
+        }
+        else {
+          $folder = $is_adult ? 'mother' : 'girl';
+        }
+        $file_key = array_rand($data['managed_files'][$folder]);
+        $file = $data['managed_files'][$folder][$file_key];
+        $node->field_photo[LANGUAGE_NONE][0] = (array) $file;
+      }
+
+      try {
+        node_save($node);
+      }
+      catch (\Exception $e) {
+        drush_print("Error when saving person node ID $node->nid.");
+      }
     }
 
     return;
   }
 
-  // For any other node type:
+  // For any other node type.
   $shards = sanitise_shards($node->field_shards[LANGUAGE_NONE], $sample_health_centers_ids);
   if (empty($shards)) {
-    node_delete($node);
+    node_delete($node->nid);
   }
   else {
     $node->field_shards[LANGUAGE_NONE] = $shards;
-    node_save($node);
+    if (strpos($node->type, 'photo')) {
+      // Replace photo with the one from person profile.
+      $current_file = field_get_items('node', $node, 'field_photo');
+      if ($current_file) {
+        $file = file_load($current_file[0]['fid']);
+        if ($file) {
+          file_delete($file);
+        }
+      }
+      $person_id = $node->field_person[LANGUAGE_NONE][0]['target_id'];
+      $person = node_load($person_id);
+      if ($person) {
+        $photo_field = field_get_items('node', $person, 'field_photo');
+        if ($photo_field) {
+          $node->field_photo[LANGUAGE_NONE][0] = $photo_field[0];
+        }
+      }
+    }
+    try {
+      node_save($node);
+    }
+    catch (\Exception $e) {
+      drush_print("Error when saving node ID $node->nid.");
+    }
   }
 }
 
