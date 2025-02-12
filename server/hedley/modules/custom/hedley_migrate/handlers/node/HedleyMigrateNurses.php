16e85326b6e30590bdee1cdb1ed0565fe20c2f9a
anvmn <anatoly@gizra.com>
2025-02-12 12:25:23 +0200

    Expand nurse migration [ci skip]

--- server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateNurses.php
+++ server/hedley/modules/custom/hedley_migrate/handlers/node/HedleyMigrateNurses.php
@@ -31,10 +31,18 @@ class HedleyMigrateNurses extends HedleyMigrateBase {
         'id',
         'title',
         'field_role',
-        'field_health_centers',
-        'field_villages',
         'field_pin_code',
         'created',
+        // New.
+        'field_resilience_program',
+        'field_resilience_start_date',
+        'field_resilience_role',
+        'field_birth_date',
+        'field_gender',
+        'field_education_level',
+        'field_ubudehe',
+        'field_marital_status',
+        'field_next_reminder',
       ]
     );
   }
@@ -48,6 +56,13 @@ class HedleyMigrateNurses extends HedleyMigrateBase {
     return array_merge(
       $mappings, [
         'field_pin_code',
+        'field_resilience_program',
+        'field_resilience_role',
+        'field_gender',
+        'field_education_level',
+        'field_ubudehe',
+        'field_marital_status',
+        'field_next_reminder',
       ]
     );
   }
@@ -73,21 +88,13 @@ class HedleyMigrateNurses extends HedleyMigrateBase {
   public function __construct($arguments) {
     parent::__construct($arguments);
 
-    $this->dependencies = [
-      'HedleyMigrateClinics',
-      'HedleyMigrateHealthCenters',
-      'HedleyMigrateVillages',
-    ];
+    $this
+      ->addFieldMapping('field_resilience_start_date', 'field_resilience_start_date')
+      ->callbacks([$this, 'dateProcess']);
 
     $this
-      ->addFieldMapping('field_health_centers', 'field_health_centers')
-      ->separator('|')
-      ->sourceMigration('HedleyMigrateHealthCenters');
-
-    $this
-      ->addFieldMapping('field_villages', 'field_villages')
-      ->separator('|')
-      ->sourceMigration('HedleyMigrateVillages');
+      ->addFieldMapping('field_birth_date', 'field_birth_date')
+      ->callbacks([$this, 'dateProcess']);
   }
 
 }
