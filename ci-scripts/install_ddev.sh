b17152255164283425138214a2550791db99c296
Aron Novak <AronNovak@users.noreply.github.com>
2025-03-12 12:28:39 +0100

    Update DDEV inside CI (#1440)
    * bump inside travis
    
    * fix

--- ci-scripts/install_ddev.sh
+++ ci-scripts/install_ddev.sh
@@ -13,7 +13,7 @@ then
 fi
 
 echo "Install ddev."
-curl -s -L https://raw.githubusercontent.com/drud/ddev/master/scripts/install_ddev.sh | bash -s v1.23.2
+curl -s -L https://raw.githubusercontent.com/drud/ddev/master/scripts/install_ddev.sh | bash -s v1.24.3
 
 echo "Configuring ddev."
 mkdir ~/.ddev
