7fc84b1384a826e5dd4368a133abd936d2e3ba74
Amitai Burstein <amitai@gizra.com>
2025-04-05 18:48:29 +0300

    File upload

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -706,7 +706,8 @@ elmApp.ports.askFromIndexDb.subscribe(function(info) {
 
               const uploadUrl = [
                 backendUrl,
-                '/api/file-upload?access_token=',
+                '/api/CreateUploadFile' +
+                '?access_token=',
                 accessToken,
               ].join('');
 
@@ -739,8 +740,9 @@ elmApp.ports.askFromIndexDb.subscribe(function(info) {
               }
 
               const changes = {
-                'fileId': parseInt(json.data[0].id),
-                'remoteFileName': json.data[0].label,
+                'uuid': json.uuid,
+                'fileId': parseInt(json.serial),
+                'remoteFileName': json.signed_url,
                 'isSynced': 1,
               }
 
