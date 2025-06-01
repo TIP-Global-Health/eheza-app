8f0264dfecc2aa73370b38fff4f21971d95b1b09
anvmn <anatoly@gizra.com>
2025-06-01 10:57:10 +0300

    Cherry pick https://github.com/TIP-Global-Health/eheza-app/pull/1500

--- server/hedley/modules/custom/hedley_general/js/elm-main.js
+++ server/hedley/modules/custom/hedley_general/js/elm-main.js
@@ -8618,9 +8618,9 @@ var $author$project$Backend$Completion$Update$update = F3(
 			});
 		return A4($author$project$Backend$Types$BackendReturn, modelUpdated, $elm$core$Platform$Cmd$none, $author$project$Error$Utils$noError, _List_Nil);
 	});
-var $author$project$Backend$CompletionMenu$Model$MenuData = F2(
-	function (site, healthCenters) {
-		return {healthCenters: healthCenters, site: site};
+var $author$project$Backend$CompletionMenu$Model$MenuData = F3(
+	function (site, healthCenters, scope) {
+		return {healthCenters: healthCenters, scope: scope, site: site};
 	});
 var $author$project$Backend$Components$Model$HealthCenterData = F2(
 	function (id, name) {
@@ -8653,15 +8653,43 @@ var $author$project$Backend$Components$Decoder$decodeHealthCenterData = A3(
 		'id',
 		$author$project$Gizra$Json$decodeInt,
 		$elm$json$Json$Decode$succeed($author$project$Backend$Components$Model$HealthCenterData)));
-var $author$project$Backend$CompletionMenu$Decoder$decodeMenuData = A3(
-	$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
-	'health_centers',
-	$elm$json$Json$Decode$list($author$project$Backend$Components$Decoder$decodeHealthCenterData),
+var $author$project$Backend$Components$Model$ScopeFull = {$: 'ScopeFull'};
+var $author$project$Backend$Components$Model$ScopeHealthCenters = {$: 'ScopeHealthCenters'};
+var $author$project$Backend$Components$Decoder$decodeMenuScope = A2(
+	$elm$json$Json$Decode$andThen,
+	function (scope) {
+		switch (scope) {
+			case 'full':
+				return $elm$json$Json$Decode$succeed($author$project$Backend$Components$Model$ScopeFull);
+			case 'health_centers':
+				return $elm$json$Json$Decode$succeed($author$project$Backend$Components$Model$ScopeHealthCenters);
+			default:
+				return $elm$json$Json$Decode$fail(scope + ' is unknown MenuScope type');
+		}
+	},
+	$elm$json$Json$Decode$string);
+var $elm$json$Json$Decode$maybe = function (decoder) {
+	return $elm$json$Json$Decode$oneOf(
+		_List_fromArray(
+			[
+				A2($elm$json$Json$Decode$map, $elm$core$Maybe$Just, decoder),
+				$elm$json$Json$Decode$succeed($elm$core$Maybe$Nothing)
+			]));
+};
+var $author$project$Backend$CompletionMenu$Decoder$decodeMenuData = A4(
+	$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$optional,
+	'scope',
+	$elm$json$Json$Decode$maybe($author$project$Backend$Components$Decoder$decodeMenuScope),
+	$elm$core$Maybe$Nothing,
 	A3(
 		$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
-		'site',
-		$author$project$Backend$Decoder$decodeSite,
-		$elm$json$Json$Decode$succeed($author$project$Backend$CompletionMenu$Model$MenuData)));
+		'health_centers',
+		$elm$json$Json$Decode$list($author$project$Backend$Components$Decoder$decodeHealthCenterData),
+		A3(
+			$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
+			'site',
+			$author$project$Backend$Decoder$decodeSite,
+			$elm$json$Json$Decode$succeed($author$project$Backend$CompletionMenu$Model$MenuData))));
 var $author$project$Backend$CompletionMenu$Update$update = F3(
 	function (currentDate, msg, model) {
 		var value = msg.a;
@@ -9540,19 +9568,24 @@ var $author$project$Backend$Reports$Update$update = F3(
 			});
 		return A4($author$project$Backend$Types$BackendReturn, modelUpdated, $elm$core$Platform$Cmd$none, $author$project$Error$Utils$noError, _List_Nil);
 	});
-var $author$project$Backend$ReportsMenu$Model$MenuData = F2(
-	function (site, healthCenters) {
-		return {healthCenters: healthCenters, site: site};
+var $author$project$Backend$ReportsMenu$Model$MenuData = F3(
+	function (site, healthCenters, scope) {
+		return {healthCenters: healthCenters, scope: scope, site: site};
 	});
-var $author$project$Backend$ReportsMenu$Decoder$decodeMenuData = A3(
-	$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
-	'health_centers',
-	$elm$json$Json$Decode$list($author$project$Backend$Components$Decoder$decodeHealthCenterData),
+var $author$project$Backend$ReportsMenu$Decoder$decodeMenuData = A4(
+	$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$optional,
+	'scope',
+	$elm$json$Json$Decode$maybe($author$project$Backend$Components$Decoder$decodeMenuScope),
+	$elm$core$Maybe$Nothing,
 	A3(
 		$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
-		'site',
-		$author$project$Backend$Decoder$decodeSite,
-		$elm$json$Json$Decode$succeed($author$project$Backend$ReportsMenu$Model$MenuData)));
+		'health_centers',
+		$elm$json$Json$Decode$list($author$project$Backend$Components$Decoder$decodeHealthCenterData),
+		A3(
+			$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$required,
+			'site',
+			$author$project$Backend$Decoder$decodeSite,
+			$elm$json$Json$Decode$succeed($author$project$Backend$ReportsMenu$Model$MenuData))));
 var $author$project$Backend$ReportsMenu$Update$update = F3(
 	function (currentDate, msg, model) {
 		var value = msg.a;
@@ -10068,14 +10101,6 @@ var $author$project$Backend$Scoreboard$Decoder$decodeNutritionCriterionsData = f
 var $author$project$Backend$Scoreboard$Model$emptyNCDAData = {ancNewborn: $author$project$Backend$Scoreboard$Model$emptyANCNewbornData, infrastructureEnvironmentWash: $author$project$Backend$Scoreboard$Model$emptyInfrastructureEnvironmentWashData, nutritionBehavior: $author$project$Backend$Scoreboard$Model$emptyNutritionBehaviorData, targetedInterventions: $author$project$Backend$Scoreboard$Model$emptyTargetedInterventionsData, universalIntervention: $author$project$Backend$Scoreboard$Model$emptyUniversalInterventionData};
 var $author$project$Backend$Scoreboard$Model$emptyCriterionBySeverities = {moderate: _List_Nil, normal: _List_Nil, severe: _List_Nil};
 var $author$project$Backend$Scoreboard$Model$emptyNutritionCriterionsData = {muac: $author$project$Backend$Scoreboard$Model$emptyCriterionBySeverities, stunting: $author$project$Backend$Scoreboard$Model$emptyCriterionBySeverities, underweight: $author$project$Backend$Scoreboard$Model$emptyCriterionBySeverities, wasting: $author$project$Backend$Scoreboard$Model$emptyCriterionBySeverities};
-var $elm$json$Json$Decode$maybe = function (decoder) {
-	return $elm$json$Json$Decode$oneOf(
-		_List_fromArray(
-			[
-				A2($elm$json$Json$Decode$map, $elm$core$Maybe$Just, decoder),
-				$elm$json$Json$Decode$succeed($elm$core$Maybe$Nothing)
-			]));
-};
 var $author$project$Backend$Scoreboard$Decoder$decodePatientData = function (currentDate) {
 	return A4(
 		$NoRedInk$elm_json_decode_pipeline$Json$Decode$Pipeline$optional,
@@ -16121,21 +16146,30 @@ var $author$project$Pages$Utils$viewLoadDataButton = F3(
 	});
 var $author$project$Pages$CompletionMenu$View$viewMenu = F4(
 	function (language, themePath, data, model) {
-		var populationSelectionInput = A4(
-			$author$project$Pages$Utils$wrapSelectListInput,
-			language,
-			$author$project$Translate$Scope,
-			false,
-			A7(
-				$author$project$Pages$Utils$viewSelectListInput,
+		var populationSelectionInput = function () {
+			var allOptions = _List_fromArray(
+				[$author$project$Pages$Components$Types$SelectionOptionGlobal, $author$project$Pages$Components$Types$SelectionOptionHealthCenter]);
+			var options = A2(
+				$elm$core$Maybe$withDefault,
+				allOptions,
+				A2(
+					$elm$core$Maybe$map,
+					function (scope) {
+						if (scope.$ === 'ScopeFull') {
+							return allOptions;
+						} else {
+							return _List_fromArray(
+								[$author$project$Pages$Components$Types$SelectionOptionHealthCenter]);
+						}
+					},
+					data.scope));
+			return A4(
+				$author$project$Pages$Utils$wrapSelectListInput,
 				language,
-				model.populationSelection,
-				_List_fromArray(
-					[$author$project$Pages$Components$Types$SelectionOptionGlobal, $author$project$Pages$Components$Types$SelectionOptionHealthCenter]),
-				$author$project$Pages$Components$Utils$populationSelectionOptionToString,
-				$author$project$Pages$CompletionMenu$Model$SetPopulationSelection,
-				$author$project$Translate$PopulationSelectionOption,
-				'select-input'));
+				$author$project$Translate$Scope,
+				false,
+				A7($author$project$Pages$Utils$viewSelectListInput, language, model.populationSelection, options, $author$project$Pages$Components$Utils$populationSelectionOptionToString, $author$project$Pages$CompletionMenu$Model$SetPopulationSelection, $author$project$Translate$PopulationSelectionOption, 'select-input'));
+		}();
 		var _v0 = A2(
 			$elm$core$Maybe$withDefault,
 			_Utils_Tuple2(_List_Nil, $author$project$Gizra$Html$emptyNode),
@@ -38758,21 +38792,30 @@ var $author$project$Pages$Components$View$viewDemographicsSelectionActionButton 
 	});
 var $author$project$Pages$ReportsMenu$View$viewMenu = F4(
 	function (language, themePath, data, model) {
-		var populationSelectionInput = A4(
-			$author$project$Pages$Utils$wrapSelectListInput,
-			language,
-			$author$project$Translate$Scope,
-			false,
-			A7(
-				$author$project$Pages$Utils$viewSelectListInput,
+		var populationSelectionInput = function () {
+			var allOptions = _List_fromArray(
+				[$author$project$Pages$Components$Types$SelectionOptionGlobal, $author$project$Pages$Components$Types$SelectionOptionDemographics, $author$project$Pages$Components$Types$SelectionOptionHealthCenter]);
+			var options = A2(
+				$elm$core$Maybe$withDefault,
+				allOptions,
+				A2(
+					$elm$core$Maybe$map,
+					function (scope) {
+						if (scope.$ === 'ScopeFull') {
+							return allOptions;
+						} else {
+							return _List_fromArray(
+								[$author$project$Pages$Components$Types$SelectionOptionHealthCenter]);
+						}
+					},
+					data.scope));
+			return A4(
+				$author$project$Pages$Utils$wrapSelectListInput,
 				language,
-				model.populationSelection,
-				_List_fromArray(
-					[$author$project$Pages$Components$Types$SelectionOptionGlobal, $author$project$Pages$Components$Types$SelectionOptionDemographics, $author$project$Pages$Components$Types$SelectionOptionHealthCenter]),
-				$author$project$Pages$Components$Utils$populationSelectionOptionToString,
-				$author$project$Pages$ReportsMenu$Model$SetPopulationSelection,
-				$author$project$Translate$PopulationSelectionOption,
-				'select-input'));
+				$author$project$Translate$Scope,
+				false,
+				A7($author$project$Pages$Utils$viewSelectListInput, language, model.populationSelection, options, $author$project$Pages$Components$Utils$populationSelectionOptionToString, $author$project$Pages$ReportsMenu$Model$SetPopulationSelection, $author$project$Translate$PopulationSelectionOption, 'select-input'));
+		}();
 		var _v0 = A2(
 			$elm$core$Maybe$withDefault,
 			_Utils_Tuple2(_List_Nil, $author$project$Gizra$Html$emptyNode),
