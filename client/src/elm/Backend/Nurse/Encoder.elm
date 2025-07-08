2d37075bdb919cc72ff8ec104a9066dc4bf98757
Diama1 <danimahoro75@gmail.com>
2025-07-08 14:06:21 +0200

    encode the reasons for not consent

--- client/src/elm/Backend/Nurse/Encoder.elm
+++ client/src/elm/Backend/Nurse/Encoder.elm
@@ -5,6 +5,7 @@ import Backend.Nurse.Model exposing (..)
 import Backend.Nurse.Utils exposing (resilienceRoleToString)
 import Backend.Person.Encoder exposing (encodeEducationLevel, encodeGender, encodeMaritalStatus, encodeUbudehe)
 import Backend.ResilienceMessage.Encoder exposing (encodeResilienceMessage)
+import Backend.ResilienceMessage.Model exposing (ReasonForNotConsenting(..))
 import EverySet
 import Gizra.NominalDate exposing (encodeYYYYMMDD)
 import Gizra.TimePosix exposing (encodePosixAsSeconds)
@@ -36,6 +37,7 @@ encodeNurse nurse =
     , ( "resilience_program", bool nurse.resilienceProgramEnabled )
     , ( "resilience_start_date", maybe encodeYYYYMMDD nurse.resilienceProgramStartDate )
     , ( "resilience_role", maybe encodeResilienceRole nurse.resilienceRole )
+    , ( "resilience_consent_reason", maybe encodeReasonForNotConsenting nurse.resilienceConsentReason )
     , ( "birth_date", maybe encodeYYYYMMDD nurse.resilienceBirthDate )
     , ( "gender", maybe encodeGender nurse.resilienceGender )
     , ( "education_level", maybe encodeEducationLevel nurse.resilienceEducationLevel )
@@ -68,3 +70,25 @@ encodeRole role =
 encodeResilienceRole : ResilienceRole -> Value
 encodeResilienceRole =
     resilienceRoleToString >> string
+
+
+encodeReasonForNotConsenting : ReasonForNotConsenting -> Value
+encodeReasonForNotConsenting reason =
+    case reason of
+        ManyOtherCommitments ->
+            string "many_other_commitments"
+
+        NoDedicatedTimeForTheProgram ->
+            string "no_dedicated_time"
+
+        ProgramNotAddressingMyStressors ->
+            string "program_not_addressing_stressors"
+
+        DontWantToBeSeenAsStruggling ->
+            string "dont_want_to_be_seen_as_struggling"
+
+        TriedSimilarProgramBefore ->
+            string "tried_similar_program"
+
+        NotInterestedInProgram ->
+            string "not_interested"
