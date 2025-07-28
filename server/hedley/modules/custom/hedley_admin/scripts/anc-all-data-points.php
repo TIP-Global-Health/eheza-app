dca4ef52f7c185e82102ec7d9d0cb4128893bbbe
anvmn <anatoly@gizra.com>
2025-07-28 20:06:44 +0300

    Complete logic

--- server/hedley/modules/custom/hedley_admin/scripts/anc-all-data-points.php
+++ server/hedley/modules/custom/hedley_admin/scripts/anc-all-data-points.php
@@ -2,6 +2,7 @@
 
 /**
  * @file
+ * Records all data points of a Prenatal encounter.
  *
  * Execution:  drush scr
  *   profiles/hedley/modules/custom/hedley_admin/scripts/anc-all-data-points.php.
@@ -40,16 +41,32 @@ foreach ($data['measurements_types'] as $measurement_type) {
     }
 
     $field_info = field_info_field($field_name);
+    if ($field_info['type'] == 'image') {
+      continue;
+    }
+
     $data[$measurement_type][$field_name] = [
       'type' => $field_info['type'],
-      'multivalue' => $field_instance['cardinality'] == 1,
+      'multivalue' => $field_info['cardinality'] == -1,
     ];
   }
 }
 
-$labels = ['Pregnancy ID', 'Patient ID', 'Patient Birth Date'];
+$labels = [
+  'Pregnancy ID',
+  'Encounter ID',
+  'Encounter Type',
+  'Encounter Date',
+  'Patient ID',
+  'Patient Birth Date',
+  'Diagnoses',
+  'Diagnoses from previous encounters',
+];
 foreach ($data['measurements_types'] as $measurement_type) {
   $measurement_name = format_name($measurement_type);
+  if (empty($data[$measurement_type])) {
+    continue;
+  }
   foreach ($data[$measurement_type] as $field_name => $field_info) {
     $name = format_name($field_name);
     $labels[] = "$measurement_name: $name";
@@ -58,13 +75,13 @@ foreach ($data['measurements_types'] as $measurement_type) {
 
 $labels = implode(",", $labels);
 drush_print($labels);
-return;
 
 $base_query = new EntityFieldQuery();
 $base_query
   ->entityCondition('entity_type', 'node')
   ->propertyCondition('type', $type)
-  ->propertyOrderBy('nid');
+  ->propertyOrderBy('nid')
+  ->addTag('exclude_deleted');
 
 $count_query = clone $base_query;
 $count_query->propertyCondition('nid', $nid, '>');
@@ -75,13 +92,7 @@ if ($count == 0) {
   exit;
 }
 
-drush_print("$count nodes of type $type located.");
-
-$processed = 0;
 while (TRUE) {
-  // Free up memory.
-  drupal_static_reset();
-
   $query = clone $base_query;
   if ($nid) {
     $query->propertyCondition('nid', $nid, '>');
@@ -97,6 +108,7 @@ while (TRUE) {
   }
 
   $ids = array_keys($result['node']);
+  $nid = end($ids);
   $encounters = node_load_multiple($ids);
   foreach ($encounters as $encounter) {
     $pregnancy_id = $encounter->field_individual_participant[LANGUAGE_NONE][0]['target_id'];
@@ -104,21 +116,155 @@ while (TRUE) {
     $patient_id = $pregnancy->field_person[LANGUAGE_NONE][0]['target_id'];
     $patient = node_load($patient_id);
     $birth_date = explode(' ', $patient->field_birth_date[LANGUAGE_NONE][0]['value'])[0];
+    $encounter_type = $encounter->field_prenatal_encounter_type[LANGUAGE_NONE][0]['value'];
+    $encounter_type = empty($encounters) ? 'Nurse' : hedley_general_get_field_sign_label('field_prenatal_encounter_type', $encounter_type);
+    $encounter_date = explode(' ', $encounter->field_scheduled_date[LANGUAGE_NONE][0]['value'])[0];
+    // Diagnoses.
+    $field_values = $encounter->field_prenatal_diagnoses[LANGUAGE_NONE];
+    if (empty($field_values)) {
+      $diagnoses = '';
+    }
+    else {
+      $values = [];
+      foreach ($field_values as $item) {
+        $values[] = hedley_general_get_field_sign_label('field_prenatal_diagnoses', $item['value']);
+      }
+      $diagnoses = implode(' & ', $values);
+    }
+
+    // Past Diagnoses.
+    $field_values = $encounter->field_past_prenatal_diagnoses[LANGUAGE_NONE];
+    if (empty($field_values)) {
+      $past_diagnoses = '';
+    }
+    else {
+      $values = [];
+      foreach ($field_values as $item) {
+        $values[] = hedley_general_get_field_sign_label('field_past_prenatal_diagnoses', $item['value']);
+      }
+      $past_diagnoses = implode(' & ', $values);
+    }
+
+    $out = [
+      $pregnancy_id,
+      $encounter->nid,
+      $encounter_type,
+      $encounter_date,
+      $patient_id,
+      $birth_date,
+      $diagnoses,
+      $past_diagnoses,
+    ];
+
+    // Get all measurements that belong to encounter.
+    $query = new EntityFieldQuery();
+    $result = $query
+      ->entityCondition('entity_type', 'node')
+      ->entityCondition('bundle', $data['measurements_types'], 'IN')
+      ->propertyCondition('status', NODE_PUBLISHED)
+      ->fieldCondition('field_prenatal_encounter', 'target_id', $encounter->nid)
+      ->propertyOrderBy('nid')
+      ->addTag('exclude_deleted')
+      ->execute();
+
+    if (empty($result['node'])) {
+      // Skip encounter without any measurements.
+      continue;
+    }
+
+    // As there's a possibility of multiple measurements of sane type
+    // (due to glitches), make sure to take only the most recent ones.
+    $measurements = [];
+    $ids = array_keys($result['node']);
+    $nodes = node_load_multiple($ids);
+    foreach ($nodes as $node) {
+      $measurements[$node->type] = $node;
+    }
+    // Explicitly unset large variables after use, for memory optimization.
+    unset($nodes);
+
+    // Collect values.
+    foreach ($data['measurements_types'] as $measurement_type) {
+      $measurement = $measurements[$measurement_type];
+      if (empty($data[$measurement_type])) {
+        continue;
+      }
+
+      foreach ($data[$measurement_type] as $field_name => $field_info) {
+        if (empty($measurement)) {
+          $out[] = '';
+          continue;
+        }
+
+        switch ($field_info['type']) {
+          case 'datetime':
+            $out[] = explode(' ', $measurement->{$field_name}[LANGUAGE_NONE][0]['value'])[0];
+            break;
+
+          case 'entityreference':
+            $out[] = $measurement->{$field_name}[LANGUAGE_NONE][0]['target_id'];
+            break;
+
+          case 'list_text':
+            if ($field_info['multivalue']) {
+              $values = [];
+              $field_values = $measurement->{$field_name}[LANGUAGE_NONE];
+              if (empty($field_values)) {
+                $out[] = '';
+              }
+              else {
+                foreach ($field_values as $item) {
+                  $values[] = hedley_general_get_field_sign_label($field_name, $item['value']);
+                }
+                $out[] = implode(' & ', $values);
+              }
+            }
+            else {
+              $value = $measurement->{$field_name}[LANGUAGE_NONE][0]['value'];
+              $out[] = hedley_general_get_field_sign_label($field_name, $value);
+            }
+            break;
+
+          case 'list_boolean':
+            $value = $measurement->{$field_name}[LANGUAGE_NONE][0]['value'];
+            $out[] = $value ? 'true' : 'false';
+            break;
+
+          default:
+            $out[] = $measurement->{$field_name}[LANGUAGE_NONE][0]['value'];
+        }
+      }
+    }
+    // Explicitly unset large variables after use, for memory optimization.
+    unset($measurements);
+
+    // Print values.
+    $out = implode(',', $out);
+    drush_print($out);
+
+    // Free up memory.
+    drupal_static_reset();
   }
+  // Explicitly unset large variables after use, for memory optimization.
+  unset($encounters);
 }
 
+/**
+ * Formats given value from snake case to readable.
+ *
+ * @param string $value
+ *   The value.
+ *
+ * @return string
+ *   Formated readable value.
+ */
+function format_name($value) {
+  // Remove common prefixes.
+  $clean_name = preg_replace('/^(prenatal_|field_)/', '', $value);
 
-
-drush_print("Done!");
-
-
-function format_name($name) {
-  // Remove common prefixes
-  $clean_name = preg_replace('/^(prenatal_|field_)/', '', $name);
-
-  // Replace underscores with spaces
+  // Replace underscores with spaces.
   $clean_name = str_replace('_', ' ', $clean_name);
 
-  // Capitalize first letter of each word
+  // Capitalize first letter of each word.
   return ucwords($clean_name);
 }
