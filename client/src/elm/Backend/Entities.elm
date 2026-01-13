19dc66c9c170fbaf8443b3fb8caea098131c0643
anvmn <anatoly@gizra.com>
2026-01-13 17:56:03 +0200

    ID [ci skip]

--- client/src/elm/Backend/Entities.elm
+++ client/src/elm/Backend/Entities.elm
@@ -1729,3 +1729,11 @@ type alias HIVTreatmentReviewId =
 
 type HIVTreatmentReviewUuidType
     = HIVTreatmentReviewUuidType
+
+
+type alias PrenatalUltrasoundId =
+    EntityUuid PrenatalUltrasoundUuidType
+
+
+type PrenatalUltrasoundUuidType
+    = PrenatalUltrasoundUuidType
