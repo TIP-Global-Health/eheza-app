5ebb5c209dd1ab6462dad38c30dd50574292daca
anvmn <anatoly@gizra.com>
2026-01-29 11:41:36 +0200

    Drop blocking (unused) files

--- ci-scripts/install_elm_review.sh
+++ ci-scripts/install_elm_review.sh
@@ -11,4 +11,7 @@ set -e
 # This avoids issues with installing all client dependencies which may have
 # native compilation requirements not needed for elm-review
 
+rm -f ./src/elm/ConfigDeploy.elm
+rm -f ./src/elm/ZScore/Test.elm
+
 echo "elm-review will be fetched by npx on first use"
